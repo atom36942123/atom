f5ea52c810a926e55f4b2e7d6de465f4c01be643
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-22 12:47:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,6 +34,5 @@ config_admin_panel=env("admin_panel")
 
 #custom
 config_x=["test","atom"]
-config_table=["atom","users","post","likes","comment","bookmark","report","rating","block","message","helpdesk","s3","otp","workseeker"]
 config_switch_mongo=0
 config_switch_elasticsearch=0
