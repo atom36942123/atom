9abc2d0e9a39b03b1134a76eb832d969e7091dc8
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-22 12:02:37 +0530

    Update api_s3.py

--- api_s3.py
+++ api_s3.py
@@ -1,1 +1,16 @@
+#import
+from object import postgres_object
+from config import *
+from function import *
+from fastapi import Request
+from fastapi import BackgroundTasks
+from datetime import datetime
+from typing import Literal
+import json
+import random
+import uuid
+
+#router
+from fastapi import APIRouter
+router=APIRouter(tags=["s3"])
 
