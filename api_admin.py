17d2404d41403b5ccb2ae6c1ba6393f96b700f7a
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-22 15:30:11 +0530

    Update api_admin.py

--- api_admin.py
+++ api_admin.py
@@ -3,16 +3,10 @@ from config import *
 from function import *
 from object import postgres_object
 from fastapi import Request
-from fastapi import BackgroundTasks
-from datetime import datetime
 from typing import Literal
-import json
-import random
 from fastapi import File
 from fastapi import UploadFile
-from fastapi import Body
 import csv,codecs
-from fastapi_cache.decorator import cache
 
 #router
 from fastapi import APIRouter
