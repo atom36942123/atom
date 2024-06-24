4e50c4db4333e5dd2f162818b95552073dedbb77
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-24 15:36:31 +0530

    Update api_utility.py

--- api_utility.py
+++ api_utility.py
@@ -126,7 +126,7 @@ async def api_func(x:str,request:Request,table:str,id:int,column:str,value:str):
     if column_datatype=="integer":value=int(value)
   except Exception as e:return function_http_response(400,0,e.args)
   #permission check
-  if request_user["type"] in ["root"]:created_by_id=None
+  if request_user["type"] in ["root","admin"]:created_by_id=None
   else:
     if table=="users":created_by_id,id=None,request_user['id']
     if table!="users":created_by_id=request_user['id']
