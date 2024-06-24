0aea157cdb764f3d4250260a053fc3b7f982663f
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-24 08:49:46 +0530

    Update api_mongo.py

--- api_mongo.py
+++ api_mongo.py
@@ -1,12 +1,3 @@
-#impport
-from object import mongo_object
-from function import function_http_response
-from bson import ObjectId
-from fastapi import Body
-
-#router
-from fastapi import APIRouter
-router=APIRouter(tags=["mongo"])
 
 #api
 @router.post("/{x}/mongo-create-object")
