c6cd113a58c4919674df4e13a72f7f8124272062
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-24 09:01:46 +0530

    Update api_utility.py

--- api_utility.py
+++ api_utility.py
@@ -13,6 +13,23 @@ from fastapi import APIRouter
 router=APIRouter(tags=["utility"])
 
 #api
+@router.get("/{x}/create-s3-url")
+async def api_func(x:str,request:Request,filename:str,background_tasks:BackgroundTasks):
+    #token check
+    response=await function_token_decode(request,config_jwt_secret_key)
+    if response["status"]==0:return function_http_response(400,0,response["message"])
+    request_user=response["message"]
+    #logic
+    key=str(uuid.uuid4())+"."+filename.split(".")[-1]
+    response=await function_s3_create_url(config_aws_s3_bucket_region,config_aws_access_key_id,config_aws_secret_access_key,config_aws_s3_bucket_name,key)
+    if response["status"]==0:return function_http_response(400,0,response["message"])
+    #save s3 url
+    query="insert into s3 (created_by_id,file_url) values (:created_by_id,:file_url) returning *;"
+    values={"created_by_id":request_user["id"],"file_url":response["message"]['url']+response["message"]['fields']['key']}
+    background_tasks.add_task(function_query_runner,postgres_object[x],"write",query,values)
+    #finally
+    return response
+    
 @router.get("/{x}/send-email")
 async def api_func(x:str,request:Request,to:str,title:str,description:str):
     #token check
