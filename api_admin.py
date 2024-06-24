0e3ece0a6f151103fac089c8c429143598c77256
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-24 18:29:16 +0530

    Update api_admin.py

--- api_admin.py
+++ api_admin.py
@@ -123,7 +123,8 @@ async def api_func(x:str,request:Request,mode:str,query:str):
    if query=="table":query="select * from information_schema.tables where table_schema='public' and table_type='BASE TABLE';"
    if query=="column":query="select * from information_schema.columns where table_schema='public';"
    if query=="constraint":query="select constraint_name from information_schema.constraint_column_usage;"
-   if query=="table_count":query='''
+   if query=="index":query="select * from pg_indexes where schemaname='public';"
+   if query=="column_count":query='''
    with x as (select relname as table_name,n_live_tup as count_row from pg_stat_user_tables),
    y as (select table_name,count(*) as count_column from information_schema.columns group by table_name)
    select x.*,y.count_column from x left join y on x.table_name=y.table_name order by count_column desc;
