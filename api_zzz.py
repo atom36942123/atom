be871bf13e597fc1c545e6cf9d7255e5920222fb
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-24 08:51:59 +0530

    Update api_zzz.py

--- api_zzz.py
+++ api_zzz.py
@@ -1,1 +1,10 @@
+#impport
+from object import mongo_object
+from function import function_http_response
+from bson import ObjectId
+from fastapi import Body
+
+#router
+from fastapi import APIRouter
+router=APIRouter(tags=["zzz"])
 
