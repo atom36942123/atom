a80837591361b294136374f64789651d91d8eabc
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-25 14:52:24 +0530

    Update api_crud.py

--- api_crud.py
+++ api_crud.py
@@ -131,7 +131,7 @@ async def api_func(x:str,request:Request,table:str,id:int,body:schema_atom):
    #keys not allowed
    if request_user["type"] not in ["root","admin"]:
       for item in ["created_by_id","received_by_id","is_active","is_verified"]:
-         del param[item]
+         if item in param:del param[item]
    if not param:return function_http_response(400,0,"body null issue after not allowed keys remove")
    #permission set
    if request_user["type"] in ["root","admin"]:created_by_id=None
