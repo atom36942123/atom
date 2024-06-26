57f7d8007dff4f1d90fca4b6b55c179bfa0d840f
atom <102727104+atom36942123@users.noreply.github.com>
2024-06-26 15:37:06 +0530

    Update api_zzz.py

--- api_zzz.py
+++ api_zzz.py
@@ -10,14 +10,14 @@ router=APIRouter(tags=["zzz"])
 
 #mongo
 @router.post("/{x}/mongo-create-object")
-async def api_func(x:str,database:str,table:str,body:dict=Body(...)):
+async def function_api(x:str,database:str,table:str,body:dict=Body(...)):
     if database=="test" and table=="users":
         try:response=await mongo_object.test.users.insert_one(body)
         except Exception as e:return function_http_response(400,0,e.args)
     return {"status":1,"message":str((response.inserted_id))}
     
 @router.get("/{x}/mongo-read-object")
-async def api_func(x:str,database:str,table:str,id:str):
+async def function_api(x:str,database:str,table:str,id:str):
     if database=="test" and table=="users":
         try:response=await mongo_object.test.users.find_one({"_id": ObjectId(id)})
         except Exception as e:return function_http_response(400,0,e.args)
@@ -25,14 +25,14 @@ async def api_func(x:str,database:str,table:str,id:str):
     return response
     
 @router.put("/{x}/mongo-update-object")
-async def api_func(x:str,database:str,table:str,id:str,body:dict=Body(...)):
+async def function_api(x:str,database:str,table:str,id:str,body:dict=Body(...)):
     if database=="test" and table=="users":
         try:response=await mongo_object.test.users.update_one({"_id":ObjectId(id)},{"$set":body})
         except Exception as e:return function_http_response(400,0,e.args)
     return response.modified_count
  
 @router.delete("/{x}/mongo-delete-object")
-async def api_func(x:str,database:str,table:str,id:str):
+async def function_api(x:str,database:str,table:str,id:str):
     if database=="test" and table=="users":
         try:response=await mongo_object.test.users.delete_one({"_id":ObjectId(id)})
         except Exception as e:return function_http_response(400,0,e.args)
@@ -41,37 +41,37 @@ async def api_func(x:str,database:str,table:str,id:str):
 
 #elasticsearch
 @router.post("/{x}/elasticsearch-create-object")
-async def api_func(x:str,table:str,id:int,body:dict=Body(...)):
+async def function_api(x:str,table:str,id:int,body:dict=Body(...)):
     try:response=elasticsearch_object.index(index=table,id=id,document=body)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
 
 @router.get("/{x}/elasticsearch-read-object")
-async def api_func(x:str,table:str,id:int):
+async def function_api(x:str,table:str,id:int):
     try:response=elasticsearch_object.get(index=table,id=id)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
 
 @router.put("/{x}/elasticsearch-update-object")
-async def api_func(x:str,table:str,id:int,body:dict=Body(...)):
+async def function_api(x:str,table:str,id:int,body:dict=Body(...)):
     try:response=elasticsearch_object.update(index=table,id=id,doc=body)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
 
 @router.delete("/{x}/elasticsearch-delete-object")
-async def api_func(x:str,table:str,id:int):
+async def function_api(x:str,table:str,id:int):
     try:response=elasticsearch_object.delete(index=table,id=id)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
 
 @router.get("/{x}/elasticsearch-refresh-table")
-async def api_func(x:str,table:str):
+async def function_api(x:str,table:str):
     try:response=elasticsearch_object.indices.refresh(index=table)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
 
 @router.get("/{x}/elasticsearch-search")
-async def api_func(x:str,table:str,column:str,keyword:str):
+async def function_api(x:str,table:str,column:str,keyword:str):
     query={"query":{"match":{column:keyword}},"size":30}
     try:response=elasticsearch_object.search(index=table,body=query)
     except Exception as e:return function_http_response(400,0,e.args)
