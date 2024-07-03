173961932b4cf1cd22cc745bccd2aa144d032043
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-03 13:18:02 +0530

    Update api_database.py

--- api_database.py
+++ api_database.py
@@ -84,20 +84,6 @@ config_column_unique={
 "username":["users"],
 "created_by_id,parent_table,parent_id":["likes","bookmark","report","block"],
 }
-config_column_index={
-"created_at":["default",["atom","users","post","message"]],
-"created_by_id":["default",["atom","post","likes","comment","bookmark","report","rating","message"]],
-"parent_table":["default",["likes","comment","bookmark","report","rating","block"]],
-"parent_id":["default",["likes","comment","bookmark","report","rating","block"]],
-"received_by_id":["default",["message"]],
-"description":["default",["comment","message","helpdesk"]],
-"type":["default",["atom","post","helpdesk","users"]],
-"password":["default",["users"]],
-"firebase_id":["default",["users"]],
-"email":["default",["otp"]],
-"mobile":["default",["otp"]],
-"tag":["array",["atom","users","post"]],
-}
 config_query={
 "create_root_user":"insert into users (username,password,type) values ('root','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','root') on conflict do nothing returning *;",
 "rule_delete_disable_users_root":"create or replace rule rule_delete_disable_users_root as on delete to users where old.id=1 or old.type='root' do instead nothing;",
@@ -181,15 +167,7 @@ async def function_api_database_init(x:str,request:Request):
             if constraint_name not in schema_constraint_name_list:
                 query=f"alter table {table} add constraint {constraint_name} unique ({k});"
                 response=await function_query_runner(postgres_object[x],"write",query,{})
-                if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
-    #column index
-    for k,v in config_column_index.items():
-        for table in v[1]:
-            index_name=f"index_{k}_{table}"
-            query=f"create index if not exists {index_name} on {table}({k});"
-            if v[0]=="array":query=f"create index if not exists {index_name} on {table} using gin ({k});"
-            response=await function_query_runner(postgres_object[x],"write",query,{})
-            if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
+                if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")    
     #query
     for k,v in config_query.items():
         response=await function_query_runner(postgres_object[x],"write",v,{})
@@ -201,16 +179,31 @@ async def function_api_database_init(x:str,request:Request):
 async def function_api_database_index(x:str,request:Request):
     #token check
     if request.headers.get("token")!=config_token_root:return function_http_response(400,0,"token mismatch")
-    #drop all query
-    query='''
-    DO $$ DECLARE r RECORD;
-    BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname=current_schema()) LOOP
-    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE'; 
-    END LOOP;
-    END $$;
-    '''
-    #logic
-    response=await function_query_runner(postgres_object[x],"write",query,{})
+    #drop all index
+    response=await function_drop_all_index(postgres_object[x],function_query_runner)
     if response["status"]==0:return function_http_response(400,0,response["message"])
+    #mapping
+    config_column_index={
+    "created_at":["default",["atom","users","post","message"]],
+    "created_by_id":["default",["atom","post","likes","comment","bookmark","report","rating","message"]],
+    "parent_table":["default",["likes","comment","bookmark","report","rating","block"]],
+    "parent_id":["default",["likes","comment","bookmark","report","rating","block"]],
+    "received_by_id":["default",["message"]],
+    "description":["default",["comment","message","helpdesk"]],
+    "type":["default",["atom","post","helpdesk","users"]],
+    "password":["default",["users"]],
+    "firebase_id":["default",["users"]],
+    "email":["default",["otp"]],
+    "mobile":["default",["otp"]],
+    "tag":["array",["atom","users","post"]],
+    }
+    #logic
+    for k,v in config_column_index.items():
+        for table in v[1]:
+            index_name=f"index_{k}_{table}"
+            query=f"create index if not exists {index_name} on {table}({k});"
+            if v[0]=="array":query=f"create index if not exists {index_name} on {table} using gin ({k});"
+            response=await function_query_runner(postgres_object[x],"write",query,{})
+            if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
     #finally
     return {"status":1,"message":"reset done"}
