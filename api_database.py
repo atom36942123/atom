069940f7afcb6f74ef002eff68ae5ae35d1e4c8c
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-03 14:32:12 +0530

    Update api_database.py

--- api_database.py
+++ api_database.py
@@ -11,34 +11,6 @@ router=APIRouter(tags=["database"])
 #config
 config_table=["atom","users","post","likes","comment","bookmark","report","rating","block","message","helpdesk","s3","otp","workseeker"]
 
-
-config_column_checkin={
-"is_active":[(0,1),["atom","users","post","comment","workseeker"]],
-"is_verified":[(0,1),["atom","users","post","comment","workseeker"]],
-"is_working":[(0,1),["workseeker"]],
-}
-config_column_default={
-"created_at":["now()",config_table],
-"last_active_at":["now()",["users"]],
-"is_active":[1,["atom","users","post","comment","workseeker"]],
-"is_verified":[0,["atom","users","post","comment","workseeker"]],
-}
-config_column_nullable={
-"created_by_id":["message"],
-"received_by_id":["message"],
-"parent_table":["likes","comment","bookmark","report","rating","block"],
-"parent_id":["likes","comment","bookmark","report","rating","block"],
-}
-config_column_unique={
-"username":["users"],
-"created_by_id,parent_table,parent_id":["likes","bookmark","report","block"],
-}
-config_query={
-"create_root_user":"insert into users (username,password,type) values ('root','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','root') on conflict do nothing returning *;",
-"rule_delete_disable_users_root":"create or replace rule rule_delete_disable_users_root as on delete to users where old.id=1 or old.type='root' do instead nothing;",
-"index_comment_parent":"create index if not exists index_comment_parent on comment(parent_table,parent_id);",
-}
-
 #api
 @router.get("/{x}/database-reset")
 async def function_api_database_reset(x:str,request:Request):
@@ -131,8 +103,24 @@ async def function_api_database_create(x:str,request:Request):
     #finally
     return {"status":1,"message":"create done"}
 
-@router.get("/{x}/database-init")
-async def function_api_database_init(x:str,request:Request):
+@router.get("/{x}/database-query")
+async def function_api_database_query(x:str,request:Request):
+    #token check
+    if request.headers.get("token")!=config_token_root:return function_http_response(400,0,"token mismatch")
+    #config
+    config_query={
+    "create_root_user":"insert into users (username,password,type) values ('root','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','root') on conflict do nothing returning *;",
+    "rule_delete_disable_users_root":"create or replace rule rule_delete_disable_users_root as on delete to users where old.id=1 or old.type='root' do instead nothing;",
+    }
+    #logic
+    for k,v in config_query.items():
+        response=await function_query_runner(postgres_object[x],"write",v,{})
+        if response["status"]==0:return function_http_response(400,0,f"error={response['message']}")
+    #finally
+    return {"status":1,"message":"query done"}
+
+@router.get("/{x}/database-alter")
+async def function_api_database_alter(x:str,request:Request):
     #token check
     if request.headers.get("token")!=config_token_root:return function_http_response(400,0,"token mismatch")
     #read schema column
@@ -145,15 +133,13 @@ async def function_api_database_init(x:str,request:Request):
     response=await function_query_runner(postgres_object[x],"read",query,{})
     if response["status"]==0:return function_http_response(400,0,response["message"])
     schema_constraint_name_list=[item["constraint_name"] for item in response["message"]]
-    #column checkin
-    for k,v in config_column_checkin.items():
-        for table in v[1]:
-            constraint_name=f"checkin_{k}_{table}"
-            if constraint_name not in schema_constraint_name_list:
-                query=f"alter table {table} add constraint {constraint_name} check ({k} in {v[0]});"
-                response=await function_query_runner(postgres_object[x],"write",query,{})
-                if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
     #column default
+    config_column_default={
+    "created_at":["now()",config_table],
+    "last_active_at":["now()",["users"]],
+    "is_active":[1,["atom","users","post","comment","workseeker"]],
+    "is_verified":[0,["atom","users","post","comment","workseeker"]],
+    }
     for column in schema_column:
         for k,v in config_column_default.items():
             for table in v[1]:
@@ -161,7 +147,26 @@ async def function_api_database_init(x:str,request:Request):
                     query=f"alter table {table} alter column {k} set default {v[0]};"
                     response=await function_query_runner(postgres_object[x],"write",query,{})
                     if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
+    #column checkin
+    config_column_checkin={
+    "is_active":[(0,1),["atom","users","post","comment","workseeker"]],
+    "is_verified":[(0,1),["atom","users","post","comment","workseeker"]],
+    "is_working":[(0,1),["workseeker"]],
+    }
+    for k,v in config_column_checkin.items():
+        for table in v[1]:
+            constraint_name=f"checkin_{k}_{table}"
+            if constraint_name not in schema_constraint_name_list:
+                query=f"alter table {table} add constraint {constraint_name} check ({k} in {v[0]});"
+                response=await function_query_runner(postgres_object[x],"write",query,{})
+                if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
     #column nullable
+    config_column_nullable={
+    "created_by_id":["message"],
+    "received_by_id":["message"],
+    "parent_table":["likes","comment","bookmark","report","rating","block"],
+    "parent_id":["likes","comment","bookmark","report","rating","block"],
+    }
     for column in schema_column:
         for k,v in config_column_nullable.items():
             for table in v:
@@ -170,6 +175,10 @@ async def function_api_database_init(x:str,request:Request):
                     response=await function_query_runner(postgres_object[x],"write",query,{})
                     if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
     #column unique
+    config_column_unique={
+    "username":["users"],
+    "created_by_id,parent_table,parent_id":["likes","bookmark","report","block"],
+    }
     for k,v in config_column_unique.items():
         for table in v:
             constraint_name=f"unique_{k}_{table}".replace(",","_")
@@ -177,12 +186,8 @@ async def function_api_database_init(x:str,request:Request):
                 query=f"alter table {table} add constraint {constraint_name} unique ({k});"
                 response=await function_query_runner(postgres_object[x],"write",query,{})
                 if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")    
-    #query
-    for k,v in config_query.items():
-        response=await function_query_runner(postgres_object[x],"write",v,{})
-        if response["status"]==0:return function_http_response(400,0,f"error={response['message']}")
     #finally
-    return {"status":1,"message":"init done"}
+    return {"status":1,"message":"alter done"}
 
 @router.get("/{x}/database-index")
 async def function_api_database_index(x:str,request:Request):
@@ -205,6 +210,7 @@ async def function_api_database_index(x:str,request:Request):
     "email":["default",["otp"]],
     "mobile":["default",["otp"]],
     "tag":["array",["atom","users","post"]],
+    "parent_table,parent_id":["default",["likes","comment","bookmark","report","rating","block"]],
     }
     #logic
     for k,v in config_column_index.items():
