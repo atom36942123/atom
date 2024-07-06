2e8e3dca5499f604027389b9e3007bf7415d9785
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-06 13:30:26 +0530

    Update function.py

--- function.py
+++ function.py
@@ -46,9 +46,9 @@ async def function_object_read(postgres_object,function_query_runner,table,param
    #where set
    where="where "
    for k,v in param.items():
+      where=where+f"({k} = :{k} or :{k} is null) and "
       if k=="tag":where=where+f"({k} @> :{k} or :{k} is null) and "
       if k=="rating":where=where+f"({k} >= :{k} or :{k} is null) and "
-      else:where=where+f"({k} = :{k} or :{k} is null) and "
    where=where.strip().rsplit('and',1)[0]
    if where=="where":where=""
    #logic
