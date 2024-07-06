096540930d5393c2652cdc88311d703cb3d6deb1
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-06 11:08:40 +0530

    Update api_crud.py

--- api_crud.py
+++ api_crud.py
@@ -239,7 +239,7 @@ async def function_api_object_read_public(x:str,request:Request,table:Literal["u
    limit=30
    offset=(page-1)*limit
    if tag:tag=tag.split(",")
-   param={"id":id,"created_by_id":created_by_id,"type":type,"username":username,"parent_table":parent_table,"parent_id":parent_id,"tag":tag}
+   param={"id":id,"created_by_id":created_by_id,"type":type,"username":username,"parent_table":parent_table,"parent_id":parent_id,"tag":tag,"is_pinned":is_pinned}
    response=await function_object_read(postgres_object[x],function_query_runner,table,param,["id","desc"],limit,offset)
    if response["status"]==0:return function_http_response(400,0,response["message"])
    #add user key
