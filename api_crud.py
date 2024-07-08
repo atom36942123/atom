58cc0314d1a88aaa1ff0949ce4154cfe7bc1f580
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-08 21:29:21 +0530

    Update api_crud.py

--- api_crud.py
+++ api_crud.py
@@ -116,9 +116,6 @@ async def function_api_object_update(x:str,request:Request,table:str,id:int,body
    #param validation
    response=await function_param_validation(param)
    if response["status"]==0:return function_http_response(400,0,response["message"])
-   #param table validation
-   response=await function_param_table_validation(param,table)
-   if response["status"]==0:return function_http_response(400,0,response["message"])
    #param conversion
    response=await function_param_conversion(param)
    if response["status"]==0:return function_http_response(400,0,response["message"])
