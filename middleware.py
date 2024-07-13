0bce054d22b428a9f9a41c574ffbfcb5bcbd59ba
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-13 15:34:57 +0530

    Update middleware.py

--- middleware.py
+++ middleware.py
@@ -1,16 +1,1 @@
-#import
-from app import app
-from fastapi import Request
-from config import config_x
-from function import function_http_response
 
-#logic
-@app.middleware("http")
-async def middleware(request:Request,api_function):
-   #x check
-   if str(request.url).split("/")[3] not in ["","docs","redoc","openapi.json"]+config_x:return function_http_response(400,0,"wrong x")
-   #api response
-   try:response=await api_function(request)
-   except Exception as e:return function_http_response(400,0,e.args)
-   #finally
-   return response
