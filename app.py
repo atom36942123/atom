cc4acca047fe1bc9e4b018dcd35e9800f6b334a5
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-13 14:18:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
 #import
-from middleware import *
+# from middleware import *
 
 #api
 @app.get("/")
