c0dc115cef5ae583a0256d3fcba4ee1d368ca96a
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-13 14:14:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,8 @@ app=FastAPI(lifespan=lifespan)
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
-#middleware
-from fastapi import Request
-from config import config_x
-from function import function_http_response
-@app.middleware("http")
-async def middleware(request:Request,api_function):
-   #x check
-   if str(request.url).split("/")[3] not in ["","docs","redoc","openapi.json"]+config_x:return function_http_response(400,0,"wrong x")
-   #api response
-   try:response=await api_function(request)
-   except Exception as e:return function_http_response(400,0,e.args)
-   #finally
-   return response
+#import
+from middleware import *
 
 #api
 @app.get("/")
