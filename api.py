552ed0dad6daabe11285c707e58a0a022d93e308
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-13 17:58:04 +0530

    Update api.py

--- api.py
+++ api.py
@@ -13,18 +13,18 @@ router_database=APIRouter(tags=["database"])
 async def function_api_database_create(x:str,request:Request):
     #token check
     if request.headers.get("token")!=config_token_root:return function_http_response(400,0,"token mismatch")
-    #table
+    #table create
     for item in config_table:
         query=f"create table if not exists {item} (id bigint primary key generated always as identity);"
         response=await function_query_runner(postgres_object[x],"write",query,{})
         if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
-    #column
+    #column create
     for k,v in config_column.items():
         for table in v[1]:
             query=f"alter table {table} add column if not exists {k} {v[0]};"
             response=await function_query_runner(postgres_object[x],"write",query,{})
             if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
-    #finally
+    #final response
     return {"status":1,"message":"database create done"}
 
 
