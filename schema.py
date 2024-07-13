266711c1e8b4e82330647c750ba293261577b0ff
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-13 22:19:12 +0530

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,1 +1,17 @@
+#import
+from pydantic import BaseModel
+from typing import Literal
 
+#schema
+class schema_signup(BaseModel):
+   username:str
+   password:str
+   
+class schema_login(BaseModel):
+   mode:Literal["username","firebase","email","mobile"]="username"
+   username:str|None=None
+   password:str|None=None
+   firebase_id:str|None=None
+   email:str|None=None
+   mobile:str|None=None
+   otp:int|None=None
