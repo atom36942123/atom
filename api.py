44ec4d7f3ca12c74081fff3db3dffe4f80c76abb
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-13 20:30:00 +0530

    Update api.py

--- api.py
+++ api.py
@@ -87,7 +87,7 @@ async def function_api_database_index(x:str,request:Request):
     #drop all index
     response=await function_drop_all_index(postgres_object[x],function_query_runner)
     if response["status"]==0:return function_http_response(400,0,response["message"])
-    #logic
+    #create index
     for k,v in config_column_index.items():
         for table in v[1]:
             index_name=f"index_{k.replace(',','_')}_{table}"
@@ -96,22 +96,17 @@ async def function_api_database_index(x:str,request:Request):
             response=await function_query_runner(postgres_object[x],"write",query,{})
             if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
     #finally
-    return {"status":1,"message":"index done"}
+    return {"status":1,"message":"database index done"}
 
 @router.get("/{x}/database-query")
 async def function_api_database_query(x:str,request:Request):
     #token check
     if request.headers.get("token")!=config_token_root:return function_http_response(400,0,"token mismatch")
-    #config
-    config_query={
-    "create_root_user":"insert into users (username,password,type) values ('root','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','root') on conflict do nothing returning *;",
-    "rule_delete_disable_users_root":"create or replace rule rule_delete_disable_users_root as on delete to users where old.id=1 or old.type='root' do instead nothing;",
-    }
     #logic
     for k,v in config_query.items():
         response=await function_query_runner(postgres_object[x],"write",v,{})
         if response["status"]==0:return function_http_response(400,0,f"error={response['message']}")
     #finally
-    return {"status":1,"message":"query done"}
+    return {"status":1,"message":"database query done"}
 
 
