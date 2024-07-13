5fac562801e8bef0b4924ee258e5eea827bedca8
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-13 20:35:00 +0530

    Update query.py

--- query.py
+++ query.py
@@ -1,3 +1,6 @@
 query_schema_column="select * from information_schema.columns where table_schema='public';"
 query_schema_constraint="select constraint_name from information_schema.constraint_column_usage;"
+query_create_root_user="insert into users (username,password,type) values ('root','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','root') on conflict do nothing returning *;"
+query_rule_delete_disable_users_root="create or replace rule rule_delete_disable_users_root as on delete to users where old.id=1 or old.type='root' do instead nothing;"
+
 
