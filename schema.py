12f1346baf99a31ab75afa2552c3638a27472f52
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-14 21:53:24 +0530

    Update schema.py

--- schema.py
+++ schema.py
@@ -5,18 +5,27 @@ from datetime import datetime
 
 #signup
 class schema_signup(BaseModel):
-   username:str
-   password:str
+    username:str
+    password:str
+   
+   
 
 #login
 class schema_login(BaseModel):
-   mode:Literal["username","firebase","email","mobile"]="username"
-   username:str|None=None
-   password:str|None=None
-   firebase_id:str|None=None
-   email:str|None=None
-   mobile:str|None=None
-   otp:int|None=None
+    mode:Literal["username","firebase","email","mobile"]="username"
+    username:str|None=None
+    password:str|None=None
+    firebase_id:str|None=None
+    email:str|None=None
+    mobile:str|None=None
+    otp:int|None=None
+   
+   
+   
+   
+   
+   
+   
 
 #atom
 
