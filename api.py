8e0a6b326a5a2c223e8d21d1db9a005d20f59e23
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-14 17:04:12 +0530

    Update api.py

--- api.py
+++ api.py
@@ -22,28 +22,21 @@ async def function_api_root():
    return {"status":1,"message":"welcome to atom"}
 
 #database
-@router.get("/{x}/database-create")
-async def function_api_database_create(x:str,request:Request):
+@router.get("/{x}/database-init")
+async def function_api_database_init(x:str,request:Request):
     #token check
     if request.headers.get("token")!=config_token_root:return function_http_response(400,0,"token mismatch")
-    #table create
+    #create table
     for item in config_table:
         query=f"create table if not exists {item} (id bigint primary key generated always as identity);"
         response=await function_query_runner(postgres_object[x],"write",query,{})
         if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
-    #column create
+    #create column
     for k,v in config_column.items():
         for table in v[1]:
             query=f"alter table {table} add column if not exists {k} {v[0]};"
             response=await function_query_runner(postgres_object[x],"write",query,{})
             if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")
-    #final response
-    return {"status":1,"message":"database create done"}
-
-@router.get("/{x}/database-alter")
-async def function_api_database_alter(x:str,request:Request):
-    #token check
-    if request.headers.get("token")!=config_token_root:return function_http_response(400,0,"token mismatch")
     #read schema column
     query="select * from information_schema.columns where table_schema='public';"
     response=await function_query_runner(postgres_object[x],"read",query,{})
@@ -87,7 +80,7 @@ async def function_api_database_alter(x:str,request:Request):
                 response=await function_query_runner(postgres_object[x],"write",query,{})
                 if response["status"]==0:return function_http_response(400,0,f"error={response['message']}+{query}")    
     #final response
-    return {"status":1,"message":"database alter done"}
+    return {"status":1,"message":"database init done"}
 
 @router.get("/{x}/database-index")
 async def function_api_database_index(x:str,request:Request):
