1ebb8daf38f49a5962a1f04406b2fb8dfab3fefd
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-15 11:30:43 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 from fastapi import Request,Response
-def request_key_builder(func,namespace: str = "",*,request: Request = None,response: Response = None,**kwargs,):
+def request_key_builder(func,namespace:str="",*,request:Request=None,response:Response=None,**kwargs,):
     return ":".join([namespace,request.method.lower(),request.url.path,repr(sorted(request.query_params.items()))])
         
 async def function_query_runner(postgres_object,mode,query,values):
