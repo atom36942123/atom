66aebc3482d3f6164e0a95546382d35c64478422
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-15 10:14:49 +0530

    Update api.py

--- api.py
+++ api.py
@@ -21,6 +21,10 @@ router=APIRouter(tags=["api"])
 async def function_api_root():
    return {"status":1,"message":"welcome to atom"}
 
+@router.get("/{x}")
+async def function_api_root(x:str):
+   return {"status":1,"message":f"welcome to {x}"}
+
 #database
 @router.get("/{x}/database-init")
 async def function_api_database_init(x:str,request:Request):
