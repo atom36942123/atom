69d37dbb669b71d33e7a7a2946464ce92f35b10b
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-15 10:00:38 +0530

    Update api.py

--- api.py
+++ api.py
@@ -691,7 +691,6 @@ async def function_api_object_read_public(x:str,request:Request,table:Literal["u
    param=dict(request.query_params)
    if "tag" in param and param["tag"]:param["tag"]=param["tag"].split(",")
    param=vars(schema_atom(**param))
-   #param={"id":id,"created_by_id":created_by_id,"type":type,"username":username,"parent_table":parent_table,"parent_id":parent_id,"tag":tag,"is_pinned":is_pinned}
    #function call
    limit=30
    offset=(page-1)*limit
