c3d71af5e14be4125a23678ac6151aeb1e35f29e
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-18 10:49:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+#app
+from fastapi import FastAPI
+from lifespan import lifespan
+app=FastAPI(lifespan=lifespan,title="atom")
+
+#cors
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
+
+#middleware
+from fastapi import Request
+from function import function_http_response
+from config import config_x
+@app.middleware("http")
+async def middleware(request:Request,api_function):
+   #x check
+   if str(request.url).split("/")[3] not in ["","docs","redoc","openapi.json"]+config_x:return function_http_response(400,0,f"allowed x={str(config_x)}")
+   #api response
+   try:response=await api_function(request)
+   except Exception as e:return function_http_response(400,0,e.args)
+   #final response
+   return response
+
+#router
+from api import router
+app.include_router(router)
+from zzz import router
+app.include_router(router)
+
 #server start
 from app import app
 from function import function_server_start
