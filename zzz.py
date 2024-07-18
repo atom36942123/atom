445bb1b61ea71c847e4c5ffcec58fe680f533df1
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-18 12:17:01 +0530

    Update zzz.py

--- zzz.py
+++ zzz.py
@@ -1,55 +1,39 @@
-#router
-from fastapi import APIRouter
-router=APIRouter(tags=["zzz"])
-
-
-
-
 #elasticsearch
-from object import elasticsearch_object
 from fastapi import Body
-from function import function_http_response
+from elasticsearch import Elasticsearch
+
+from config import config_elasticsearch_cloud_id
+from config import config_elasticsearch_username
+from config import config_elasticsearch_password
+
+if False:
+   try:elasticsearch_object=Elasticsearch(cloud_id=cloud_id,basic_auth=(username,password))
+   except Exception as e:print(e)
 @router.post("/{x}/elasticsearch-create-object")
 async def function_api(x:str,table:str,id:int,body:dict=Body(...)):
     try:response=elasticsearch_object.index(index=table,id=id,document=body)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
-
-
-#elasticsearch
-from elasticsearch import Elasticsearch
-from config import config_elasticsearch_cloud_id
-from config import config_elasticsearch_username
-from config import config_elasticsearch_password
-elasticsearch_object=None
-if False:
-   try:elasticsearch_object=Elasticsearch(cloud_id=config_elasticsearch_cloud_id,basic_auth=(config_elasticsearch_username,config_elasticsearch_password))
-   except Exception as e:print(e)
-
 @router.get("/{x}/elasticsearch-read-object")
 async def function_api(x:str,table:str,id:int):
     try:response=elasticsearch_object.get(index=table,id=id)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
-
 @router.put("/{x}/elasticsearch-update-object")
 async def function_api(x:str,table:str,id:int,body:dict=Body(...)):
     try:response=elasticsearch_object.update(index=table,id=id,doc=body)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
-
 @router.delete("/{x}/elasticsearch-delete-object")
 async def function_api(x:str,table:str,id:int):
     try:response=elasticsearch_object.delete(index=table,id=id)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
-
 @router.get("/{x}/elasticsearch-refresh-table")
 async def function_api(x:str,table:str):
     try:response=elasticsearch_object.indices.refresh(index=table)
     except Exception as e:return function_http_response(400,0,e.args)
     return response
-
 @router.get("/{x}/elasticsearch-search")
 async def function_api(x:str,table:str,column:str,keyword:str):
     query={"query":{"match":{column:keyword}},"size":30}
