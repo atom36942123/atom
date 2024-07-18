cd83446f7513eef46e12306a4f75b3fd230ea20f
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-18 10:50:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,5 @@ from zzz import router
 app.include_router(router)
 
 #server start
-from app import app
 from function import function_server_start
 if __name__=="__main__":function_server_start(app,"0.0.0.0",8000)
