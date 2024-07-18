b7a8ca46f6d6ef565da37fb4f3fc8093e14a7db7
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-18 11:59:53 +0530

    Update object.py

--- object.py
+++ object.py
@@ -1,18 +1,2 @@
 
-#mongo
-from config import config_mongo_url
-import motor.motor_asyncio
-mongo_object=None
-if False:
-   try:mongo_object=motor.motor_asyncio.AsyncIOMotorClient(config_mongo_url)
-   except Exception as e:print(e)
 
-#elasticsearch
-from elasticsearch import Elasticsearch
-from config import config_elasticsearch_cloud_id
-from config import config_elasticsearch_username
-from config import config_elasticsearch_password
-elasticsearch_object=None
-if False:
-   try:elasticsearch_object=Elasticsearch(cloud_id=config_elasticsearch_cloud_id,basic_auth=(config_elasticsearch_username,config_elasticsearch_password))
-   except Exception as e:print(e)
