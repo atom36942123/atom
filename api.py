7b89fed156136d138a6701a57aca4a8b4b92f5dd
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-18 14:08:01 +0530

    Update api.py

--- api.py
+++ api.py
@@ -385,7 +385,7 @@ async def function_api_login(x:str,request:Request,body:schema_atom):
          user=response["message"][0]
    #token encode
    data={"x":x,"id":user["id"],"is_active":user["is_active"],"type":user["type"]}
-   response=await function_token_encode(data,config_jwt_secret_key)
+   response=await function_token_encode(config_jwt_secret_key,data)
    if response["status"]==0:return function_http_response(400,0,response["message"])
    #final response
    return response
@@ -407,7 +407,7 @@ async def function_api_token_refresh(x:str,request:Request):
    user=response["message"][0]
    #token encode
    data={"x":x,"id":user["id"],"is_active":user["is_active"],"type":user["type"]}
-   response=await function_token_encode(data,config_jwt_secret_key)
+   response=await function_token_encode(config_jwt_secret_key,data)
    if response["status"]==0:return function_http_response(400,0,response["message"])
    #final response
    return response
