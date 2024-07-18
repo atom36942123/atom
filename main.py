e14d38fed8e11be3dc23e91a5eeee2b8338dbca0
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-18 09:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #server start
 from app import app
-from config import config_backend_host,config_backend_port
 from function import function_server_start
 if __name__=="__main__":
    function_server_start(app,config_backend_host,config_backend_port)
