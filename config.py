e6823bf9d150a82d0a3bdbd5fcfa2f1f480bb3a8
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-18 15:07:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-#env
-from environs import Env
-env=Env()
-env.read_env()
-
 #aws
 config_aws_access_key_id=env("aws_access_key_id")
 config_aws_secret_access_key=env("aws_secret_access_key")
@@ -11,7 +6,4 @@ config_aws_s3_bucket_name=env("aws_s3_bucket_name")
 config_aws_ses_region=env("aws_ses_region")
 config_aws_ses_sender=env("aws_ses_sender")
 
-#misc
-config_jwt_secret_key=env("jwt_secret_key")
-config_token_root=env("token_root")
 
