75052fcdfe90be4562123e8b424e4e08072feb05
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-18 09:40:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #server start
 from app import app
 from function import function_server_start
-if __name__=="__main__":function_server_start(app,0.0.0.0,8000)
+if __name__=="__main__":function_server_start(app,"0.0.0.0",8000)
