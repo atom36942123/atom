b4ca36bd06b864e1c484d751c9fbd5760796c01a
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-19 21:48:08 +0530

    Update function.py

--- function.py
+++ function.py
@@ -199,17 +199,32 @@ async def function_drop_all_index(postgres_object,function_query_runner):
    return {"status":1,"message":"done"}
 
 async def function_drop_all_view(postgres_object,function_query_runner):
-   #logic
-   drop_all_view_get_query='''
-   select 'drop view if exists ' || string_agg (table_name, ', ') || ' cascade;' as output 
-   from information_schema.views 
-   where table_schema not in ('pg_catalog', 'information_schema') and table_name !~ '^pg_';
-   '''
+   drop_all_view_get_query='''select 'drop view if exists ' || string_agg (table_name, ', ') || ' cascade;' as output from information_schema.views where table_schema not in ('pg_catalog', 'information_schema') and table_name !~ '^pg_';'''
    response=await function_query_runner(postgres_object,"read",drop_all_view_get_query,{})
    if response["status"]==0:return response
    drop_all_query=response["message"][0]["output"]
    if drop_all_query:
       response=await function_query_runner(postgres_object,"write",drop_all_query,{})
       if response["status"]==0:return response
-   #final response
+   return {"status":1,"message":"done"}
+
+async def function_delete_object_abandon(postgres_object,function_query_runner):
+   #logic
+   query_dict={
+   "delete_post_creator_null":"delete from post where id in (select x.id from post as x left join users as y on x.created_by_id=y.id where x.created_by_id is not null and y.id is null);",
+   "delete_likes_creator_null":"delete from likes where id in (select x.id from likes as x left join users as y on x.created_by_id=y.id where x.created_by_id is not null and y.id is null);",
+   "delete_bookmark_creator_null":"delete from bookmark where id in (select x.id from bookmark as x left join users as y on x.created_by_id=y.id where x.created_by_id is not null and y.id is null);",
+   "delete_report_creator_null":"delete from report where id in (select x.id from report as x left join users as y on x.created_by_id=y.id where x.created_by_id is not null and y.id is null);",
+   "delete_comment_creator_null":"delete from comment where id in (select x.id from comment as x left join users as y on x.created_by_id=y.id where x.created_by_id is not null and y.id is null);",
+   "delete_rating_creator_null":"delete from rating where id in (select x.id from rating as x left join users as y on x.created_by_id=y.id where x.created_by_id is not null and y.id is null);",
+   "delete_block_creator_null":"delete from block where id in (select x.id from block as x left join users as y on x.created_by_id=y.id where x.created_by_id is not null and y.id is null);",
+   "delete_likes_parent_null_post":"delete from likes where id in (select x.id from likes as x left join post as y on x.parent_id=y.id where x.parent_table='post' and y.id is null);",
+   "delete_bookmark_parent_null_post":"delete from bookmark where id in (select x.id from bookmark as x left join post as y on x.parent_id=y.id where x.parent_table='post' and y.id is null);",
+   "delete_comment_parent_null_post":"delete from comment where id in (select x.id from comment as x left join post as y on x.parent_id=y.id where x.parent_table='post' and y.id is null);",
+   "delete_duplicate_tag":"delete from atom as a1 using atom as a2 where a1.type='tag' and a2.type='tag' and a1.ctid>a2.ctid and a1.title=a2.title;",
+   "delete_message_old":"delete from message where created_at<now()-interval '30 days';",
+   }
+   for k,v in query_dict.items():
+      response=await function_query_runner(request.state.postgres_object,"write",v,{})
+      if response["status"]==0:return response
    return {"status":1,"message":"done"}
