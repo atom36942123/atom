02ebb408748ef9a70e1aec8c6190be295db799e8
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-19 08:48:21 +0530

    Update api.py

--- api.py
+++ api.py
@@ -7,13 +7,6 @@ from environs import Env
 env=Env()
 env.read_env()
 
-config_aws_access_key_id=env("aws_access_key_id")
-config_aws_secret_access_key=env("aws_secret_access_key")
-config_aws_s3_bucket_region=env("aws_s3_bucket_region")
-config_aws_s3_bucket_name=env("aws_s3_bucket_name")
-config_aws_ses_region=env("aws_ses_region")
-config_aws_ses_sender=env("aws_ses_sender")
-
 #import
 from function import *
 from fastapi import Request,BackgroundTasks,Depends,Body,File,UploadFile
