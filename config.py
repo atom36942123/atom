b904b2a1aec50476b366d5a206ca757bcbb78f58
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-19 08:40:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 #aws
-config_aws_access_key_id=env("aws_access_key_id")
-config_aws_secret_access_key=env("aws_secret_access_key")
-config_aws_s3_bucket_region=env("aws_s3_bucket_region")
-config_aws_s3_bucket_name=env("aws_s3_bucket_name")
-config_aws_ses_region=env("aws_ses_region")
-config_aws_ses_sender=env("aws_ses_sender")
+
 
 
