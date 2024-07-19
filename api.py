2a9d8e7c0e6ca0acbe0ee300577a60ef546a8be7
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-19 08:57:43 +0530

    Update api.py

--- api.py
+++ api.py
@@ -676,10 +676,7 @@ async def function_api_object_delete(x:str,request:Request,table:str,id:int,back
    if response["status"]==0:return function_http_response(400,0,response["message"])
    #background task
    for item in ["likes","bookmark","comment","rating","block","report"]:background_tasks.add_task(function_query_runner,request.state.postgres_object,"write",f"delete from {item} where parent_table=:parent_table and parent_id=:parent_id;",{"parent_table":table,"parent_id":id})
-   #delete s3
-   if "file_url" in object and object["file_url"]:
-      for url in object["file_url"].split(","):
-         if config_aws_s3_bucket_name in url:background_tasks.add_task(function_s3_delete_url,config_aws_access_key_id,config_aws_secret_access_key,config_aws_s3_bucket_name,url)
+   if "file_url" in object:background_tasks.add_task(function_s3_delete_url,env.list("aws")[0],env.list("aws")[1],env.list("s3")[0],object["file_url"])
    #final response
    return {"status":1,"message":"object deleted"}
 
