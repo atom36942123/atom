590cb79acec523cea3946a59a03770636ee25252
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-20 10:28:44 +0530

    Update function.py

--- function.py
+++ function.py
@@ -221,8 +221,6 @@ async def function_delete_object_abandon(postgres_object,function_query_runner):
    "delete_likes_parent_null_post":"delete from likes where id in (select x.id from likes as x left join post as y on x.parent_id=y.id where x.parent_table='post' and y.id is null);",
    "delete_bookmark_parent_null_post":"delete from bookmark where id in (select x.id from bookmark as x left join post as y on x.parent_id=y.id where x.parent_table='post' and y.id is null);",
    "delete_comment_parent_null_post":"delete from comment where id in (select x.id from comment as x left join post as y on x.parent_id=y.id where x.parent_table='post' and y.id is null);",
-   "delete_duplicate_tag":"delete from atom as a1 using atom as a2 where a1.type='tag' and a2.type='tag' and a1.ctid>a2.ctid and a1.title=a2.title;",
-   "delete_message_old":"delete from message where created_at<now()-interval '30 days';",
    }
    for k,v in query_dict.items():
       response=await function_query_runner(postgres_object,"write",v,{})
