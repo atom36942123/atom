0fb5cfe0ce7582796003dfe28a1f517b78e32dc6
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-20 10:33:30 +0530

    Update api.py

--- api.py
+++ api.py
@@ -336,7 +336,7 @@ async def function_my_profile(x:str,request:Request,background_tasks:BackgroundT
         if response["status"]==0:return function_http_response(400,0,response["message"])
         user[k]=response["message"][0]["number"]
     #background task
-    #background_tasks.add_task(function_query_runner,request.state.postgres_object,"write",f"update users set last_active_at=:last_active_at where id=:id;",{"last_active_at":datetime.now(),"id":response["message"][0]["id"]})
+    background_tasks.add_task(function_query_runner,request.state.postgres_object,"write","update users set last_active_at=:last_active_at where id=:id;",{"last_active_at":datetime.now(),"id":response["message"][0]["id"]})
     #final response
     return {"status":1,"message":user}
 
