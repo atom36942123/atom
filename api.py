1de3bb1b599974d283cc46f8733598f8834c8015
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-20 10:38:01 +0530

    Update api.py

--- api.py
+++ api.py
@@ -322,7 +322,7 @@ async def function_my_profile(x:str,request:Request,background_tasks:BackgroundT
     response=await function_query_runner(request.state.postgres_object,"read","select * from users where id=:id;",{"id":request_user["id"]})
     if response["status"]==0:return function_http_response(400,0,response["message"])
     if not response["message"]:return function_http_response(400,0,"no user exist for token passed")
-    output=response["message"][0]
+    user=response["message"][0]
     #extra key
     query_dict={
     "post_count":"select count(*) as number from post where created_by_id=:user_id;",
@@ -334,10 +334,9 @@ async def function_my_profile(x:str,request:Request,background_tasks:BackgroundT
     for k,v in query_dict.items():
         response=await function_query_runner(request.state.postgres_object,"read",v,{"user_id":request_user["id"]})
         if response["status"]==0:return function_http_response(400,0,response["message"])
-        output[k]=response["message"][0]["number"]
+        user[k]=response["message"][0]["number"]
     #background task
-    query,values="update users set last_active_at=:last_active_at where id=:id;",{"last_active_at":datetime.now(),"id":response["message"][0]["id"]}
-    background_tasks.add_task(function_query_runner,request.state.postgres_object,"write",query,values)
+    background_tasks.add_task(function_query_runner,request.state.postgres_object,"write","update users set last_active_at=:last_active_at where id=:id;",{"last_active_at":datetime.now(),"id":user["id"]})
     #final response
     return {"status":1,"message":output}
 
