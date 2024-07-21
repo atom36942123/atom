f37a22e9907b8c1015a03e3012f076aa45a174e4
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-21 08:09:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ from fastapi.encoders import jsonable_encoder
 @app.middleware("http")
 async def middleware(request:Request,api_function):
    x=str(request.url).split("/")[3]
-   if x not in ["","docs","redoc","openapi.json"]+[*postgres_object]:return JSONResponse(status_code=500,content=jsonable_encoder({"status":0,"message":f"allowed x={[*postgres_object]}"}))
+   if x not in ["","docs","redoc","openapi.json"]+[*postgres_object]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":f"x={[*postgres_object]}"}))
    if x in postgres_object:request.state.postgres_object=postgres_object[x]
    try:response=await api_function(request)
    except Exception as e:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":e.args}))
