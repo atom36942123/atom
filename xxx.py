8f25d5d434986166fb792899c1b6df8481ccd681
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-21 09:52:43 +0530

    Update xxx.py

--- xxx.py
+++ xxx.py
@@ -3,19 +3,7 @@ from environs import Env
 env=Env()
 env.read_env()
 
-#import
-from function import *
-from fastapi import Request,BackgroundTasks,Depends,Body,File,UploadFile
-from fastapi_cache.decorator import cache
-from fastapi_limiter.depends import RateLimiter
-import hashlib,json,random,csv,codecs
-from pydantic import BaseModel
-from typing import Literal
-from datetime import datetime
-import motor.motor_asyncio
-from bson import ObjectId
-from elasticsearch import Elasticsearch
-import boto3,uuid
+
 
 #schema
 class schema_atom(BaseModel):
