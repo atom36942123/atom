3ec2a4ab7e7d9f476d67953ce7a22a5520a1f541
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-21 10:01:48 +0530

    Update api.py

--- api.py
+++ api.py
@@ -8,7 +8,6 @@ env=Env()
 env.read_env()
 
 #import
-from function import *
 from fastapi import Request,BackgroundTasks,Depends,Body,File,UploadFile
 from fastapi_cache.decorator import cache
 from fastapi_limiter.depends import RateLimiter
