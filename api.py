e94e7d8d1d784a7df1867d5dffe2776d762ee6ba
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-22 18:39:00 +0530

    Update api.py

--- api.py
+++ api.py
@@ -43,11 +43,11 @@ async def function_database_init(request:Request):
          if k!="alter_query":await request.state.postgres_object.fetch_all(query=f"alter table {table} add column if not exists {k} {v[1]};",values={})
          if k!="alter_query" and v[2] is not None:await request.state.postgres_object.fetch_all(query=f"alter table {table} alter column {k} set default {v[2]};",values={})
          if k!="alter_query" and v[3] is not None and f'checkin_{k}_{table}' not in schema_constraint_name_list:await request.state.postgres_object.fetch_all(query=f"alter table {table} add constraint {f'checkin_{k}_{table}'} check ({k} in {v[3]});",values={})
-         if k!="alter_query" and v[4]==1 and v[1]=="array":await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table} using gin ({k});",values={})
-         if k!="alter_query" and v[4]==1 and v[1] not in ["array"]:await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table}({k});",values={})
+         if k!="alter_query" and v[4]==1 and "[]" in v[1]:await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table} using gin ({k});",values={})
+         if k!="alter_query" and v[4]==1 and "[]" not in v[1]:await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table}({k});",values={})
       if k=="alter_query":[await request.state.postgres_object.fetch_all(query=item,values={}) for item in v if item.split()[5] not in schema_constraint_name_list]
    return {"status":1,"message":"done"}
-
+9
 @router.post("/{x}/signup",dependencies=[Depends(RateLimiter(times=1,seconds=1))])
 async def function_signup(request:Request):
    body=await request.json()
@@ -59,40 +59,40 @@ async def function_signup(request:Request):
       
 
       
-@router.post("/{x}/login")
-async def function_login(x:str,request:Request):
-   body=await request.json()
-   #opt verify
-   await request.state.postgres_object.fetch_all(query=query,values={})}
+# @router.post("/{x}/login")
+# async def function_login(x:str,request:Request):
+#    body=await request.json()
+#    #opt verify
+#    await request.state.postgres_object.fetch_all(query=query,values={})}
    
-   if response:
-       if response["status"]==0:return function_http_response(400,0,response["message"])
-       if not response["message"]:return function_http_response(400,0,"otp not exist")
-       if response["message"][0]["otp"]!=body["otp"]:return function_http_response(400,0,"otp mismatched")
-   #read user
-   if all(item in body for item in ["username","password"]):response=await function_query_runner(request.state.postgres_object,"read","select * from users where username=:username and password=:password;",{"username":body["username"],"password":hashlib.sha256(body["password"].encode()).hexdigest()})
-   if all(item in body for item in ["firebase_id"]):respomse=await function_query_runner(request.state.postgres_object,"read","select * from users where firebase_id=:firebase_id order by id desc limit 1;",{"firebase_id":hashlib.sha256(body["firebase_id"].encode()).hexdigest()})
-   if all(item in body for item in ["email","otp"]):response=await function_query_runner(request.state.postgres_object,"read","select * from users where email=:email order by id desc limit 1;",{"email":body["email"]})
-   if all(item in body for item in ["mobile","otp"]):response=await function_query_runner(request.state.postgres_object,"read","select * from users where mobile=:mobile order by id desc limit 1;",{"mobile":body["mobile"]})
-   if response["status"]==0:return function_http_response(400,0,response["message"])
-   user=None if not response["message"] else response["message"][0]
-   #not user
-   if all(item in body for item in ["username","password"]) and not user:return function_http_response(400,0,"no such user")
-   if all(item in body for item in ["firebase_id"]) and not user:response=await function_query_runner(request.state.postgres_object,"write","insert into users (firebase_id) values (:firebase_id) returning *;",{"firebase_id":hashlib.sha256(body["firebase_id"].encode()).hexdigest()})
-   if all(item in body for item in ["email","otp"]) and not user:response=await function_query_runner(request.state.postgres_object,"write","insert into users (email) values (:email) returning *;",{"email":body["email"]})
-   if all(item in body for item in ["mobile","otp"]) and not user:response=await function_query_runner(request.state.postgres_object,"write","insert into users (mobile) values (:mobile) returning *;",{"mobile":body["mobile"]})
-   if response["status"]==0:return function_http_response(400,0,response["message"])
-   #read user
-   if not user:
-       response=await function_query_runner(request.state.postgres_object,"read","select * from users where id=:id;",{"id":response["message"]})
-       if response["status"]==0:return function_http_response(400,0,response["message"])
-       user=response["message"][0]
-   #token encode
-   data=json.dumps({"x":x,"id":user["id"],"is_active":user["is_active"],"type":user["type"]},default=str)
-   response=await function_token_encode(data,env("key"))
-   if response["status"]==0:return function_http_response(400,0,response["message"])
-   #final response
-   return response
+#    if response:
+#        if response["status"]==0:return function_http_response(400,0,response["message"])
+#        if not response["message"]:return function_http_response(400,0,"otp not exist")
+#        if response["message"][0]["otp"]!=body["otp"]:return function_http_response(400,0,"otp mismatched")
+#    #read user
+#    if all(item in body for item in ["username","password"]):response=await function_query_runner(request.state.postgres_object,"read","select * from users where username=:username and password=:password;",{"username":body["username"],"password":hashlib.sha256(body["password"].encode()).hexdigest()})
+#    if all(item in body for item in ["firebase_id"]):respomse=await function_query_runner(request.state.postgres_object,"read","select * from users where firebase_id=:firebase_id order by id desc limit 1;",{"firebase_id":hashlib.sha256(body["firebase_id"].encode()).hexdigest()})
+#    if all(item in body for item in ["email","otp"]):response=await function_query_runner(request.state.postgres_object,"read","select * from users where email=:email order by id desc limit 1;",{"email":body["email"]})
+#    if all(item in body for item in ["mobile","otp"]):response=await function_query_runner(request.state.postgres_object,"read","select * from users where mobile=:mobile order by id desc limit 1;",{"mobile":body["mobile"]})
+#    if response["status"]==0:return function_http_response(400,0,response["message"])
+#    user=None if not response["message"] else response["message"][0]
+#    #not user
+#    if all(item in body for item in ["username","password"]) and not user:return function_http_response(400,0,"no such user")
+#    if all(item in body for item in ["firebase_id"]) and not user:response=await function_query_runner(request.state.postgres_object,"write","insert into users (firebase_id) values (:firebase_id) returning *;",{"firebase_id":hashlib.sha256(body["firebase_id"].encode()).hexdigest()})
+#    if all(item in body for item in ["email","otp"]) and not user:response=await function_query_runner(request.state.postgres_object,"write","insert into users (email) values (:email) returning *;",{"email":body["email"]})
+#    if all(item in body for item in ["mobile","otp"]) and not user:response=await function_query_runner(request.state.postgres_object,"write","insert into users (mobile) values (:mobile) returning *;",{"mobile":body["mobile"]})
+#    if response["status"]==0:return function_http_response(400,0,response["message"])
+#    #read user
+#    if not user:
+#        response=await function_query_runner(request.state.postgres_object,"read","select * from users where id=:id;",{"id":response["message"]})
+#        if response["status"]==0:return function_http_response(400,0,response["message"])
+#        user=response["message"][0]
+#    #token encode
+#    data=json.dumps({"x":x,"id":user["id"],"is_active":user["is_active"],"type":user["type"]},default=str)
+#    response=await function_token_encode(data,env("key"))
+#    if response["status"]==0:return function_http_response(400,0,response["message"])
+#    #final response
+#    return response
 
 
 
