587639b3b84908277108051600c772e034a98de9
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-22 14:40:26 +0530

    Update api.py

--- api.py
+++ api.py
@@ -30,10 +30,8 @@ async def function_insert_csv(request:Request,table:str,file:UploadFile):
 
 @router.get("/{x}/database-init")
 async def function_database_init(request:Request):
-   #token check
    if request.headers.get("token")!=env("key"):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
    schema_constraint_name_list=[item["constraint_name"] for item in await request.state.postgres_object.fetch_all(query="select constraint_name from information_schema.constraint_column_usage;",values={})]
-   #logic
    for k,v in config_database.items():
       for table in v[0]:
          if len(v)!=5:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"config_databae length issue"}))
@@ -41,24 +39,9 @@ async def function_database_init(request:Request):
          if k not in ["alter_query"]:await request.state.postgres_object.fetch_all(query=f"alter table {table} add column if not exists {k} {v[1]};",values={})
          if k not in ["alter_query"] and v[2] is not None:await request.state.postgres_object.fetch_all(query=f"alter table {table} alter column {k} set default {v[2]};",values={})
          if k not in ["alter_query"] and v[3] is not None and f'checkin_{k}_{table}' not in schema_constraint_name_list:await request.state.postgres_object.fetch_all(query=f"alter table {table} add constraint {f'checkin_{k}_{table}'} check ({k} in {v[3]});",values={})
-         
-         if k in ["alter_query"]    await request.state.postgres_object.fetch_all(query=item,values={}) for item in alter_query if item.split()[5] not in schema_constraint_name_list
-         
-         
-      
-   
-   
-   
-   #index
-   output=await request.state.postgres_object.fetch_all(query='''select 'drop index ' || string_agg(i.indexrelid::regclass::text,', ' order by n.nspname,i.indrelid::regclass::text, cl.relname) as output from pg_index i join pg_class cl ON cl.oid = i.indexrelid join pg_namespace n ON n.oid = cl.relnamespace left join pg_constraint co ON co.conindid = i.indexrelid where  n.nspname <> 'information_schema' and n.nspname not like 'pg\_%' and co.conindid is null and not i.indisprimary and not i.indisunique and not i.indisexclusion and not i.indisclustered and not i.indisreplident;''',values={})
-   if output[0]["output"]:await request.state.postgres_object.fetch_all(query=output[0]["output"],values={})
-   for k,v in config_database.items():
-      for table in v[0]:
-         if v[4]==1:
-            if v[1]=="array":await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table} using gin ({k});",values={})
-            else:await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table}({k});",values={})
-   [await request.state.postgres_object.fetch_all(query=item,values={}) for item in ["create index if not exists index_parent_table_parent_id_likes on likes(parent_table,parent_id);","create index if not exists index_parent_table_parent_id_bookmark on bookmark(parent_table,parent_id);","create index if not exists index_parent_table_parent_id_comment on comment(parent_table,parent_id);"]]
-   #final response
+         if k in ["alter_query"]:[await request.state.postgres_object.fetch_all(query=item,values={}) for item in alter_query if item.split()[5] not in schema_constraint_name_list]
+         if k not in ["alter_query"] and v[4]==1 and v[1]=="array":await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table} using gin ({k});",values={})
+         if k not in ["alter_query"] and v[4]==1 and v[1] not in ["array"]:await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table}({k});",values={})
    return {"status":1,"message":"done"}
 
 @router.post("/{x}/signup",dependencies=[Depends(RateLimiter(times=1,seconds=1))])
