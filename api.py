774345594fd4d214870a56d2d38a0016a6c43fcc
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-22 17:09:55 +0530

    Update api.py

--- api.py
+++ api.py
@@ -28,19 +28,35 @@ async def function_insert_csv(request:Request,table:str,file:UploadFile):
    file.file.close   
    return {"status":1,"message":await request.state.postgres_object.execute_many(query=f"insert into {table} (created_by_id,type,title,description,file_url,link_url,tag) values (:created_by_id,:type,:title,:description,:file_url,:link_url,:tag) returning *;",values=values)}
 
+# @router.get("/{x}/database-init")
+# async def function_database_init(request:Request):
+#    if request.headers.get("token")!=env("key"):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
+#    schema_constraint_name_list=[item["constraint_name"] for item in await request.state.postgres_object.fetch_all(query="select constraint_name from information_schema.constraint_column_usage;",values={})]
+#    for k,v in config_database.items():
+#       if k!="alter_query" and len(v)!=5:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":f"config_databae length issue-{k}"}))
+#       if k=="created_at":[await request.state.postgres_object.fetch_all(query=f"create table if not exists {table} (id bigint primary key generated always as identity);",values={}) for table in v[0]]
+#       if k!="alter_query":[await request.state.postgres_object.fetch_all(query=f"alter table {table} add column if not exists {k} {v[1]};",values={}) for table in v[0]]
+#       if k!="alter_query" and v[2] is not None:[await request.state.postgres_object.fetch_all(query=f"alter table {table} alter column {k} set default {v[2]};",values={}) for table in v[0]]
+#       if k!="alter_query" and v[3] is not None and f'constraint_checkin_{k}_{table}' not in schema_constraint_name_list:[await request.state.postgres_object.fetch_all(query=f"alter table {table} add constraint {f'constraint_checkin_{k}_{table}'} check ({k} in {v[3]});",values={}) for table in v[0]]
+#       if k!="alter_query" and v[4]==1 and v[1]=="array":[await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table} using gin ({k});",values={}) for table in v[0]]
+#       if k!="alter_query" and v[4]==1 and v[1] not in ["array"]:[await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table}({k});",values={}) for table in v[0]]
+#       if k=="alter_query":[await request.state.postgres_object.fetch_all(query=query,values={}) for query in v if query.split()[5] not in schema_constraint_name_list]
+#    return {"status":1,"message":"done"}
+
 @router.get("/{x}/database-init")
 async def function_database_init(request:Request):
    if request.headers.get("token")!=env("key"):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
    schema_constraint_name_list=[item["constraint_name"] for item in await request.state.postgres_object.fetch_all(query="select constraint_name from information_schema.constraint_column_usage;",values={})]
    for k,v in config_database.items():
-      if k!="alter_query" and len(v)!=5:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":f"config_databae length issue-{k}"}))
+      if k!="alter_query" and len(v)!=5:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":f"config_databae length issue {k}"}))
       if k=="created_at":[await request.state.postgres_object.fetch_all(query=f"create table if not exists {table} (id bigint primary key generated always as identity);",values={}) for table in v[0]]
-      if k!="alter_query":[await request.state.postgres_object.fetch_all(query=f"alter table {table} add column if not exists {k} {v[1]};",values={}) for table in v[0]]
-      # if k!="alter_query" and v[2] is not None:[await request.state.postgres_object.fetch_all(query=f"alter table {table} alter column {k} set default {v[2]};",values={}) for table in v[0]]
-      # if k!="alter_query" and v[3] is not None and f'constraint_checkin_{k}_{table}' not in schema_constraint_name_list:[await request.state.postgres_object.fetch_all(query=f"alter table {table} add constraint {f'constraint_checkin_{k}_{table}'} check ({k} in {v[3]});",values={}) for table in v[0]]
-      # if k!="alter_query" and v[4]==1 and v[1]=="array":[await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table} using gin ({k});",values={}) for table in v[0]]
-      # if k!="alter_query" and v[4]==1 and v[1] not in ["array"]:[await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table}({k});",values={}) for table in v[0]]
-      # if k=="alter_query":[await request.state.postgres_object.fetch_all(query=query,values={}) for query in v if query.split()[5] not in schema_constraint_name_list]
+      for table in v[0]:
+         if k!="alter_query":await request.state.postgres_object.fetch_all(query=f"alter table {table} add column if not exists {k} {v[1]};",values={})
+         if k!="alter_query" and v[2] is not None:await request.state.postgres_object.fetch_all(query=f"alter table {table} alter column {k} set default {v[2]};",values={})
+         if k!="alter_query" and v[3] is not None and f'checkin_{k}_{table}' not in schema_constraint_name_list:await request.state.postgres_object.fetch_all(query=f"alter table {table} add constraint {f'checkin_{k}_{table}'} check ({k} in {v[3]});",values={})
+         if k!="alter_query" and v[4]==1 and v[1]=="array":await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table} using gin ({k});",values={})
+         if k!="alter_query" and v[4]==1 and v[1] not in ["array"]:await request.state.postgres_object.fetch_all(query=f"create index if not exists {f'index_{k}_{table}'} on {table}({k});",values={})
+      if k=="alter_query":[await request.state.postgres_object.fetch_all(query=item,values={}) for item in v if item.split()[5] not in schema_constraint_name_list]
    return {"status":1,"message":"done"}
 
 @router.get("/{x}/metric")
