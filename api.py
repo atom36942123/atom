323337cfa7e696afbece6c76230fe5b6f9776c21
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-23 10:16:33 +0530

    Update api.py

--- api.py
+++ api.py
@@ -14,13 +14,8 @@ async def function_query_runner(request:Request,query:str):
 
 @router.post("/{x}/insert-csv")
 async def function_insert_csv(request:Request,table:str,file:UploadFile):
-   #prework
    if request.headers.get("token")!=env("key"):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
-   # if file.content_type!="text/csv":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"only csv allowed"}))
-   if file.size>=100000:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"file size should be<=100000 bytes"}))
-   #logic
    file_object=csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
-   if set(file_object.fieldnames)!=set(["created_by_id","type","title","description","file_url","link_url","tag"]):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"csv column mismatch-ct2t"}))
    values=[]
    for row in file_object:
       row["created_by_id"]=int(row["created_by_id"]) if row["created_by_id"] else None
