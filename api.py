bc9b21b899eac3a34b6894ac948d7d3c2e65a689
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-23 09:32:32 +0530

    Update api.py

--- api.py
+++ api.py
@@ -126,7 +126,7 @@ async def function_my_profile(request:Request,background_tasks:BackgroundTasks):
    #background task
    background_tasks.add_task(await request.state.postgres_object.fetch_all(query="update users set last_active_at=:last_active_at where id=:id;",values={"id":user["id"],"last_active_at":datetime.now()}))
    #response
-   return {"status":1,"message":user|output}
+   return {"status":1,"message":user}
 
 
    
