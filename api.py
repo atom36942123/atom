9445b9e4365059de587a37f2083098dd9d075800
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-23 12:41:41 +0530

    Update api.py

--- api.py
+++ api.py
@@ -35,10 +35,10 @@ async def function_query_runner(request:Request,query:str):
 async def function_database_init(request:Request):
    #prework
    if request.headers.get("token")!=env("key"):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
-   x={}
-   for k,v in config_database.items():
-      x[k]=[v[1],v[2],v[0]]
-   return x
+   # x={}
+   # for k,v in config_database.items():
+   #    x[k]=[v[1],v[2],v[0]]
+   return config_database
    schema_constraint_name_list=[item["constraint_name"] for item in await request.state.postgres_object.fetch_all(query="select constraint_name from information_schema.constraint_column_usage;",values={})]
    #logic
    for k,v in config_database.items():
