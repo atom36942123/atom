fce4112176fcf7d656a0bb89f143435f09387cab
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-24 20:42:50 +0530

    Update api.py

--- api.py
+++ api.py
@@ -217,6 +217,24 @@ async def function_my_read_parent(request:Request,table:str,parent_table:str,pag
    #final response
    return {"status":1,"message":output}
 
+@router.get("/{x}/my-action-check")
+async def function_my_action_check(request:Request,action:str,table:str,ids:str):
+   #token check
+   user=json.loads(jwt.decode(request.headers.get("token"),env("key"),algorithms="HS256")["data"])
+   if user["x"]!=str(request.url).split("/")[3]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x issue"}))
+   #logic
+   ids=[int(x) for x in ids.split(',')]
+   output=await request.state.postgres_object.fetch_all(query=f"select parent_id from {action} join unnest(array{ids}::int[]) with ordinality t(parent_id, ord) using (parent_id) where parent_table=:parent_table and created_by_id=:created_by_id;",values={"parent_table":table,"created_by_id":request_user["id"]})
+   ids_filtered=list(set([item["parent_id"] for item in response["message"] if item["parent_id"]]))
+   #final response
+   return {"status":1,"message":ids_filtered}
+
+
+
+
+    
+
+
 
 
    
