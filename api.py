36f0247bde43c0bb3532234adc3d7020d31f3bd9
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-24 11:56:17 +0530

    Update api.py

--- api.py
+++ api.py
@@ -138,8 +138,8 @@ async def function_my_message_inbox(request:Request,page:int,is_unread:int=None,
    for user_column in ["created_by_id","received_by_id"]: 
       output_user=await request.state.postgres_object.fetch_all(query=f"select * from users where id in ({','.join([str(item[user_column]) for item in output if item[user_column]])});",values={})
       for object in output:
-         object[user_column.replace("id","username")],object[user_column.replace("id","profile_pic_url")]=None,None
-         for user in  output_user:
+         for user in output_user:
+            object[user_column.replace("id","username")],object[user_column.replace("id","profile_pic_url")]=None,None
             if object[user_column]==user["id"]:object[user_column.replace("id","username")],object[user_column.replace("id","profile_pic_url")]=user["username"],user["profile_pic_url"]
    #response
    return {"status":1,"message":output}
