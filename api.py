9788b7072789dc7df4360d51ff9fed4b6cbae268
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-24 11:07:47 +0530

    Update api.py

--- api.py
+++ api.py
@@ -137,7 +137,6 @@ async def function_my_message_inbox(request:Request,page:int,is_unread:int=None,
    #add user key
    for user_column in ["created_by_id","received_by_id"]:
       output_user=await request.state.postgres_object.fetch_all(query=f"select * from users join unnest(array{list(set([item[user_column] for item in output if item[user_column]]))}::int[]) with ordinality t(id, ord) using (id) order by t.ord;",values={})
-      return output_user
       for object in output:
          object[user_column.replace("id","username")],object[user_column.replace("id","profile_pic_url")]=None,None
          for user in  output_user:
