bb1d3ceef585a18a80159116a558ffe30411a45b
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-24 13:27:48 +0530

    Update api.py

--- api.py
+++ api.py
@@ -137,6 +137,7 @@ async def function_my_message_inbox(request:Request,page:int,is_unread:int=None,
    #add user key
    for user_column in ["created_by_id","received_by_id"]:
       output_user=await request.state.postgres_object.fetch_all(query=f"select * from users where id in ({','.join([str(item[user_column]) for item in output if item and item[user_column]])});",values={})
+      return output_user
       for object in output:
          object[f"{user_column}_username"],object[f"{user_column}_profile_pic_url"]=None,None
          for user in output_user:
