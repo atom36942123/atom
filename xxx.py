66686f132d009102a0e7a2d7388a7b59af395b7d
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-25 09:54:54 +0530

    Update xxx.py

--- xxx.py
+++ xxx.py
@@ -1,36 +1,6 @@
 
 
-@router.put("/{x}/object-update/{table}/{id}")
-async def function_object_update(request:Request,table:str,id:int,body:schema_atom):
-   #token check
-   response=await function_token_decode(request,env("key"))
-   if response["status"]==0:return function_http_response(400,0,response["message"])
-   request_user=response["message"]
-   #refresh request_user
-   response=await function_query_runner(request.state.postgres_object,"read","select * from users where id=:id;",{"id":request_user["id"]})
-   if response["status"]==0:return function_http_response(400,0,response["message"])
-   if not response["message"]:return function_http_response(400,0,"no user for token passed")
-   request_user=response["message"][0]
-   #param
-   param={k: v for k, v in vars(body).items() if v not in [None,""," "]}
-   if not param:return function_http_response(400,0,"body cant be null")
-   if "metadata" in param:param["metadata"]=json.dumps(param["metadata"],default=str)
-   if "number" in param:param["number"]=round(param["number"],5)
-   param["updated_at"],param["updated_by_id"]=datetime.now(),request_user["id"]
-   #non admin case
-   id,created_by_id=id,None
-   if request_user["type"] not in ["root","admin"]:
-      if table=="users":id,created_by_id=request_user['id'],None
-      else:id,created_by_id=id,request_user['id']
-      for item in ["created_at","created_by_id","received_by_id","is_active","is_verified","type"]:param.pop(item,None)
-   #logic
-   key=""
-   for k,v in param.items():key=key+f"{k}=coalesce(:{k},{k}) ,"
-   query=f"update {table} set {key.strip().rsplit(',', 1)[0]} where id=:id and (created_by_id=:created_by_id or :created_by_id is null) returning *;"
-   response=await function_query_runner(request.state.postgres_object,"write",query,param|{"id":id,"created_by_id":created_by_id})
-   if response["status"]==0:return function_http_response(400,0,response["message"])
-   #final response
-   return response
+
 
 @router.delete("/{x}/object-delete/{table}/{id}")
 async def function_object_delete(request:Request,table:str,id:int,background_tasks:BackgroundTasks):
