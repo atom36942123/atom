f970517d6c6cfea3cff0e785914a737f637eb629
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-26 13:17:08 +0530

    Update api.py

--- api.py
+++ api.py
@@ -55,7 +55,7 @@ async def function_database(request:Request):
 async def function_insert(request:Request,table:str,file:UploadFile):
    #prework
    if request.headers.get("token")!=env("key"):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
-   schema_column_datatype={item["column_name"]:item["datatype"] for item in await request.state.postgres_object.fetch_all(query="select column_name,count(*),max(data_type) as data_type from information_schema.columns where table_schema='public' group by  column_name order by count desc;",values={})}
+   schema_column_datatype={item["column_name"]:item["datatype"] for item in await request.state.postgres_object.fetch_all(query="select column_name,count(*),max(data_type) as datatype from information_schema.columns where table_schema='public' group by  column_name order by count desc;",values={})}
    file_object=csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
    file_column_name_list=list(set(file_object.fieldnames))
    #logic
