bff91f02afd51ddec12115b918a1ac86ea1f4ba9
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-27 13:20:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ async def function_insert(request:Request,file:UploadFile):
    schema_column_datatype={item["column_name"]:item["datatype"] for item in await request.state.postgres_object.fetch_all(query="select column_name,count(*),max(data_type) as datatype from information_schema.columns where table_schema='public' group by  column_name order by count desc;",values={})}
    file_object=csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
    file_column_name_list=file_object.fieldnames
-   return file_object.filename
+   return file.filename
    #logic
    values=[]
    for row in file_object:
