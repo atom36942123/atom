6e9054fec8206770bbb4a245a681f6bd9ae7d551
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-27 13:24:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,6 +110,7 @@ async def function_insert(request:Request,file:UploadFile):
    if request.headers.get("token")!=env("key"):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
    schema_column_datatype={item["column_name"]:item["datatype"] for item in await request.state.postgres_object.fetch_all(query="select column_name,count(*),max(data_type) as datatype from information_schema.columns where table_schema='public' group by  column_name order by count desc;",values={})}
    file_object=csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
+   return file.filename.split(".")[0]
    #logic
    values=[]
    for row in file_object:
