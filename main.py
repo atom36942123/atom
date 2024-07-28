846b8bd589b3003ceac59e19ba65e462ab5bdd3f
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-28 12:19:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -253,6 +253,7 @@ async def function_feed(request:Request):
    where="where "
    for k,v in param.items():where=where+f"({k} {body[f'{k}_operator']} :{k} or :{k} is null) and " if f"{k}_operator" in body else where+f"({k} = :{k} or :{k} is null) and "
    where=where.strip().rsplit('and',1)[0]
+   return where
    #logic
    output=await request.state.postgres_object.fetch_all(query=f"select * from {body['table']} {where} order by id desc limit :limit offset :offset;",values=param|{"limit":body['limit'],"offset":(body['page']-1)*body['limit']})
    #final
