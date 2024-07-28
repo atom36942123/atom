bd12b9bf687786011dbecba951fcdcacb718662e
atom <102727104+atom36942123@users.noreply.github.com>
2024-07-28 12:14:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ async def function_object(request:Request,background:BackgroundTasks):
 
 def function_redis_key_builder(func,namespace:str="",*,request:Request=None,response:Response=None,**kwargs):return ":".join([namespace,request.method.lower(),request.url.path,repr(sorted(request.query_params.items())),])
 @app.get("/{x}/feed")
-@cache(expire=60,key_builder=function_redis_key_builder)
+@cache(expire=1,key_builder=function_redis_key_builder)
 async def function_feed(request:Request):
    #prework
    body=dict(request.query_params)
