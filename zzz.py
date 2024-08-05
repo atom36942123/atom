c1becc2cda44088b70558bbac74448986119ad3b
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 20:59:11 +0530

    Update zzz.py

--- zzz.py
+++ zzz.py
@@ -11,17 +11,7 @@ from bson import ObjectId
 from elasticsearch import Elasticsearch
 
 
-@app.get("/{x}/qrunner")
-async def function_qrunner(request:Request,query:str):
-   #prework
-   database=request.state.postgres_object.fetch_all
-   if request.headers.get("token")!=config_key:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
-   #logic
-   query=query
-   values={}
-   output=await database(query=query,values=values)
-   #final
-   return output
+
     
 @app.get("/{x}/database")
 async def function_database(request:Request):
