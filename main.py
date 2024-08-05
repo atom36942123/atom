a37a2b0d929257063dd0a1b5eb593d7fbfc64447
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 16:02:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,7 @@
 
 
 
-#middleware
-from fastapi import Request
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-import traceback
-@app.middleware("http")
-async def middleware(request:Request,api_function):
-   #x check
-   x=str(request.url.path).split("/")[1]
-   if x not in ["","docs","redoc","openapi.json"]+[*postgres_object]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"wrong x"}))
-   #database assgin
-   if x in postgres_object:request.state.postgres_object=postgres_object[x]
-   #api response
-   try:response=await api_function(request)
-   except Exception as e:
-      print(traceback.format_exc())
-      return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":e.args}))
-   #final
-   return response
+
 
 #api import
 from fastapi import Request,Response,BackgroundTasks,Depends,Body,File,UploadFile
