bfcb32d40372f7bedb767749dae84ec53a3a7016
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 16:26:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from app import app
+import uvicorn
+import asyncio
+
+if __name__=="__main__":
+  uvicorn_object=uvicorn.Server(config=uvicorn.Config(app,"0.0.0.0",8000,workers=16,log_level="info",reload=False,lifespan="on",loop="asyncio"))
+  loop=asyncio.new_event_loop()
+  asyncio.set_event_loop(loop)
+  loop.run_until_complete(uvicorn_object.serve())
 
