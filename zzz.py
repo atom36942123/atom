59954aba6fb8a39df7b7163efee2efdcb4569b68
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 16:25:24 +0530

    Update zzz.py

--- zzz.py
+++ zzz.py
@@ -688,11 +688,3 @@ async def function_elasticsearch(request:Request,mode:str):
       response=elasticsearch_object.search(index=table,body={"query":{"match":{column:body["keyword"]}},"size":body["size"]})
    #final
    return response
-   
-#server start
-import uvicorn,asyncio
-if __name__=="__main__":
-   uvicorn_object=uvicorn.Server(config=uvicorn.Config(app,"0.0.0.0",8000,workers=16,log_level="info",reload=False,lifespan="on",loop="asyncio"))
-   loop=asyncio.new_event_loop()
-   asyncio.set_event_loop(loop)
-   loop.run_until_complete(uvicorn_object.serve())
