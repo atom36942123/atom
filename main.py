04a9a26ff97736e8d50bc1187f800fa649573439
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 15:40:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#env read
-from environs import Env
-env=Env()
-env.read_env()
-
-#config
-config_postgres_url=env("postgres_url")
-config_key=env("key")
-config_key_jwt=env("key_jwt")
-config_aws_access_key_id=env("aws_access_key_id")
-config_aws_secret_access_key=env("aws_secret_access_key")
-config_s3_bucket=env("s3_bucket")
-config_s3_region=env("s3_region")
-config_ses_sender=env("ses_sender")
-config_ses_region=env("ses_region")
-
 #database
 from databases import Database
 postgres_object={item.split("/")[-1]:Database(item,min_size=1,max_size=100) for item in config_postgres_url.split(",")}
