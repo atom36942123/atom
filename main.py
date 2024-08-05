068d8b6ab7dd57947a6c90854e581b2de90091c8
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 16:40:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from app import app
 import uvicorn
 import asyncio
 
-import os
-os.system("redis-cli ping")
-
 if __name__=="__main__":
   uvicorn_object=uvicorn.Server(config=uvicorn.Config(app,"0.0.0.0",8000,workers=16,log_level="info",reload=False,lifespan="on",loop="asyncio"))
   loop=asyncio.new_event_loop()
