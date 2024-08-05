9e11ef704f4747e85897e0a059b4717f438b3f64
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 22:04:07 +0530

    Update and rename api_core.py to api.py

--- api.py
+++ api.py
@@ -2,6 +2,18 @@
 from fastapi import APIRouter
 router = APIRouter()
 
+#import
+from fastapi import Request,Response,BackgroundTasks,Depends,Body,File,UploadFile
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi_cache.decorator import cache
+from fastapi_limiter.depends import RateLimiter
+import hashlib,json,random,csv,codecs,jwt,time,boto3,uuid
+from datetime import datetime,timedelta
+import motor.motor_asyncio
+from bson import ObjectId
+from elasticsearch import Elasticsearch
+
 #api
 @app.get("/{x}/qrunner")
 async def function_qrunner(request:Request,query:str):
