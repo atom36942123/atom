1c7e36c4dd05a97c9b9fcad5a6314b5f2e25958d
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 15:47:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,8 @@ from environs import Env
 env=Env()
 env.read_env()
 
+#postgres object
+from databases import Database
+postgres_object={item.split("/")[-1]:Database(item,min_size=1,max_size=100) for item in env("postgres_url").split(",")}
+
 
