22bcd4262dc9e5c70439333f80f426b7bafaa2e2
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-05 16:23:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
+#app import
+from config import config_redis_url
+from config import config_postgres_object
+
 #lifespan
-
-
-
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from redis import asyncio as aioredis
+from fastapi_limiter import FastAPILimiter
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+@asynccontextmanager
+async def function_lifespan(app:FastAPI):
+   #redis rate limiter
+   await FastAPILimiter.init(aioredis.from_url(config_redis_url,encoding="utf-8",decode_responses=True))
+   #redis cache
+   FastAPICache.init(RedisBackend(aioredis.from_url(config_redis_url)))
+   #postgres connect
+   for k,v in config_postgres_object.items():await v.connect()
+   #shutdown
+   yield
+   #postgres disconnect
+   for k,v in config_postgres_object.items():await v.disconnect()
 
 #app
 from fastapi import FastAPI
-from function import function_lifespan
 app=FastAPI(lifespan=function_lifespan,title="atom")
 
 #cors
@@ -13,7 +31,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
 #middleware
-from config import config_postgres_object
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
