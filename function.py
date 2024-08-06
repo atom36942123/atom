434b95ae9abf5369110de2a6408898431a533275
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-06 08:22:42 +0530

    Update function.py

--- function.py
+++ function.py
@@ -5,8 +5,13 @@ async def function_read_constraint_name_list(postgres_object):
   query="select constraint_name from information_schema.constraint_column_usage;"
   values={}
   try:output=await postgres_object.fetch_all(query=query,values=values)
-  
+  except Exception as e:return {"status":0,"message":e.args}
   schema_constraint_name_list=[item["constraint_name"] for item in output]
+  return {"status":1,"message":schema_constraint_name_list}
+  
+    
+
+  
 
   
   
