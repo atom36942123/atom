7817984a3201bcae2a493dd3606c9f041e7b9b2a
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-06 15:28:44 +0530

    Update api.py

--- api.py
+++ api.py
@@ -103,13 +103,12 @@ async def function_csv(request:Request,file:UploadFile):
    for index,object in values_list:
       for k,v in object.items():
          datatype=schema_column_datatype[k]
-         position=values_list[index][k]
-         if k in ["password","google_id"]:position=hashlib.sha256(v.encode()).hexdigest() if v else None
-         if datatype in ["jsonb"]:position=json.dumps(v) if v else None
-         if datatype in ["ARRAY"]:position=v.split(",") if v else None
-         if datatype in ["integer","bigint"]:position=int(v) if v else None
-         if datatype in ["decimal","numeric","real","double precision"]:position=round(float(v),3) if v else None
-         if datatype in ["date","timestamp with time zone"]:position=datetime.strptime(v,'%Y-%m-%d') if v else None
+         if k in ["password","google_id"]:values_list[index][k]=hashlib.sha256(v.encode()).hexdigest() if v else None
+         if datatype in ["jsonb"]:values_list[index][k]=json.dumps(v) if v else None
+         if datatype in ["ARRAY"]:values_list[index][k]=v.split(",") if v else None
+         if datatype in ["integer","bigint"]:values_list[index][k]=int(v) if v else None
+         if datatype in ["decimal","numeric","real","double precision"]:values_list[index][k]=round(float(v),3) if v else None
+         if datatype in ["date","timestamp with time zone"]:values_list[index][k]=datetime.strptime(v,'%Y-%m-%d') if v else None
    #logic
    if mode=="create":
       column_to_insert_list=file_column_list
