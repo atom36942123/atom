6494847bcd2a602ce4765ff28636cdfe31b08bee
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-06 08:33:42 +0530

    Update function.py

--- function.py
+++ function.py
@@ -10,7 +10,7 @@ async def function_read_constraint_name_list(postgres_object):
   schema_constraint_name_list=[item["constraint_name"] for item in output]
   return {"status":1,"message":schema_constraint_name_list}
 
-async def function_delete_all_index(postgres_object):
+async def function_delete_index_all(postgres_object):
   query="select 'drop index ' || string_agg(i.indexrelid::regclass::text,', ' order by n.nspname,i.indrelid::regclass::text, cl.relname) as output from pg_index i join pg_class cl ON cl.oid = i.indexrelid join pg_namespace n ON n.oid = cl.relnamespace left join pg_constraint co ON co.conindid = i.indexrelid where  n.nspname <> 'information_schema' and n.nspname not like 'pg\_%' and co.conindid is null and not i.indisprimary and not i.indisunique and not i.indisexclusion and not i.indisclustered and not i.indisreplident;"
   values={}
   try:output=await postgres_object.fetch_all(query=query,values=values)
