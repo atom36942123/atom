e9ee2d31fc413acc381023329d1e783673044521
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-06 07:19:27 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,1 +1,3 @@
+def function_redis_key_builder(func,namespace:str="",*,request:Request=None,response:Response=None,**kwargs):
+  return ":".join([namespace,request.method.lower(),request.url.path,repr(sorted(request.query_params.items()))])
 
