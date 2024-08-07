6fc1a3ad553e9c524a2ca22ae341a2867138a10f
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-07 21:55:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# 1. copy atom folder
+# 2. install requirement.txt
+# 3. create .env
+# 4. check if postgres/redis server is running
+# 5. python main.py
+
 #config
 from config import config_redis_server_uri
 from config import config_postgres_database_uri
