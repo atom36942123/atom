eaba122d91d55435d3db4ba77e907e064cbdca7e
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-08 08:37:38 +0530

    Update api.py

--- api.py
+++ api.py
@@ -508,16 +508,16 @@ async def function_elasticsearch(request:Request):
    #logic
    #body={"mode":"create","table":"users","id":1,"username":"xxx","age":33,"country":"korea"}
    if body["mode"]=="create":
-      object={k:v for k,v in body.items() if k not in ["mode","table"]}
-      response=elasticsearch_object.index(index=body["table"],document=object)
-   #body={"mode":"read","table":"users","id":"CNPnL5EB-4esJ9tXqJLe"}
+      object={k:v for k,v in body.items() if k not in ["mode","table","id"]}
+      response=elasticsearch_object.index(index=body["table"],id=body["id"],document=object)
+   #body={"mode":"read","table":"users","id":"1"}
    if body["mode"]=="read":
       response=elasticsearch_object.get(index=body["table"],id=body["id"])
-   #{"mode":"update","table":"users","id":"CNPnL5EB-4esJ9tXqJLe","username":"bob","age":100}
+   #{"mode":"update","table":"users","id":"1","username":"bob","age":100}
    if body["mode"]=="update":
       key_to_update={k:v for k,v in body.items() if k not in ["mode","table","id"]}
       response=elasticsearch_object.update(index=body["table"],id=body["id"],doc=key_to_update)
-   #body={"mode":"delete","table":"users","id":"CNPnL5EB-4esJ9tXqJLe"}
+   #body={"mode":"delete","table":"users","id":"1"}
    if body["mode"]=="delete":
       response=elasticsearch_object.delete(index=body["table"],id=body["id"])
    if body["mode"]=="refresh":
