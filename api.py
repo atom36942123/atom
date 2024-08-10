60579fc55d9494f6ad33c8d10dd43fd1548351fa
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-10 23:39:16 +0530

    Update api.py

--- api.py
+++ api.py
@@ -366,8 +366,8 @@ async def function_my(request:Request,background:BackgroundTasks):
       values={"user_1":user["id"],"user_2":body["user_id"]}
       output=await request.state.postgres_object.fetch_all(query=query,values=values)
       background.add_task(await request.state.postgres_object.fetch_all(query="update message set status=:status,updated_by_id=:updated_by_id,updated_at=:updated_at where parent_table='users' and created_by_id=:created_by_id and parent_id=:parent_id returning *;",values={"status":"read","created_by_id":body["user_id"],"parent_id":user["id"],"updated_at":datetime.now(),"updated_by_id":user['id']}))
-   #body={"mode":"read_parent_table_data","table":"likes","parent_table":"post"}
-   if body["mode"]=="read_parent_table_data":
+   #body={"mode":"parent_read","table":"likes","parent_table":"post"}
+   if body["mode"]=="parent_read":
       query=f"select parent_id from {body['table']} where parent_table=:parent_table and created_by_id=:created_by_id order by {order} limit {limit} offset {offset};"
       values={"parent_table":body["parent_table"],"created_by_id":user["id"]}
       output=await request.state.postgres_object.fetch_all(query=query,values=values)
@@ -375,10 +375,10 @@ async def function_my(request:Request,background:BackgroundTasks):
       query=f"select * from {body['parent_table']} join unnest(array{parent_ids}::int[]) with ordinality t(id, ord) using (id) order by t.ord;"
       values={}
       output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   #body={"mode":"action_check","action_table":"likes","parent_table":"post","parent_ids":[1,2,3]}
-   if body["mode"]=="action_check":
-      query=f"select parent_id from {body['action_table']} join unnest(array{body['parent_ids']}::int[]) with ordinality t(parent_id, ord) using (parent_id) where created_by_id=:created_by_id and parent_table=:parent_table;"
-      values={"created_by_id":user["id"],"parent_table":body["parent_table"]}
+   #body={"mode":"parent_check","table":"likes","parent_table":"post","parent_ids":[1,2,3]}
+   if body["mode"]=="parent_check":
+      query=f"select parent_id from {body['table']} join unnest(array{body['parent_ids']}::int[]) with ordinality t(parent_id, ord) using (parent_id) where parent_table=:parent_table and created_by_id=:created_by_id;"
+      values={"parent_table":body["parent_table"],"created_by_id":user["id"]}
       output=await request.state.postgres_object.fetch_all(query=query,values=values)
       output=list(set([item["parent_id"] for item in output if item["parent_id"]]))
    #final
