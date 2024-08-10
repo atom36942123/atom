72adb6b037da188c7a163538d172640b1c3b2d1c
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-10 15:53:24 +0530

    Update function.py

--- function.py
+++ function.py
@@ -104,7 +104,7 @@ async def function_add_action_count(postgres_object,object_list,object_table,act
 
 
 #body min={"table":"post"}
-#body max={"table":"post","id":100,"id_operator":">=","page":100,"limit":100}
+#body max={"table":"post","order":"id desc","limit":100,"page":100,"id":100,"id_operator":">="}
 from datetime import datetime
 async def function_read_object(postgres_object,body,function_read_schema_column_datatype):
   try:
