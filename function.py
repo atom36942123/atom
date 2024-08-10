74c3a3ff7218f9b981739b4acf224ef3ada5fe44
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-10 21:00:06 +0530

    Update function.py

--- function.py
+++ function.py
@@ -10,14 +10,6 @@ async def function_delete_index_all(postgres_object):
   except Exception as e:return {"status":0,"message":e.args}
   return {"status":1,"message":"done"}
 
-async def function_read_column_datatype(postgres_object):
-  query="select column_name,count(*),max(data_type) as datatype from information_schema.columns where table_schema='public' group by  column_name order by count desc;"
-  values={}
-  try:output=await postgres_object.fetch_all(query=query,values=values)
-  except Exception as e:return {"status":0,"message":e.args}
-  column_datatype={item["column_name"]:item["datatype"] for item in output}
-  return {"status":1,"message":column_datatype}
-
 async def function_verify_otp(postgres_object,otp,email,mobile):
   if email and mobile:return {"status":0,"message":"wrong param"}
   if email:
