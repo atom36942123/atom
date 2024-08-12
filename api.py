6481c57cef404faa8843115a643c86cfa7003f0b
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-12 09:59:27 +0530

    Update api.py

--- api.py
+++ api.py
@@ -46,7 +46,7 @@ async def function_database(request:Request):
 @router.post("/{x}/csv",dependencies=[Depends(RateLimiter(times=1,seconds=3))])
 async def function_csv(request:Request,file:UploadFile):
    #prework
-   if request.headers.get("token")!=config_key_root:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
+   if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
    if file.content_type!="text/csv":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"file type issue"}))
    #column datatype
    output=await request.state.postgres_object.fetch_all(query="select column_name,count(*),max(data_type) as datatype from information_schema.columns where table_schema='public' group by  column_name order by count desc;",values={})
