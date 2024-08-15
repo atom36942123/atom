a5762e0d1549e258e652c94b0f47b3175c575776
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 15:09:56 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -69,6 +69,82 @@ router=APIRouter(tags=["auth"])
 #    #final
 #    return {"status":1,"message":token}
 
+# from config import config_key_jwt
+# from fastapi import Request
+# import json,jwt,time
+# from datetime import datetime
+# from datetime import timedelta
+# from fastapi.responses import JSONResponse
+# from fastapi.encoders import jsonable_encoder
+# @router.post("/{x}/auth/email")
+# async def function_auth_email(request:Request,email:str,otp:int):
+#    #verify otp
+#    query="select otp from otp where email=:email order by id desc limit 1;"
+#    values={"email":email}
+#    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#    if not output:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp not exist"}))
+#    if int(output[0]["otp"])!=int(otp):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp mismatch"}))
+#    #read user
+#    query="select * from users where email=:email order by id desc limit 1;"
+#    values={"email":email}
+#    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#    user=output[0] if output else None
+#    #create user
+#    if not user:
+#       query="insert into users (email) values (:email) returning *;"
+#       values={"email":email}
+#       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#       user_id=output[0]["id"]
+#       query="select * from users where id=:id;"
+#       values={"id":user_id}
+#       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#       user=output[0]
+#    #token encode
+#    user={"created_at_token":datetime.today().strftime('%Y-%m-%d'),"x":str(request.url.path).split("/")[1],"id":user["id"],"is_active":user["is_active"],"type":user["type"]}
+#    data=json.dumps(user,default=str)
+#    payload={"exp":time.mktime((datetime.now()+timedelta(days=100000)).timetuple()),"data":data}
+#    token=jwt.encode(payload,config_key_jwt)
+#    #final
+#    return {"status":1,"message":token}
+
+# from config import config_key_jwt
+# from fastapi import Request
+# import json,jwt,time
+# from datetime import datetime
+# from datetime import timedelta
+# from fastapi.responses import JSONResponse
+# from fastapi.encoders import jsonable_encoder
+# @router.post("/{x}/auth/mobile")
+# async def function_auth_mobile(request:Request,mobile:str,otp:int):
+#    #verify otp
+#    query="select otp from otp where mobile=:mobile order by id desc limit 1;"
+#    values={"mobile":mobile}
+#    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#    if not output:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp not exist"}))
+#    if int(output[0]["otp"])!=int(otp):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp mismatch"}))
+#    #read user
+#    query="select * from users where mobile=:mobile order by id desc limit 1;"
+#    values={"mobile":mobile}
+#    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#    user=output[0] if output else None
+#    #create user
+#    if not user:
+#       query="insert into users (mobile) values (:mobile) returning *;"
+#       values={"mobile":mobile}
+#       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#       user_id=output[0]["id"]
+#       query="select * from users where id=:id;"
+#       values={"id":user_id}
+#       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#       user=output[0]
+#    #token encode
+#    user={"created_at_token":datetime.today().strftime('%Y-%m-%d'),"x":str(request.url.path).split("/")[1],"id":user["id"],"is_active":user["is_active"],"type":user["type"]}
+#    data=json.dumps(user,default=str)
+#    payload={"exp":time.mktime((datetime.now()+timedelta(days=100000)).timetuple()),"data":data}
+#    token=jwt.encode(payload,config_key_jwt)
+#    #final
+#    return {"status":1,"message":token}
+  
 from config import config_key_jwt
 from fastapi import Request
 import json,jwt,time
@@ -76,29 +152,16 @@ from datetime import datetime
 from datetime import timedelta
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-@router.post("/{x}/auth/email")
-async def function_auth_email(request:Request,email:str,otp:int):
-   #verify otp
-   query="select otp from otp where email=:email order by id desc limit 1;"
-   values={"email":email}
-   output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   if not output:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp not exist"}))
-   if int(output[0]["otp"])!=int(otp):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp mismatch"}))
+@router.post("/{x}/auth/token-refresh")
+async def function_auth_token_refresh(request:Request):
+   #prework
+   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
    #read user
-   query="select * from users where email=:email order by id desc limit 1;"
-   values={"email":email}
+   query="select * from users where id=:id;"
+   values={"id":user["id"]}
    output=await request.state.postgres_object.fetch_all(query=query,values=values)
    user=output[0] if output else None
-   #create user
-   if not user:
-      query="insert into users (email) values (:email) returning *;"
-      values={"email":email}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      user_id=output[0]["id"]
-      query="select * from users where id=:id;"
-      values={"id":user_id}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      user=output[0]
+   if not user:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"no user"}))
    #token encode
    user={"created_at_token":datetime.today().strftime('%Y-%m-%d'),"x":str(request.url.path).split("/")[1],"id":user["id"],"is_active":user["is_active"],"type":user["type"]}
    data=json.dumps(user,default=str)
@@ -106,56 +169,3 @@ async def function_auth_email(request:Request,email:str,otp:int):
    token=jwt.encode(payload,config_key_jwt)
    #final
    return {"status":1,"message":token}
-
-from config import config_key_jwt
-from fastapi import Request
-import json,jwt,time
-from datetime import datetime
-from datetime import timedelta
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-@router.post("/{x}/auth/mobile")
-async def function_auth_mobile(request:Request,mobile:str,otp:int):
-   #verify otp
-   query="select otp from otp where mobile=:mobile order by id desc limit 1;"
-   values={"mobile":mobile}
-   output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   if not output:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp not exist"}))
-   if int(output[0]["otp"])!=int(otp):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp mismatch"}))
-   #read user
-   query="select * from users where mobile=:mobile order by id desc limit 1;"
-   values={"mobile":mobile}
-   output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   user=output[0] if output else None
-   #create user
-   if not user:
-      query="insert into users (mobile) values (:mobile) returning *;"
-      values={"mobile":mobile}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      user_id=output[0]["id"]
-      query="select * from users where id=:id;"
-      values={"id":user_id}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      user=output[0]
-   #token encode
-   user={"created_at_token":datetime.today().strftime('%Y-%m-%d'),"x":str(request.url.path).split("/")[1],"id":user["id"],"is_active":user["is_active"],"type":user["type"]}
-   data=json.dumps(user,default=str)
-   payload={"exp":time.mktime((datetime.now()+timedelta(days=100000)).timetuple()),"data":data}
-   token=jwt.encode(payload,config_key_jwt)
-   #final
-   return {"status":1,"message":token}
-  
-
-
-   
-   # #body={"mode":"token_refresh"}
-   # if "mode" in body and body["mode"]=="token_refresh":
-   #    user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   #    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
-   #    query="select * from users where id=:id;"
-   #    values={"id":user["id"]}
-   #    output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   #    user=output[0] if output else None
-   #    if not user:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"no user"}))
-  
-  
