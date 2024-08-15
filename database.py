f84fb97ab838b5a1cfdfa75685b31f4fe6b3e93b
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 13:56:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,22 +2,25 @@
 from fastapi import APIRouter
 router=APIRouter(tags=["database"])
 
+# from config import config_key_root
+# from fastapi import Request
+# from fastapi.responses import JSONResponse
+# from fastapi.encoders import jsonable_encoder
+# @router.get("/{x}/database/query-runner")
+# async def function_database_query_runner(request:Request,query:str):
+#    #prework
+#    if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
+#    #logic
+#    query=query
+#    values={}
+#    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#    #final
+#    return output
+
 from config import config_key_root
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-@router.get("/{x}/database/query-runner")
-async def function_database_query_runner(request:Request,query:str):
-   #prework
-   if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
-   #logic
-   query=query
-   values={}
-   output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   #final
-   return output
-
-
 @router.get("/{x}/database/clean")
 async def function_database_clean(request:Request):
    #prework
