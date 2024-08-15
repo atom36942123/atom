679bc97769ac7e6e7e871a34d517eb0529844ce8
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 14:18:35 +0530

    Update api.py

--- api.py
+++ api.py
@@ -17,13 +17,6 @@ from elasticsearch import Elasticsearch
 
 
 
-@router.post("/{x}/signup")
-async def function_signup(request:Request):
-   body=await request.json()
-   query="insert into users (username,password) values (:username,:password) returning *;"
-   values={"username":body["username"],"password":hashlib.sha256(body["password"].encode()).hexdigest()}
-   output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   return {"status":1,"message":output}
 
 @router.post("/{x}/login")
 async def function_login(request:Request):
