10d56db50808464f8818a249c1621462787f75c7
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 14:18:41 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,3 +1,12 @@
 #router
 from fastapi import APIRouter
 router=APIRouter(tags=["auth"])
+
+@router.post("/{x}/signup")
+async def function_signup(request:Request):
+   body=await request.json()
+   query="insert into users (username,password) values (:username,:password) returning *;"
+   values={"username":body["username"],"password":hashlib.sha256(body["password"].encode()).hexdigest()}
+   output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   return {"status":1,"message":output}
+
