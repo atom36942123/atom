4624fbef3c3d336fb64b88a90f61283f011f64d4
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 19:08:18 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -20,6 +20,34 @@ async def function_utility_pcache(request:Request):
    #final
    return {"status":1,"message":temp}
 
+#update cell
+from config import config_key_jwt
+import jwt,json
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from datetime import datetime
+@router.put("/{x}/utility/update-cell")
+async def function_utility_update_cell(request:Request):
+   #prework
+   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
+   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   if user["type"]!="admin":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"admin issue"}))
+   body=dict(await request.json())
+   #body unpack
+   table=body["table"]
+   id=body["id"]
+   column=body["column"]
+   value=body["value"]
+   #logic
+   if column in ["password","google_id"]:value=hashlib.sha256(value.encode()).hexdigest()
+   if column in ["metadata"]:value=json.dumps(value)
+   query=f"update {table} set {column}=:value,updated_at=:updated_at,updated_by_id=:updated_by_id where id=:id returning *;"
+   values={"value":value,"id":id,"updated_at":datetime.now(),"updated_by_id":user['id']}
+   output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #final
+   return {"status":1,"message":output}
+
 #presigned url
 from fastapi import Request
 from config import config_aws_access_key_id,config_aws_secret_access_key,config_s3_bucket_name,config_s3_region_name
@@ -75,4 +103,3 @@ async def function_utility_send_email_ses(request:Request,to:str,title:str,descr
    output=ses_client.send_email(Source=config_ses_sender_email,Destination={"ToAddresses":[to]},Message={"Subject":{"Charset":"UTF-8","Data":title},"Body":{"Text":{"Charset":"UTF-8","Data":description}}})
    #final
    return {"status":1,"message":output}
-
