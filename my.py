fd9f43e5f5b1d4b07298f450be6e59e878edd22c
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 15:31:27 +0530

    Update my.py

--- my.py
+++ my.py
@@ -5,6 +5,8 @@ router=APIRouter(tags=["my"])
 #profile
 from fastapi import Request
 from fastapi import BackgroundTasks
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.get("/{x}/my/profile")
 async def function_my_profile(request:Request,background:BackgroundTasks):
    #prework
