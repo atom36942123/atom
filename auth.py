f077a21ef8b867c0c30fd74cf256008a11de4748
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 14:40:28 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -46,63 +46,63 @@ async def function_auth_login(request:Request):
       
       
       
-   #body={"mode":"google","google_id":"123"}
-   if "mode" in body and body["mode"]=="google":
-      query="select * from users where google_id=:google_id order by id desc limit 1;"
-      values={"google_id":hashlib.sha256(body["google_id"].encode()).hexdigest()}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      user=output[0] if output else None
-      if not user:
-         query="insert into users (google_id) values (:google_id) returning *;"
-         values={"google_id":hashlib.sha256(body["google_id"].encode()).hexdigest()}
-         output=await request.state.postgres_object.fetch_all(query=query,values=values)
-         user_id=output[0]["id"]
-         query="select * from users where id=:id;"
-         values={"id":user_id}
-         output=await request.state.postgres_object.fetch_all(query=query,values=values)
-         user=output[0]
-   #body={"mode":"email","email":"xxx","otp":123}
-   if "mode" in body and body["mode"]=="email":
-      response=await function_verify_otp(request.state.postgres_object,body["otp"],body["email"],None)
-      if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
-      query="select * from users where email=:email order by id desc limit 1;"
-      values={"email":body["email"]}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      user=output[0] if output else None
-      if not user:
-         query="insert into users (email) values (:email) returning *;"
-         values={"email":body["email"]}
-         output=await request.state.postgres_object.fetch_all(query=query,values=values)
-         user_id=output[0]["id"]
-         query="select * from users where id=:id;"
-         values={"id":user_id}
-         output=await request.state.postgres_object.fetch_all(query=query,values=values)
-         user=output[0]
-   #body={"mode":"mobile","mobile":"xxx","otp":123}
-   if "mode" in body and body["mode"]=="mobile":
-      response=await function_verify_otp(request.state.postgres_object,body["otp"],None,body["mobile"])
-      if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
-      query="select * from users where mobile=:mobile order by id desc limit 1;"
-      values={"mobile":body["mobile"]}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      user=output[0] if output else None
-      if not user:
-         query="insert into users (mobile) values (:mobile) returning *;"
-         values={"mobile":body["mobile"]}
-         output=await request.state.postgres_object.fetch_all(query=query,values=values)
-         user_id=output[0]["id"]
-         query="select * from users where id=:id;"
-         values={"id":user_id}
-         output=await request.state.postgres_object.fetch_all(query=query,values=values)
-         user=output[0]
-   #body={"mode":"token_refresh"}
-   if "mode" in body and body["mode"]=="token_refresh":
-      user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-      if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
-      query="select * from users where id=:id;"
-      values={"id":user["id"]}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      user=output[0] if output else None
-      if not user:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"no user"}))
+   # #body={"mode":"google","google_id":"123"}
+   # if "mode" in body and body["mode"]=="google":
+   #    query="select * from users where google_id=:google_id order by id desc limit 1;"
+   #    values={"google_id":hashlib.sha256(body["google_id"].encode()).hexdigest()}
+   #    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #    user=output[0] if output else None
+   #    if not user:
+   #       query="insert into users (google_id) values (:google_id) returning *;"
+   #       values={"google_id":hashlib.sha256(body["google_id"].encode()).hexdigest()}
+   #       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #       user_id=output[0]["id"]
+   #       query="select * from users where id=:id;"
+   #       values={"id":user_id}
+   #       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #       user=output[0]
+   # #body={"mode":"email","email":"xxx","otp":123}
+   # if "mode" in body and body["mode"]=="email":
+   #    response=await function_verify_otp(request.state.postgres_object,body["otp"],body["email"],None)
+   #    if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   #    query="select * from users where email=:email order by id desc limit 1;"
+   #    values={"email":body["email"]}
+   #    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #    user=output[0] if output else None
+   #    if not user:
+   #       query="insert into users (email) values (:email) returning *;"
+   #       values={"email":body["email"]}
+   #       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #       user_id=output[0]["id"]
+   #       query="select * from users where id=:id;"
+   #       values={"id":user_id}
+   #       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #       user=output[0]
+   # #body={"mode":"mobile","mobile":"xxx","otp":123}
+   # if "mode" in body and body["mode"]=="mobile":
+   #    response=await function_verify_otp(request.state.postgres_object,body["otp"],None,body["mobile"])
+   #    if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   #    query="select * from users where mobile=:mobile order by id desc limit 1;"
+   #    values={"mobile":body["mobile"]}
+   #    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #    user=output[0] if output else None
+   #    if not user:
+   #       query="insert into users (mobile) values (:mobile) returning *;"
+   #       values={"mobile":body["mobile"]}
+   #       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #       user_id=output[0]["id"]
+   #       query="select * from users where id=:id;"
+   #       values={"id":user_id}
+   #       output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #       user=output[0]
+   # #body={"mode":"token_refresh"}
+   # if "mode" in body and body["mode"]=="token_refresh":
+   #    user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
+   #    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #    query="select * from users where id=:id;"
+   #    values={"id":user["id"]}
+   #    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #    user=output[0] if output else None
+   #    if not user:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"no user"}))
   
   
