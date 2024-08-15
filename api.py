9b1bc683741b6c211f12a685308e878fb187ffab
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 18:46:51 +0530

    Update api.py

--- api.py
+++ api.py
@@ -42,27 +42,7 @@ async def function_feed(request:Request):
    #final
    return {"status":1,"message":output}
 
-@router.post("/{x}/aws")
-async def function_aws(request:Request):
-   #prework
-   body=await request.json()
-   #logic
-   #body={"mode":"s3_create_presigned_url","filename":"xxx.png"}
-   if body["mode"]=="s3_create_presigned_url":
-      output=boto3.client("s3",region_name=config_s3_region_name,aws_access_key_id=config_aws_access_key_id,aws_secret_access_key=config_aws_secret_access_key).generate_presigned_post(Bucket=config_s3_bucket_name,Key=str(uuid.uuid4())+"-"+body["filename"],ExpiresIn=10,Conditions=[['content-length-range',1,250*1024]])
-   #body={"mode":"s3_delete_bucket_key","url":"www.xxx.png/xxx"}
-   if body["mode"]=="s3_delete_bucket_key":
-      if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
-      output=boto3.resource("s3",aws_access_key_id=config_aws_access_key_id,aws_secret_access_key=config_aws_secret_access_key).Object(config_s3_bucket_name,body["url"].rsplit("/",1)[1]).delete()
-   #body={"mode":"s3_delete_bucket_key_all"}
-   if body["mode"]=="s3_delete_bucket_key_all":
-      if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
-      output=boto3.resource("s3",aws_access_key_id=config_aws_access_key_id,aws_secret_access_key=config_aws_secret_access_key).Bucket(config_s3_bucket_name).objects.all().delete()
-   #body={"mode":"ses_send_email","to":"atom36942@gmail.com","title":"xxx","description":"xxx"}
-   if body["mode"]=="ses_send_email":
-      output=boto3.client("ses",region_name=config_ses_region_name,aws_access_key_id=config_aws_access_key_id,aws_secret_access_key=config_aws_secret_access_key).send_email(Source=config_ses_sender_email,Destination={"ToAddresses":[body["to"]]},Message={"Subject":{"Charset":"UTF-8","Data":body["title"]},"Body":{"Text":{"Charset":"UTF-8","Data":body["description"]}}})
-   #final
-   return {"status":1,"message":output}
+
 
 @router.post("/{x}/mongo")
 async def function_mongo(request:Request):
