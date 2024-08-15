78aa9048ce447d6e9afcc3e9a63df8fc5e79cc0e
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 14:49:35 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -16,21 +16,53 @@ router=APIRouter(tags=["auth"])
 #    #final
 #    return {"status":1,"message":output}
 
+# from config import config_key_jwt
+# from fastapi import Request
+# import hashlib,json,jwt,time
+# from datetime import datetime
+# from datetime import timedelta
+# @router.post("/{x}/auth/login")
+# async def function_auth_login(request:Request):
+#    #prework
+#    body=await request.json()
+#    #read user
+#    query="select * from users where username=:username and password=:password order by id desc limit 1;"
+#    values={"username":body["username"],"password":hashlib.sha256(body["password"].encode()).hexdigest()}
+#    output=await request.state.postgres_object.fetch_all(query=query,values=values)
+#    user=output[0] if output else None
+#    if not user:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"no user"}))
+#    #token encode
+#    user={"created_at_token":datetime.today().strftime('%Y-%m-%d'),"x":str(request.url.path).split("/")[1],"id":user["id"],"is_active":user["is_active"],"type":user["type"]}
+#    data=json.dumps(user,default=str)
+#    payload={"exp":time.mktime((datetime.now()+timedelta(days=100000)).timetuple()),"data":data}
+#    token=jwt.encode(payload,config_key_jwt)
+#    #final
+#    return {"status":1,"message":token}
+
 from config import config_key_jwt
 from fastapi import Request
 import hashlib,json,jwt,time
 from datetime import datetime
 from datetime import timedelta
-@router.post("/{x}/auth/login")
-async def function_auth_login(request:Request):
+@router.post("/{x}/auth/google")
+async def function_auth_google(request:Request):
    #prework
    body=await request.json()
    #read user
-   query="select * from users where username=:username and password=:password order by id desc limit 1;"
-   values={"username":body["username"],"password":hashlib.sha256(body["password"].encode()).hexdigest()}
+   query="select * from users where google_id=:google_id order by id desc limit 1;"
+   values={"google_id":hashlib.sha256(body["google_id"].encode()).hexdigest()}
    output=await request.state.postgres_object.fetch_all(query=query,values=values)
    user=output[0] if output else None
-   if not user:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"no user"}))
+   #create user
+   if not user:
+      query="insert into users (google_id) values (:google_id) returning *;"
+      values={"google_id":hashlib.sha256(body["google_id"].encode()).hexdigest()}
+      output=await request.state.postgres_object.fetch_all(query=query,values=values)
+      user_id=output[0]["id"]
+      query="select * from users where id=:id;"
+      values={"id":user_id}
+      output=await request.state.postgres_object.fetch_all(query=query,values=values)
+      user=output[0]
    #token encode
    user={"created_at_token":datetime.today().strftime('%Y-%m-%d'),"x":str(request.url.path).split("/")[1],"id":user["id"],"is_active":user["is_active"],"type":user["type"]}
    data=json.dumps(user,default=str)
@@ -38,29 +70,14 @@ async def function_auth_login(request:Request):
    token=jwt.encode(payload,config_key_jwt)
    #final
    return {"status":1,"message":token}
- 
 
-
-     
+         
+  
       
-      
-      
-      
-   # #body={"mode":"google","google_id":"123"}
-   # if "mode" in body and body["mode"]=="google":
-   #    query="select * from users where google_id=:google_id order by id desc limit 1;"
-   #    values={"google_id":hashlib.sha256(body["google_id"].encode()).hexdigest()}
-   #    output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   #    user=output[0] if output else None
-   #    if not user:
-   #       query="insert into users (google_id) values (:google_id) returning *;"
-   #       values={"google_id":hashlib.sha256(body["google_id"].encode()).hexdigest()}
-   #       output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   #       user_id=output[0]["id"]
-   #       query="select * from users where id=:id;"
-   #       values={"id":user_id}
-   #       output=await request.state.postgres_object.fetch_all(query=query,values=values)
-   #       user=output[0]
+        
+         
+         
+         
    # #body={"mode":"email","email":"xxx","otp":123}
    # if "mode" in body and body["mode"]=="email":
    #    response=await function_verify_otp(request.state.postgres_object,body["otp"],body["email"],None)
