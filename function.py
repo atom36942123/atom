9a14b3fbd39cca9915da7f26580c97628a12f805
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 14:34:15 +0530

    Update function.py

--- function.py
+++ function.py
@@ -5,7 +5,7 @@ async def function_token_encode(user):
 
    
    user_extra={"created_at_token":datetime.today().strftime('%Y-%m-%d')}
-   data=json.dumps(user|user_extra,default=str)
+   
    expiry_time=time.mktime((datetime.now()+timedelta(days=100000)).timetuple())
    payload={"exp":expiry_time,"data":data}
    token=jwt.encode(payload,config_key_jwt)
