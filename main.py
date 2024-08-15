77b62f8248c43b0bda9f7a7245d9866512a24213
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 18:37:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,8 @@ from my import router as router_my
 app.include_router(router_my)
 from admin import router as router_admin
 app.include_router(router_admin)
-
+from utility import router as router_utility
+app.include_router(router_utility)
 
 #server start
 import uvicorn,asyncio
