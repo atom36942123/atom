d3acb75f8a3a5c06a1a324f859168d80146eed59
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-15 18:30:36 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -10,11 +10,17 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from datetime import datetime
 @router.put("/{x}/admin/update-cell")
-async def function_admin_update_cell(request:Request,table:str,id:int,column:str,value:str):
+async def function_admin_update_cell(request:Request):
    #prework
    user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
    if user["type"]!="admin":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"admin issue"}))
+   body=dict(await request.json())
+   #body unpack
+   table=body["table"]
+   id=body["id"]
+   column=body["column"]
+   value=body["value"]
    #logic
    if column in ["password","google_id"]:value=hashlib.sha256(value.encode()).hexdigest()
    if column in ["metadata"]:value=json.dumps(value)
