9e51ec1eff53da52cb2819fcfc627382a9344447
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-16 19:24:19 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -53,21 +53,21 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from datetime import datetime
+from function import function_sanitization_values_list
 @router.put("/{x}/utility/update-cell")
-async def function_utility_update_cell(request:Request):
+async def function_utility_update_cell(request:Request,table:str,id:int,column:str,value:str):
    #prework
    user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
    if user["type"]!="admin":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"admin issue"}))
-   body=dict(await request.json())
-   #body unpack
-   table=body["table"]
-   id=body["id"]
-   column=body["column"]
-   value=body["value"]
+   from function import function_sanitization_values_list   
+   #sanitization
+   values_list=[{column:values}]
+   response=await function_sanitization_values_list(request.state.postgres_object,values_list)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   values_list=response["message"]
+   values=values_list[0]
    #logic
-   if column in ["password","google_id"]:value=hashlib.sha256(value.encode()).hexdigest()
-   if column in ["metadata"]:value=json.dumps(value)
    query=f"update {table} set {column}=:value,updated_at=:updated_at,updated_by_id=:updated_by_id where id=:id returning *;"
    values={"value":value,"id":id,"updated_at":datetime.now(),"updated_by_id":user['id']}
    output=await request.state.postgres_object.fetch_all(query=query,values=values)
