f9d986bf1b602972a211a9638b3b9faf321ff9ec
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-16 19:13:44 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -37,24 +37,6 @@ async def function_utility_feed(request:Request,table:str,order:str="id desc",li
    #final
    return {"status":1,"message":output}
 
-#pcache
-from fastapi import Request
-from fastapi_cache.decorator import cache
-@router.get("/{x}/utility/pcache")
-@cache(expire=60)
-async def function_utility_pcache(request:Request): 
-   #logic
-   config_pcache={"user_count":"select count(*) from users;"}
-   temp={}
-   for k,v in config_pcache.items():
-      query=v
-      values={}
-      output=await request.state.postgres_object.fetch_all(query=query,values=values)
-      if "count" in k:temp[k]=output[0]["count"]
-      else:temp[k]=output
-   #final
-   return {"status":1,"message":temp}
-
 #update cell
 from config import config_key_jwt
 import jwt,json
@@ -82,6 +64,24 @@ async def function_utility_update_cell(request:Request):
    output=await request.state.postgres_object.fetch_all(query=query,values=values)
    #final
    return {"status":1,"message":output}
+
+#pcache
+from fastapi import Request
+from fastapi_cache.decorator import cache
+@router.get("/{x}/utility/pcache")
+@cache(expire=60)
+async def function_utility_pcache(request:Request): 
+   #logic
+   config_pcache={"user_count":"select count(*) from users;"}
+   temp={}
+   for k,v in config_pcache.items():
+      query=v
+      values={}
+      output=await request.state.postgres_object.fetch_all(query=query,values=values)
+      if "count" in k:temp[k]=output[0]["count"]
+      else:temp[k]=output
+   #final
+   return {"status":1,"message":temp}
 
 #presigned url
 from fastapi import Request
