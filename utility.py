ba73c1cfb8b90e6f6b3f70c9420b99b0ad78da9c
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-17 00:19:26 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -19,8 +19,8 @@ async def function_utility_feed(request:Request,table:str,order:str="id desc",li
    if table not in ["users","post","atom"]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"table not allowed"}))
    #where
    where_param={k:v for k,v in query_param.items() if k not in ["table","order","limit","page"]}
-   where_param_values={k:v.rsplit(',',1)[0] for k,v in where_param.items()}
-   where_param_operator={k:v.rsplit(',',1)[1] for k,v in where_param.items()}
+   where_param_values={k:v.split(',',1)[1] for k,v in where_param.items()}
+   where_param_operator={k:v.split(',',1)[0] for k,v in where_param.items()}
    key_list=[f"({k} {where_param_operator[k]} :{k} or :{k} is null)" for k,v in where_param_values.items()]
    key_joined=' and '.join(key_list)
    where=f"where {key_joined}" if key_joined else ""
