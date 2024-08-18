a8dd0118794ca2eddaca4f0e7d24328e843b2a29
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 19:10:03 +0530

    Update action.py

--- action.py
+++ action.py
@@ -1,3 +1,15 @@
 #router
 from fastapi import APIRouter
 router=APIRouter(tags=["action"])
+
+#post
+from fastapi import Request
+import hashlib
+@router.post("/{x}/action/post")
+async def function_action_post(request:Request):
+   #logic
+   query="insert into users (username,password) values (:username,:password) returning *;"
+   values={"username":username,"password":hashlib.sha256(password.encode()).hexdigest()}
+   output=await request.state.postgres_object.fetch_all(query=query,values=values)
+   #final
+   return {"status":1,"message":output}
