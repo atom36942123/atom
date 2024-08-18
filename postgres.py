1aa8482136c7a2508002f14d85c89c83f3d463fc
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 21:18:01 +0530

    Update postgres.py

--- postgres.py
+++ postgres.py
@@ -1,1 +1,10 @@
+from config import config_postgres_database_uri
+from databases import Database
+
+postgres_object_dict={}
+postgres_url_list=config_postgres_database_uri.split(",")
+for item in postgres_url_list:
+  object=Database(item,min_size=1,max_size=100)
+  x=item.split("/")[-1]
+  postgres_object_dict={x:object}
 
