d7a32917bb3f32ec672d2c21da64d3fae73fc426
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 19:10:40 +0530

    Update action.py

--- action.py
+++ action.py
@@ -3,8 +3,11 @@ from fastapi import APIRouter
 router=APIRouter(tags=["action"])
 
 #post
+from config import config_key_jwt
+import jwt,json
 from fastapi import Request
-import hashlib
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/post")
 async def function_action_post(request:Request):
    #logic
