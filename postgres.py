59914ce1f4dd63e7428d525671b1c91f2e1f5745
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 23:47:49 +0530

    Update postgres.py

--- postgres.py
+++ postgres.py
@@ -6,7 +6,7 @@ from databases import Database
 postgres_object_dict={}
 postgres_url_list=config_postgres_database_uri.split(",")
 for item in postgres_url_list:
-  object=Database(item,min_size=1,max_size=100)
-  x=item.split("/")[-1]
-  postgres_object_dict={x:object}
+  postgres_object=Database(item,min_size=1,max_size=100)
+  database_name=item.split("/")[-1]
+  postgres_object_dict={database_name:postgres_object}
 
