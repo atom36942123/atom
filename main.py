86f9dbba53ca463c55c5055765c95c16ee544e5a
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 23:02:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
-
-#server start
+#import
 from app import app
 import uvicorn
 import asyncio
+
+#server start
 if __name__=="__main__":
   uvicorn_object=uvicorn.Server(config=uvicorn.Config(app,"0.0.0.0",8000,workers=16,log_level="info",reload=False,lifespan="on",loop="asyncio"))
   loop=asyncio.new_event_loop()
