bb66a80daa67ef33ec604f5111037954a0fac077
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 21:21:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,26 +17,6 @@ app=FastAPI(lifespan=function_lifespan,title="atom")
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
-#middleware
-from fastapi import Request
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-import traceback
-@app.middleware("http")
-async def middleware(request:Request,api_function):
-  #x check
-  key_4th=str(request.url.path).split("/")[1]
-  if key_4th not in ["","docs","openapi.json","redoc"]+[*postgres_object_dict]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"wrong x"}))
-  #postgres object assgin
-  if key_4th in postgres_object_dict:request.state.postgres_object=postgres_object_dict[key_4th]
-  #api response
-  try:response=await api_function(request)
-  except Exception as e:
-    print(traceback.format_exc())
-    return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":e.args}))
-  #final
-  return response
-
 #root api
 @app.get("/")
 async def function_root():
