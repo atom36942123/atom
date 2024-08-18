75d4260befbe27f1db901b9c38f1f2976b0a5e90
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 20:36:27 +0530

    Update lifespan.py

--- lifespan.py
+++ lifespan.py
@@ -1,1 +1,21 @@
+from config import config_redis_server_uri
+from config
 
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from redis import asyncio as aioredis
+from fastapi_limiter import FastAPILimiter
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+@asynccontextmanager
+async def function_lifespan(app:FastAPI):
+  #redis cache
+  FastAPICache.init(RedisBackend(aioredis.from_url(config_redis_server_uri)))
+  #redis rate limiter
+  await FastAPILimiter.init(aioredis.from_url(config_redis_server_uri,encoding="utf-8",decode_responses=True))
+  #postgres connect
+  for k,v in postgres_object_dict.items():await v.connect()
+  #shutdown
+  yield
+  #postgres disconnect
+  for k,v in postgres_object_dict.items():await v.disconnect()
