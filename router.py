76e0f74b6b42902fb7f25e3bfc8c77ba15b749bb
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 00:16:20 +0530

    Update router.py

--- router.py
+++ router.py
@@ -5,3 +5,7 @@ from app import app
 from api_root import router as router_root
 app.include_router(router_root)
 
+from api_database import router as router_database
+app.include_router(router_database)
+
+
