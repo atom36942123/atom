11e95cb00d9e33f76a0a981657cd40753981da00
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 21:27:01 +0530

    Update middleware.py

--- middleware.py
+++ middleware.py
@@ -11,14 +11,18 @@ async def middleware(request:Request,api_function):
   #url split (4th position)
   key_4th=str(request.url.path).split("/")[1]
   #x check
-  if key_4th not in ["","docs","openapi.json","redoc"]+[*postgres_object_dict]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"wrong x"}))
+  key_4th_allowed_general=["","docs","openapi.json","redoc"]
+  key_4th_allowed_postgres=[*postgres_object_dict]
+  if key_4th not in key_4th_allowed_general+key_4th_allowed_postgres:
+    return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"wrong x"}))
   #postgres object assgin
-  if key_4th in postgres_object_dict:request.state.postgres_object=postgres_object_dict[key_4th]
+  if key_4th in postgres_object_dict:
+    request.state.postgres_object=postgres_object_dict[key_4th]
   #api response
-  try:response=await api_function(request)
+  try:
+    response=await api_function(request)
   except Exception as e:
     print(traceback.format_exc())
     return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":e.args}))
   #final
   return response
-
