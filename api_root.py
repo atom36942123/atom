1cf8859650acbac52a6173966309216847242120
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 00:04:27 +0530

    Update api_root.py

--- api_root.py
+++ api_root.py
@@ -4,6 +4,7 @@ router=APIRouter(tags=["root"])
 
 #root
 from postgres import postgres_object_dict
+from fastapi import Request
 @router.get("/")
 async def function_root():
    x_list=[*postgres_object_dict]
