c4309ea869a0928dc1ee137e95da42ab619e238e
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-18 21:22:19 +0530

    Update middleware.py

--- middleware.py
+++ middleware.py
@@ -1,1 +1,23 @@
+#import
+from postgres import config
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+import traceback
+
+#logic
+@app.middleware("http")
+async def middleware(request:Request,api_function):
+  #x check
+  key_4th=str(request.url.path).split("/")[1]
+  if key_4th not in ["","docs","openapi.json","redoc"]+[*postgres_object_dict]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"wrong x"}))
+  #postgres object assgin
+  if key_4th in postgres_object_dict:request.state.postgres_object=postgres_object_dict[key_4th]
+  #api response
+  try:response=await api_function(request)
+  except Exception as e:
+    print(traceback.format_exc())
+    return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":e.args}))
+  #final
+  return response
 
