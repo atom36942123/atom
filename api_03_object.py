d354a7406a8a64975427b9e9e8f4c707de4de007
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 23:29:03 +0530

    Update api_03_object.py

--- api_03_object.py
+++ api_03_object.py
@@ -34,7 +34,7 @@ async def function_object_create(request:Request,table:str):
    #query_param set
    query_param={}
    for item in column_to_insert_list:
-      if k in body:query_param[item]=body[item]
+      if item in body:query_param[item]=body[item]
    query_param["created_by_id"]=user["id"]
    #sanitization query_param
    response=await function_sanitization_query_param_list(postgres_object,"create",[query_param])
@@ -67,7 +67,7 @@ async def function_object_update(request:Request,table:str,id:int):
    #query_param set
    query_param={}
    for item in column_to_update_list:
-      if k in body:query_param[item]=body[item]
+      if item in body:query_param[item]=body[item]
    query_param["updated_at"]=datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
    query_param["updated_by_id"]=user["id"]
    query_param["id"]=id
