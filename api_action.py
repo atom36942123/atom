26a4c4afb18dcdacae64c3d7e0a46d4337229a48
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 20:45:23 +0530

    Update api_action.py

--- api_action.py
+++ api_action.py
@@ -22,7 +22,7 @@ async def function_action_post(request:Request,description:str,title:str=None,fi
    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
    #logic
    query="insert into post (created_by_id,title,description,file_url,link_url,tag) values (:created_by_id,:title,:description,:file_url,:link_url,:tag) returning *;"
-   query_param=dict(request.query_params)|{"created_by_id":user["id"]}
+   query_param={"created_by_id":user["id"]}|dict(request.query_params)
    output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
    return {"status":1,"message":output}
@@ -38,7 +38,7 @@ async def function_action_like(request:Request,parent_table:str,parent_id:int):
    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
    #logic
    query="insert into likes (created_by_id,parent_table,parent_id) values (:created_by_id,:parent_table,:parent_id) returning *;"
-   query_param=dict(request.query_params)|{"created_by_id":user["id"]}
+   query_param={"created_by_id":user["id"]}|dict(request.query_params)
    query_param["parent_id"]=int(query_param["parent_id"])
    output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
@@ -55,7 +55,7 @@ async def function_action_bookmark(request:Request,parent_table:str,parent_id:in
    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
    #logic
    query="insert into bookmark (created_by_id,parent_table,parent_id) values (:created_by_id,:parent_table,:parent_id) returning *;"
-   query_param=dict(request.query_params)|{"created_by_id":user["id"]}
+   query_param={"created_by_id":user["id"]}|dict(request.query_params)
    query_param["parent_id"]=int(query_param["parent_id"])
    output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
@@ -72,7 +72,24 @@ async def function_action_block(request:Request,parent_table:str,parent_id:int,d
    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
    #logic
    query="insert into block (created_by_id,parent_table,parent_id,description) values (:created_by_id,:parent_table,:parent_id,:description) returning *;"
-   query_param=dict(request.query_params)|{"created_by_id":user["id"]}
+   query_param={"created_by_id":user["id"]}|dict(request.query_params)
+   query_param["parent_id"]=int(query_param["parent_id"])
+   output=await postgres_object.fetch_all(query=query,values=query_param)
+   #final
+   return {"status":1,"message":output}
+
+#comment
+from fastapi import Request
+@router.post("/{x}/action/comment")
+async def function_action_comment(request:Request,parent_table:str,parent_id:int,description:str,file_url:str=None,):
+   #database 
+   postgres_object=request.state.postgres_object
+   #auth check jwt
+   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
+   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #logic
+   query="insert into comment (created_by_id,parent_table,parent_id,description,file_url) values (:created_by_id,:parent_table,:parent_id,:description,:file_url) returning *;"
+   query_param={"created_by_id":user["id"]}|dict(request.query_params)
    query_param["parent_id"]=int(query_param["parent_id"])
    output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
