88a1c35ef7a4399579cccb40664f69c7dab8f990
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 18:21:34 +0530

    Update api_database.py

--- api_database.py
+++ api_database.py
@@ -19,8 +19,8 @@ async def function_database_qrunner(request:Request,query:str):
    if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
    #qrunner
    query=query
-   query_value={}
-   output=await postgres_object.fetch_all(query=query,values=query_value)
+   query_param={}
+   output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
    return output
 
@@ -35,14 +35,14 @@ async def function_database_clean(request:Request):
    #delete object having creator null
    for table in ["post","likes","bookmark","report","block","rating","comment","message"]:
       query=f"delete from {table} where created_by_id not in (select id from users);"
-      query_value={}
-      output=await postgres_object.fetch_all(query=query,values=query_value)
+      query_param={}
+      output=await postgres_object.fetch_all(query=query,values=query_param)
    #delete object having  parent null
    for table in ["likes","bookmark","report","block","rating","comment","message"]:
       for parent_table in ["users","post","comment"]:
          query=f"delete from {table} where parent_table='{parent_table}' and parent_id not in (select id from {parent_table});"
-         query_value={}
-         output=await postgres_object.fetch_all(query=query,values=query_value)
+         query_param={}
+         output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
    return {"status":1,"message":"done"}
 
@@ -60,19 +60,19 @@ async def function_database_init(request:Request):
    ]
    for item in config_database_extension:
       query=item
-      query_value={}
-      await postgres_object.fetch_all(query=query,values=query_value)
+      query_param={}
+      await postgres_object.fetch_all(query=query,values=query_param)
    #constraint name list
    query="select constraint_name from information_schema.constraint_column_usage;"
-   query_value={}
-   output=await postgres_object.fetch_all(query=query,values=query_value)
+   query_param={}
+   output=await postgres_object.fetch_all(query=query,values=query_param)
    constraint_name_list=[item["constraint_name"] for item in output]
    #create table
    config_database_table=["users","post","box","atom","likes","bookmark","report","block","rating","comment","message","helpdesk","otp"]
    for table in config_database_table:
       query=f"create table if not exists {table} (id bigint primary key generated always as identity);"
-      query_value={}
-      await postgres_object.fetch_all(query=query,values=query_value)
+      query_param={}
+      await postgres_object.fetch_all(query=query,values=query_param)
    #create column
    config_database_column={
    "created_at":["timestamptz",config_database_table],
@@ -110,18 +110,18 @@ async def function_database_init(request:Request):
    for k,v in config_database_column.items():
       for table in v[1]:
          query=f"alter table {table} add column if not exists {k} {v[0]};"
-         query_value={}
-         await postgres_object.fetch_all(query=query,values=query_value)
+         query_param={}
+         await postgres_object.fetch_all(query=query,values=query_param)
    #alter created_at default
    for table in config_database_table:
       query=f"alter table {table} alter column created_at set default now();"
-      query_value={}
-      await postgres_object.fetch_all(query=query,values=query_value)
+      query_param={}
+      await postgres_object.fetch_all(query=query,values=query_param)
    #alter protected rows
    for table in config_database_column["is_protected"][1]:
       query=f"create or replace rule rule_delete_disable_{table} as on delete to {table} where old.is_protected=1 do instead nothing;"
-      query_value={}
-      await postgres_object.fetch_all(query=query,values=query_value)
+      query_param={}
+      await postgres_object.fetch_all(query=query,values=query_param)
    #alter not null
    config_database_column_not_null={
    "parent_table":["likes","bookmark","report","block","rating","comment","message"],
@@ -130,8 +130,8 @@ async def function_database_init(request:Request):
    for k,v in config_database_column_not_null.items():
       for table in v:
          query=f"alter table {table} alter column {k} set not null;"
-         query_value={}
-         await postgres_object.fetch_all(query=query,values=query_value)
+         query_param={}
+         await postgres_object.fetch_all(query=query,values=query_param)
    #run extra query
    config_database_query=[
    "alter table users add constraint constraint_unique_users unique (username);",
@@ -147,8 +147,8 @@ async def function_database_init(request:Request):
          continue
       else:
          query=query
-         query_value={}
-         await postgres_object.fetch_all(query=query,values=query_value)
+         query_param={}
+         await postgres_object.fetch_all(query=query,values=query_param)
    #create index
    config_database_index={
    "type":"btree",
@@ -167,7 +167,7 @@ async def function_database_init(request:Request):
       for table in v[1]:
          if k in config_database_index:
             query=f"create index if not exists index_{k}_{table} on {table} using {config_database_index[k]} ({k});"
-            query_value={}
-            await postgres_object.fetch_all(query=query,values=query_value)
+            query_param={}
+            await postgres_object.fetch_all(query=query,values=query_param)
    #final
    return {"status":1,"message":"done"}
