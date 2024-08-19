ce9565afc07f78227843d9605f785979a9311ab0
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 08:44:20 +0530

    Update api_root.py

--- api_root.py
+++ api_root.py
@@ -1,11 +1,1 @@
-#router
-from fastapi import APIRouter
-router=APIRouter(tags=["root"])
 
-#root
-from postgres import postgres_object_dict
-from fastapi import Request
-@router.get("/")
-async def function_root(request:Request):
-   x_list=[*postgres_object_dict]
-   return {"status":1,"message":f"welcome to {x_list}"}
