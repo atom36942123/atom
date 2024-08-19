5c73771921d7b7f53497d31a80682d55ce20abf1
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 11:27:14 +0530

    Update api_auth.py

--- api_auth.py
+++ api_auth.py
@@ -91,37 +91,38 @@ async def function_auth_google(request:Request):
 
 #email
 from fastapi import Request
-from config import config_key_jwt
 from function import function_create_token
 @router.post("/{x}/auth/email")
 async def function_auth_email(request:Request):
+   #database
+   postgres_object=request.state.postgres_object
    #body
    body=await request.json()
    email=body["email"]
    otp=body["otp"]
    #verify otp
    query="select otp from otp where email=:email order by id desc limit 1;"
-   values={"email":email}
-   output=await postgres_object.fetch_all(query=query,values=values)
+   query_param_dict={"email":email}
+   output=await postgres_object.fetch_all(query=query,values=query_param_dict)
    if not output:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp not exist"}))
    if int(output[0]["otp"])!=int(otp):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp mismatch"}))
    #read user
    query="select * from users where email=:email order by id desc limit 1;"
-   values={"email":email}
-   output=await postgres_object.fetch_all(query=query,values=values)
+   query_param_dict={"email":email}
+   output=await postgres_object.fetch_all(query=query,values=query_param_dict)
    user=output[0] if output else None
    #create user
    if not user:
       query="insert into users (email) values (:email) returning *;"
-      values={"email":email}
-      output=await postgres_object.fetch_all(query=query,values=values)
+      query_param_dict={"email":email}
+      output=await postgres_object.fetch_all(query=query,values=query_param_dict)
       user_id=output[0]["id"]
       query="select * from users where id=:id;"
-      values={"id":user_id}
-      output=await postgres_object.fetch_all(query=query,values=values)
+      query_param_dict={"id":user_id}
+      output=await postgres_object.fetch_all(query=query,values=query_param_dict)
       user=output[0]
    #create token
-   response=await function_create_token(user,request,config_key_jwt)
+   response=await function_create_token(request,user)
    if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
    token=response["message"]
    #final
@@ -129,39 +130,38 @@ async def function_auth_email(request:Request):
 
 #mobile
 from fastapi import Request
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from config import config_key_jwt
 from function import function_create_token
 @router.post("/{x}/auth/mobile")
 async def function_auth_mobile(request:Request):
+   #database
+   postgres_object=request.state.postgres_object
    #body
    body=await request.json()
    mobile=body["mobile"]
    otp=body["otp"]
    #verify otp
    query="select otp from otp where mobile=:mobile order by id desc limit 1;"
-   values={"mobile":mobile}
-   output=await postgres_object.fetch_all(query=query,values=values)
+   query_param_dict={"mobile":mobile}
+   output=await postgres_object.fetch_all(query=query,values=query_param_dict)
    if not output:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp not exist"}))
    if int(output[0]["otp"])!=int(otp):return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"otp mismatch"}))
    #read user
    query="select * from users where mobile=:mobile order by id desc limit 1;"
-   values={"mobile":mobile}
-   output=await postgres_object.fetch_all(query=query,values=values)
+   query_param_dict={"mobile":mobile}
+   output=await postgres_object.fetch_all(query=query,values=query_param_dict)
    user=output[0] if output else None
    #create user
    if not user:
       query="insert into users (mobile) values (:mobile) returning *;"
-      values={"mobile":mobile}
-      output=await postgres_object.fetch_all(query=query,values=values)
+      query_param_dict={"mobile":mobile}
+      output=await postgres_object.fetch_all(query=query,values=query_param_dict)
       user_id=output[0]["id"]
       query="select * from users where id=:id;"
-      values={"id":user_id}
-      output=await postgres_object.fetch_all(query=query,values=values)
+      query_param_dict={"id":user_id}
+      output=await postgres_object.fetch_all(query=query,values=query_param_dict)
       user=output[0]
    #create token
-   response=await function_create_token(user,request,config_key_jwt)
+   response=await function_create_token(request,user)
    if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
    token=response["message"]
    #final
@@ -169,20 +169,21 @@ async def function_auth_mobile(request:Request):
 
 #refresh
 from fastapi import Request
-import jwt,json
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from config import config_key_jwt
 from function import function_create_token
+
+import jwt,json
+from config import config_key_jwt
 @router.get("/{x}/auth/refresh")
 async def function_auth_refresh(request:Request):
-   #token check
+   #database
+   postgres_object=request.state.postgres_object
+   #auth check
    user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
    if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
    #read user
    query="select * from users where id=:id;"
-   values={"id":user["id"]}
-   output=await postgres_object.fetch_all(query=query,values=values)
+   query_param_dict={"id":user["id"]}
+   output=await postgres_object.fetch_all(query=query,values=query_param_dict)
    user=output[0] if output else None
    if not user:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"no user"}))
    #create token
