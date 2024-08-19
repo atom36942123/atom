6beb088ac922333033b524d505bdabb0f264c987
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 21:35:21 +0530

    Update api_message.py

--- api_message.py
+++ api_message.py
@@ -65,7 +65,7 @@ async def function_thread(request:Request,background:BackgroundTasks,user_id:int
    #mark message status read
    query="update message set status=:status,updated_by_id=:updated_by_id,updated_at=:updated_at where parent_table='users' and created_by_id=:created_by_id and parent_id=:parent_id returning *;"
    query_param={"status":"read","created_by_id":user_id,"parent_id":user["id"],"updated_at":datetime.now(),"updated_by_id":user['id']}
-   background.add_task(await request.state.postgres_object.fetch_all(query=query,values=query_param))
+   background.add_task(await postgres_object.fetch_all(query=query,values=query_param))
    #final
    return {"status":1,"message":output}
 
