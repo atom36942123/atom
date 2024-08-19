096dec9636f3049fad95a23b31cb31b7f0bfa9e1
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 11:13:14 +0530

    Update api_database.py

--- api_database.py
+++ api_database.py
@@ -52,7 +52,7 @@ async def function_database_init(request:Request):
    postgres_object=request.state.postgres_object
    #auth
    if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token issue"}))
-   #extension
+   #create extension
    config_database_extension=[
    "create extension if not exists postgis;",
    ]
@@ -65,13 +65,13 @@ async def function_database_init(request:Request):
    query_param_dict={}
    output=await postgres_object.fetch_all(query=query,values=query_param_dict)
    constraint_name_list=[item["constraint_name"] for item in output]
-   #table
+   #create table
    config_database_table=["users","post","box","atom","likes","bookmark","report","block","rating","comment","message","helpdesk","otp"]
    for table in config_database_table:
       query=f"create table if not exists {table} (id bigint primary key generated always as identity);"
       query_param_dict={}
       await postgres_object.fetch_all(query=query,values=query_param_dict)
-   #column
+   #create column
    config_database_column={
    "created_at":["timestamptz",config_database_table],
    "created_by_id":["bigint",config_database_table],
@@ -110,17 +110,17 @@ async def function_database_init(request:Request):
          query=f"alter table {table} add column if not exists {k} {v[0]};"
          query_param_dict={}
          await postgres_object.fetch_all(query=query,values=query_param_dict)
-   #created_at default
+   #alter created_at default
    for table in config_database_table:
       query=f"alter table {table} alter column created_at set default now();"
       query_param_dict={}
       await postgres_object.fetch_all(query=query,values=query_param_dict)
-   #protected rows
+   #alter protected rows
    for table in config_database_column["is_protected"][1]:
       query=f"create or replace rule rule_delete_disable_{table} as on delete to {table} where old.is_protected=1 do instead nothing;"
       query_param_dict={}
       await postgres_object.fetch_all(query=query,values=query_param_dict)
-   #not null
+   #alter not null
    config_database_column_not_null={
    "parent_table":["likes","bookmark","report","block","rating","comment","message"],
    "parent_id":["likes","bookmark","report","block","rating","comment","message"]
@@ -130,7 +130,7 @@ async def function_database_init(request:Request):
          query=f"alter table {table} alter column {k} set not null;"
          query_param_dict={}
          await postgres_object.fetch_all(query=query,values=query_param_dict)
-   #query
+   #run extra query
    config_database_query=[
    "alter table users add constraint constraint_unique_users unique (username);",
    "alter table likes add constraint constraint_unique_likes unique (created_by_id,parent_table,parent_id);",
@@ -147,7 +147,7 @@ async def function_database_init(request:Request):
          query=query
          query_param_dict={}
          await postgres_object.fetch_all(query=query,values=query_param_dict)
-   #index
+   #create index
    config_database_index={
    "type":"btree",
    "is_verified":"btree",
