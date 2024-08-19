c00e946301f306aa3e302f4f2a7de8ed99877ee9
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-19 19:24:40 +0530

    Update api_object.py

--- api_object.py
+++ api_object.py
@@ -94,7 +94,7 @@ async def function_object_delete(request:Request,table:str,id:int):
 #read
 from fastapi import Request
 from function import function_sanitization_query_param_list
-from function import function_prepare_where_param
+from function import function_prepare_where
 @router.get("/{x}/object/read")
 async def function_object_read(request:Request,table:str,order:str="id desc",limit:int=100,page:int=1):
    #database 
@@ -107,7 +107,7 @@ async def function_object_read(request:Request,table:str,order:str="id desc",lim
    #prepare where
    where_param_raw={k:v for k,v in query_param.items() if k not in ["table","order","limit","page"]}
    where_param_raw["created_by_id"]=f"=,{user['id']}"
-   response=await function_prepare_where_param(where_param_raw)
+   response=await function_prepare_where(where_param_raw)
    if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
    where_string=response["message"][0]
    where_param=response["message"][1]
