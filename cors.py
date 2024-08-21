b34f3755e89703dcc2bffb268387a48133f763a3
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-21 20:02:38 +0530

    Delete cors.py

--- cors.py
+++
@@ -1,8 +0,0 @@
-#cors
-from app import app
-from fastapi.middleware.cors import CORSMiddleware
-app.add_middleware(
-  CORSMiddleware,allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],allow_headers=["*"]
-)
