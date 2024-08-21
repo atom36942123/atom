079329c2623fe90b85f3b6b298cbbc3fac9d5abe
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-21 20:05:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ if False:
   sentry_sdk.init(dsn=config_sentry_dsn,traces_sample_rate=1.0,profiles_sample_rate=1.0)
 
 #app
-from lifespan import function_lifespan
+from function import function_lifespan
 from fastapi import FastAPI
 app=FastAPI(lifespan=function_lifespan,title="atom")
 
