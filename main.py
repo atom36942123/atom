98f2e14ef31550828d21d2c7f1f915858bc0e726
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-21 22:14:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,9 @@ file_path_list=[item for item in glob.glob(f"{dir_path}/*.py")]
 file_name_list=[item.rsplit("/",1)[1].split(".")[0] for item in file_path_list]
 file_name_api_list=[item for item in file_name_list if "api" in item]
 print(file_name_api_list)
-for item in file_name_api_list:__import__(item)
+for item in file_name_api_list:
+ x=__import__(item)
+  
 
 #server start
 import uvicorn
