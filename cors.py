a1cf907a0e55eba5d3d491f033dd915c1edeb30d
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-21 20:01:51 +0530

    Update cors.py

--- cors.py
+++ cors.py
@@ -1,16 +1,8 @@
-#import
+#cors
 from app import app
 from fastapi.middleware.cors import CORSMiddleware
-
-#config
-cors_origins=["*"]
-cors_credentials=True
-cors_method=["*"]
-cors_header=["*"]
-
-#logic
 app.add_middleware(
-  CORSMiddleware,allow_origins=cors_origins,
-  allow_credentials=cors_credentials,
-  allow_methods=cors_method,allow_headers=cors_header
+  CORSMiddleware,allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],allow_headers=["*"]
 )
