5cef61da655aeb8290009802ff8d8630bd6afbbd
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-21 20:10:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,35 @@ from fastapi import FastAPI
 app=FastAPI(lifespan=function_lifespan,title="atom")
 
 #cors
-from app import app
 from fastapi.middleware.cors import CORSMiddleware
-app.add_middleware(
-  CORSMiddleware,allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],allow_headers=["*"]
-)
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
+
+#middleware
+from postgres import postgres_object_dict
+from fastapi import Request
+import traceback
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+@app.middleware("http")
+async def function_middleware(request:Request,api_function):
+  #key_4th
+  key_4th=str(request.url.path).split("/")[1]
+  #key_4th check
+  if key_4th not in ["","docs","openapi.json","redoc"]+[*postgres_object_dict]:
+    return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"wrong x"}))
+  #postgres object assgin
+  if key_4th in postgres_object_dict:
+    request.state.postgres_object=postgres_object_dict[key_4th]
+  #api response
+  try:
+    response=await api_function(request)
+  except Exception as e:
+    print(traceback.format_exc())
+    return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":e.args}))
+  #final
+  return response
 
 #app import
-from middleware import *
 from router import *
 
 #root api
@@ -27,5 +46,4 @@ from postgres import postgres_object_dict
 from fastapi import Request
 @app.get("/")
 async def function_root(request:Request):
-   x_list=[*postgres_object_dict]
-   return {"status":1,"message":f"welcome to {x_list}"}
+   return {"status":1,"message":f"welcome to {[*postgres_object_dict]}"}
