22bbf78239bf1dfef9a1f3346c6c11b2ee921cf1
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-22 12:41:02 +0530

    Update api_admin.py

--- api_admin.py
+++ api_admin.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from function import function_sanitization_query_param_list
 @router.put("/{x}/admin/update-cell")
 async def function_admin_update_cell(request:Request):
-   #postgres object 
+   #postgres object
    postgres_object=request.state.postgres_object
    #token check jwt
    response=await function_token_check_jwt(request)
@@ -19,12 +19,12 @@ async def function_admin_update_cell(request:Request):
    user=response["message"]
    #admin check
    if user["type"]!="admin":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"admin issue"}))
-   #body
-   body=await request.json()
-   table=body["table"]
-   id=body["id"]
-   column=body["column"]
-   value=body["value"]
+   #request body
+   request_body=await request.json()
+   table=request_body["table"]
+   id=request_body["id"]
+   column=request_body["column"]
+   value=request_body["value"]
    #sanitization query_param
    response=await function_sanitization_query_param_list(postgres_object,"update",[{column:value}])
    if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
