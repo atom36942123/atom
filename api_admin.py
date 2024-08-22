c9a9a596721630e2aaec51c1572d243f84bcb1c0
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-22 09:30:41 +0530

    Update api_admin.py

--- api_admin.py
+++ api_admin.py
@@ -13,9 +13,10 @@ from function import function_sanitization_query_param_list
 async def function_admin_update_cell(request:Request):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #admin check
    if user["type"]!="admin":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"admin issue"}))
    #body
