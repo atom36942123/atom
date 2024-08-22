6edc1d6e6edd5004fa905ddbb5b434aa19d3bb3b
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-22 18:24:37 +0530

    Update api_aws.py

--- api_aws.py
+++ api_aws.py
@@ -51,9 +51,12 @@ from fastapi.encoders import jsonable_encoder
 async def function_aws_delete_s3_url(request:Request,url:str):
    #postgres object
    postgres_object=request.state.postgres_object
-   #token check root
-   response=await function_token_check_root(request)
+   #token check
+   response=await function_token_check(request)
    if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
+   #admin check
+   if user["type"]!="admin":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"admin issue"}))
    #buckey key
    buckey_key=url.rsplit("/",1)[1]
    #logic
@@ -73,9 +76,12 @@ from fastapi.encoders import jsonable_encoder
 async def function_aws_empty_s3_bucket(request:Request,bucket_name:str):
    #postgres object
    postgres_object=request.state.postgres_object
-   #token check root
-   response=await function_token_check_root(request)
+   #token check
+   response=await function_token_check(request)
    if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
+   #admin check
+   if user["type"]!="admin":return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"admin issue"}))
    #logic
    s3_resource=boto3.resource("s3",aws_access_key_id=config_aws_access_key_id,aws_secret_access_key=config_aws_secret_access_key)
    output=s3_resource.Bucket(bucket_name).objects.all().delete()
