6df05d73d742097a70ba09211f4ba5ace8e50b04
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-22 08:12:08 +0530

    Update postgres.py

--- postgres.py
+++ postgres.py
@@ -1,8 +1,6 @@
-#import
+#postgres
 from config import config_postgres_database_uri
 from databases import Database
-
-#logic
 postgres_object_dict={}
 postgres_url_list=config_postgres_database_uri.split(",")
 for item in postgres_url_list:
