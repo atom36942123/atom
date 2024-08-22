d725b6b85c4a33743b5b2773f6c0b697de2e38dc
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-22 17:49:33 +0530

    Update api_admin.py

--- api_admin.py
+++ api_admin.py
@@ -59,3 +59,29 @@ async def function_admin_delete_bulk(request:Request,table:str,ids:str):
    output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
    return {"status":1,"message":output}
+
+#database clean
+from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+@router.get("/{x}/database/clean")
+async def function_database_clean(request:Request):
+   #postgres object
+   postgres_object=request.state.postgres_object
+   #token check root
+   response=await function_token_check_root(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   #delete object having creator null
+   for table in ["post","likes","bookmark","report","block","rating","comment","message"]:
+      query=f"delete from {table} where created_by_id not in (select id from users);"
+      query_param={}
+      output=await postgres_object.fetch_all(query=query,values=query_param)
+   #delete object having  parent null
+   for table in ["likes","bookmark","report","block","rating","comment","message"]:
+      for parent_table in ["users","post","comment"]:
+         query=f"delete from {table} where parent_table='{parent_table}' and parent_id not in (select id from {parent_table});"
+         query_param={}
+         output=await postgres_object.fetch_all(query=query,values=query_param)
+   #final
+   return {"status":1,"message":"done"}
