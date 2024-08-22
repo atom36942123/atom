5cc33e9025bf871a3e28b3c57760738ad6ca34f9
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-22 09:25:55 +0530

    Update api_action.py

--- api_action.py
+++ api_action.py
@@ -24,13 +24,17 @@ async def function_action_post(request:Request,description:str,title:str=None,fi
 
 #helpdesk
 from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/helpdesk")
 async def function_action_helpdesk(request:Request,type:str,description:str):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #logic
    query="insert into helpdesk (created_by_id,type,description) values (:created_by_id,:type,:description) returning *;"
    query_param={"created_by_id":user["id"]}|dict(request.query_params)
@@ -40,13 +44,17 @@ async def function_action_helpdesk(request:Request,type:str,description:str):
 
 #like
 from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/like")
 async def function_action_like(request:Request,parent_table:str,parent_id:int):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #logic
    query="insert into likes (created_by_id,parent_table,parent_id) values (:created_by_id,:parent_table,:parent_id) returning *;"
    query_param={"created_by_id":user["id"]}|dict(request.query_params)
@@ -57,13 +65,17 @@ async def function_action_like(request:Request,parent_table:str,parent_id:int):
 
 #bookmark
 from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/bookmark")
 async def function_action_bookmark(request:Request,parent_table:str,parent_id:int):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #logic
    query="insert into bookmark (created_by_id,parent_table,parent_id) values (:created_by_id,:parent_table,:parent_id) returning *;"
    query_param={"created_by_id":user["id"]}|dict(request.query_params)
@@ -74,13 +86,17 @@ async def function_action_bookmark(request:Request,parent_table:str,parent_id:in
 
 #block
 from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/block")
 async def function_action_block(request:Request,parent_table:str,parent_id:int,description:str=None):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #logic
    query="insert into block (created_by_id,parent_table,parent_id,description) values (:created_by_id,:parent_table,:parent_id,:description) returning *;"
    query_param={"created_by_id":user["id"]}|dict(request.query_params)
@@ -91,13 +107,17 @@ async def function_action_block(request:Request,parent_table:str,parent_id:int,d
 
 #comment
 from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/comment")
 async def function_action_comment(request:Request,parent_table:str,parent_id:int,description:str,file_url:str=None,):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #logic
    query="insert into comment (created_by_id,parent_table,parent_id,description,file_url) values (:created_by_id,:parent_table,:parent_id,:description,:file_url) returning *;"
    query_param={"created_by_id":user["id"]}|dict(request.query_params)
@@ -108,13 +128,17 @@ async def function_action_comment(request:Request,parent_table:str,parent_id:int
 
 #message
 from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/message")
 async def function_action_message(request:Request,parent_table:str,parent_id:int,description:str):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #logic
    query="insert into message (created_by_id,parent_table,parent_id,description) values (:created_by_id,:parent_table,:parent_id,:description) returning *;"
    query_param={"created_by_id":user["id"]}|dict(request.query_params)
@@ -125,13 +149,17 @@ async def function_action_message(request:Request,parent_table:str,parent_id:int
 
 #rating
 from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/rating")
 async def function_action_rating(request:Request,parent_table:str,parent_id:int,rating:str):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #logic
    query="insert into rating (created_by_id,parent_table,parent_id,rating) values (:created_by_id,:parent_table,:parent_id,:rating) returning *;"
    query_param={"created_by_id":user["id"]}|dict(request.query_params)
@@ -143,13 +171,17 @@ async def function_action_rating(request:Request,parent_table:str,parent_id:int,
 
 #report
 from fastapi import Request
+from function import function_token_check_jwt
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 @router.post("/{x}/action/report")
 async def function_action_report(request:Request,parent_table:str,parent_id:int,description:str=None):
    #postgres object 
    postgres_object=request.state.postgres_object
-   #auth check jwt
-   user=json.loads(jwt.decode(request.headers.get("Authorization").split(" ",1)[1],config_key_jwt,algorithms="HS256")["data"])
-   if user["x"]!=str(request.url.path).split("/")[1]:return JSONResponse(status_code=400,content=jsonable_encoder({"status":0,"message":"token x mismatch"}))
+   #token check jwt
+   response=await function_token_check_jwt(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=jsonable_encoder(response))
+   user=response["message"]
    #logic
    query="insert into report (created_by_id,parent_table,parent_id,description) values (:created_by_id,:parent_table,:parent_id,:description) returning *;"
    query_param={"created_by_id":user["id"]}|dict(request.query_params)
