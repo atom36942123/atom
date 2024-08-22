3774b053138c1fb9ef594722bdb500b4aea1aecc
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-22 08:10:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ env=Env()
 env.read_env()
 
 #env unpack
-config_postgres_database_uri=env("config_postgres_database_uri")
-config_mongo_server_uri=env("config_mongo_server_uri")
-config_redis_server_uri=env("config_redis_server_uri")
+config_postgres_database=env("config_postgres_database")
+config_mongo_server=env("config_mongo_server")
+config_redis_server=env("config_redis_server")
 config_sentry_dsn=env("config_sentry_dsn")
 config_key_root=env("config_key_root")
 config_key_jwt=env("config_key_jwt")
@@ -19,6 +19,3 @@ config_ses_region_name=env("config_ses_region_name")
 config_elasticsearch_username=env("config_elasticsearch_username")
 config_elasticsearch_password=env("config_elasticsearch_password")
 config_elasticsearch_cloud_id=env("config_elasticsearch_cloud_id")
-
-
-
