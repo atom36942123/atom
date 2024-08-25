bea42ffa09d5477d54cef5ab1632373b57108669
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-25 08:38:43 +0530

    Update api_root.py

--- api_root.py
+++ api_root.py
@@ -2,6 +2,56 @@
 from fastapi import APIRouter
 router=APIRouter(tags=["root"])
 
+#config
+config_database_extension=["create extension if not exists postgis;"]
+config_database_table=["users","post","box","atom","likes","bookmark","report","block","rating","comment","message","helpdesk","otp"]
+config_database_column={
+"created_at":["timestamptz",config_database_table],
+"created_by_id":["bigint",config_database_table],
+"updated_at":["timestamptz",["users","post","box","atom","report","comment","message","helpdesk"]],
+"updated_by_id":["bigint",["users","post","box","atom","report","comment","message","helpdesk"]],
+"is_active":["int",["users","post"]],
+"is_verified":["int",["users","post"]],
+"is_protected":["int",["users","post","box","atom"]],
+"parent_table":["text",["likes","bookmark","report","block","rating","comment","message"]],
+"parent_id":["bigint",["likes","bookmark","report","block","rating","comment","message"]],
+"type":["text",["users","post","box","atom","helpdesk"]],
+"status":["text",["report","helpdesk","message"]],
+"remark":["text",["report","helpdesk"]],
+"rating":["numeric",["rating","atom"]],
+"metadata":["jsonb",["users","post","box","atom"]],
+"username":["text",["users"]],
+"password":["text",["users"]],
+"google_id":["text",["users"]],
+"profile_pic_url":["text",["users"]],
+"last_active_at":["timestamptz",["users"]],
+"name":["text",["users"]],
+"email":["text",["users","post","box","atom","otp","helpdesk"]],
+"mobile":["text",["users","post","box","atom","otp","helpdesk"]],
+"date_of_birth":["date",["users"]],
+"title":["text",["users","post","box","atom"]],
+"description":["text",["users","post","box","atom","report","block","comment","message","helpdesk"]],
+"file_url":["text",["post","box","atom","comment","message"]],
+"link_url":["text",["post","box","atom"]],
+"tag":["text",["users","post","box","atom"]],
+"otp":["int",["otp"]],
+"tag_array":["text[]",["atom"]],
+"location":["geography(POINT)",["users","post","box","atom"]],
+}
+config_database_column_not_null={
+"parent_table":["likes","bookmark","report","block","rating","comment","message"],
+"parent_id":["likes","bookmark","report","block","rating","comment","message"]
+}
+config_database_query=[
+"alter table users add constraint constraint_unique_users unique (username);",
+"alter table likes add constraint constraint_unique_likes unique (created_by_id,parent_table,parent_id);",
+"alter table bookmark add constraint constraint_unique_bookmark unique (created_by_id,parent_table,parent_id);",
+"alter table report add constraint constraint_unique_report unique (created_by_id,parent_table,parent_id);",
+"alter table block add constraint constraint_unique_block unique (created_by_id,parent_table,parent_id);",
+"insert into users (username,password,type,is_protected) values ('root','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','admin',1) on conflict do nothing;",
+"create or replace rule rule_delete_disable_root_user as on delete to users where old.id=1 do instead nothing;",
+]
+
 #database init
 from fastapi import Request
 from config import postgres_object
@@ -11,93 +61,22 @@ from fastapi.responses import JSONResponse
 async def function_root_database_init(request:Request):
    #auth check
    if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content={"status":0,"message":"token root issue"})
-   #create extension
-   config_database_extension=[
-   "create extension if not exists postgis;",
-   ]
-   for item in config_database_extension:
-      query=item
-      query_param={}
-      await postgres_object.fetch_all(query=query,values=query_param)
-   #create table
-   config_database_table=["users","post","box","atom","likes","bookmark","report","block","rating","comment","message","helpdesk","otp"]
-   for table in config_database_table:
-      query=f"create table if not exists {table} (id bigint primary key generated always as identity);"
-      query_param={}
-      await postgres_object.fetch_all(query=query,values=query_param)
-   #create column
-   config_database_column={
-   "created_at":["timestamptz",config_database_table],
-   "created_by_id":["bigint",config_database_table],
-   "updated_at":["timestamptz",["users","post","box","atom","report","comment","message","helpdesk"]],
-   "updated_by_id":["bigint",["users","post","box","atom","report","comment","message","helpdesk"]],
-   "is_active":["int",["users","post"]],
-   "is_verified":["int",["users","post"]],
-   "is_protected":["int",["users","post","box","atom"]],
-   "parent_table":["text",["likes","bookmark","report","block","rating","comment","message"]],
-   "parent_id":["bigint",["likes","bookmark","report","block","rating","comment","message"]],
-   "type":["text",["users","post","box","atom","helpdesk"]],
-   "status":["text",["report","helpdesk","message"]],
-   "remark":["text",["report","helpdesk"]],
-   "rating":["numeric",["rating","atom"]],
-   "metadata":["jsonb",["users","post","box","atom"]],
-   "username":["text",["users"]],
-   "password":["text",["users"]],
-   "google_id":["text",["users"]],
-   "profile_pic_url":["text",["users"]],
-   "last_active_at":["timestamptz",["users"]],
-   "name":["text",["users"]],
-   "email":["text",["users","post","box","atom","otp","helpdesk"]],
-   "mobile":["text",["users","post","box","atom","otp","helpdesk"]],
-   "date_of_birth":["date",["users"]],
-   "title":["text",["users","post","box","atom"]],
-   "description":["text",["users","post","box","atom","report","block","comment","message","helpdesk"]],
-   "file_url":["text",["post","box","atom","comment","message"]],
-   "link_url":["text",["post","box","atom"]],
-   "tag":["text",["users","post","box","atom"]],
-   "otp":["int",["otp"]],
-   "tag_array":["text[]",["atom"]],
-   "location":["geography(POINT)",["users","post","box","atom"]],
-   }
-   for k,v in config_database_column.items():
-      for table in v[1]:
-         query=f"alter table {table} add column if not exists {k} {v[0]};"
-         query_param={}
-         await postgres_object.fetch_all(query=query,values=query_param)
-   #alter created_at default
-   for table in config_database_table:
-      query=f"alter table {table} alter column created_at set default now();"
-      query_param={}
-      await postgres_object.fetch_all(query=query,values=query_param)
-   #alter protected rows
-   for table in config_database_column["is_protected"][1]:
-      query=f"create or replace rule rule_delete_disable_{table} as on delete to {table} where old.is_protected=1 do instead nothing;"
-      query_param={}
-      await postgres_object.fetch_all(query=query,values=query_param)
-   #alter not null
-   config_database_column_not_null={
-   "parent_table":["likes","bookmark","report","block","rating","comment","message"],
-   "parent_id":["likes","bookmark","report","block","rating","comment","message"]
-   }
-   for k,v in config_database_column_not_null.items():
-      for table in v:
-         query=f"alter table {table} alter column {k} set not null;"
-         query_param={}
-         await postgres_object.fetch_all(query=query,values=query_param)
+   #create
+   for item in config_database_extension:await postgres_object.fetch_all(query=item,values={})
+   for item in config_database_table:await postgres_object.fetch_all(query=f"create table if not exists {item} (id bigint primary key generated always as identity);",values={})
+   [await postgres_object.fetch_all(query=f"alter table {table} add column if not exists {k} {v[0]};",values={}) for k,v in config_database_column.items() for table in v[1]]
+   #alter   
+   for item in config_database_table:await postgres_object.fetch_all(query=f"alter table {item} alter column created_at set default now();",values={})
+   for item in config_database_column["is_protected"][1]:await postgres_object.fetch_all(query=f"create or replace rule rule_delete_disable_{item} as on delete to {item} where old.is_protected=1 do instead nothing;",values={})
+   [await postgres_object.fetch_all(query=f"alter table {table} alter column {k} set not null;",values={}) for k,v in config_database_column_not_null.items() for table in v]
+      
+         
    #run extra query
    query="select constraint_name from information_schema.constraint_column_usage;"
    query_param={}
    output=await postgres_object.fetch_all(query=query,values=query_param)
    constraint_name_list=[item["constraint_name"] for item in output]
-   config_database_query=[
-   "alter table users add constraint constraint_unique_users unique (username);",
-   "alter table likes add constraint constraint_unique_likes unique (created_by_id,parent_table,parent_id);",
-   "alter table bookmark add constraint constraint_unique_bookmark unique (created_by_id,parent_table,parent_id);",
-   "alter table report add constraint constraint_unique_report unique (created_by_id,parent_table,parent_id);",
-   "alter table block add constraint constraint_unique_block unique (created_by_id,parent_table,parent_id);",
-   "insert into users (username,password,type,is_protected) values ('root','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','admin',1) on conflict do nothing;",
-   "create or replace rule rule_delete_disable_root_user as on delete to users where old.id=1 do instead nothing;",
-   ]
+   
    for query in config_database_query:
       if "add constraint" in query and query.split()[5] in constraint_name_list:
          continue
