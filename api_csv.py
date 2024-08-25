b84d9dc630c9de22584e91c4fc8d38cda86d908d
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-25 15:33:39 +0530

    Update api_csv.py

--- api_csv.py
+++ api_csv.py
@@ -36,27 +36,9 @@ async def function_csv_create(request:Request,table:str,file:UploadFile):
    return {"status":1,"message":output}
 
 #update
-from fastapi import Request
-from config import postgres_object
-from fastapi import UploadFile
-import csv,codecs
-from function import function_token_check
-from function import function_sanitization_query_param_list
-from fastapi.responses import JSONResponse
 @router.put("/csv/update")
 async def function_csv_update(request:Request,table:str,file:UploadFile):
-   #auth check
-   response=await function_token_check(request)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   user=response["message"]
-   #admin check
-   if user["type"]!="admin":return JSONResponse(status_code=400,content={"status":0,"message":"admin issue"})
-   #file extension check
-   if file.content_type!="text/csv":return JSONResponse(status_code=400,content={"status":0,"message":"file extension issue"})
-   #file read
-   file_row_list=[]
-   file_csv=csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
-   for row in file_csv:file_row_list.append(row)
+
    #column_to_update_list
    column_to_update_list=[*file_row_list[0]]
    column_to_update_list.remove("id")
@@ -77,26 +59,8 @@ async def function_csv_update(request:Request,table:str,file:UploadFile):
 
 
 #read
-from fastapi import Request
-from config import postgres_object
-from fastapi import UploadFile
-import csv,codecs
-from function import function_token_check
-from fastapi.responses import JSONResponse
 @router.get("/csv/read")
 async def function_csv_read(request:Request,table:str,file:UploadFile):
-   #auth check
-   response=await function_token_check(request)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   user=response["message"]
-   #admin check
-   if user["type"]!="admin":return JSONResponse(status_code=400,content={"status":0,"message":"admin issue"})
-   #file extension check
-   if file.content_type!="text/csv":return JSONResponse(status_code=400,content={"status":0,"message":"file extension issue"})
-   #file read
-   file_row_list=[]
-   file_csv=csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
-   for row in file_csv:file_row_list.append(row)
    #logic
    ids_list=[str(item["id"]) for item in file_row_list]
    ids_to_read=','.join(ids_list)
@@ -109,26 +73,9 @@ async def function_csv_read(request:Request,table:str,file:UploadFile):
    return {"status":1,"message":output}
 
 #delete
-from fastapi import Request
-from config import postgres_object
-from fastapi import UploadFile
-import csv,codecs
-from function import function_token_check
-from fastapi.responses import JSONResponse
+
 @router.delete("/csv/delete")
 async def function_csv_delete(request:Request,table:str,file:UploadFile):
-   #auth check
-   response=await function_token_check(request)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   user=response["message"]
-   #admin check
-   if user["type"]!="admin":return JSONResponse(status_code=400,content={"status":0,"message":"admin issue"})
-   #file extension check
-   if file.content_type!="text/csv":return JSONResponse(status_code=400,content={"status":0,"message":"file extension issue"})
-   #file read
-   file_row_list=[]
-   file_csv=csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
-   for row in file_csv:file_row_list.append(row)
    #query set
    query=f"delete from {table} where id=:id;"
    query_param_list=file_row_list
