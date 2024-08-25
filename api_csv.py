4965772e73fa487afe382983cfb91f7069c4e62a
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-25 16:19:53 +0530

    Update api_csv.py

--- api_csv.py
+++ api_csv.py
@@ -17,75 +17,16 @@ async def function_csv_create(request:Request,table:str,file:UploadFile):
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    user=response["message"]
    if user["type"]!="admin":return JSONResponse(status_code=400,content={"status":0,"message":"admin issue"})
+   #final
+   return {"status":1,"message":output}
 
-   #column_to_insert_list
+
    
-   #query set
-   query=f"insert into {table} ({','.join(column_to_insert_list)}) values ({','.join([':'+item for item in column_to_insert_list])}) returning *;"
-   #query_param set
-   query_param_list=file_row_list
-   #sanitization query_param_list
-   response=await function_sanitization_query_param_list(postgres_object,"create",query_param_list)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   query_param_list=response["message"]
-   #query run
-   output=await postgres_object.execute_many(query=query,values=query_param_list)
-   #file close
-   await file.close()
-   #final
-   return {"status":1,"message":output}
-
-#update
-@router.put("/csv/update")
-async def function_csv_update(request:Request,table:str,file:UploadFile):
-
-   #column_to_update_list
-   column_to_update_list=[*file_row_list[0]]
-   column_to_update_list.remove("id")
-   #query set
-   query=f"update {table} set {','.join([f'{item}=coalesce(:{item},{item})' for item in column_to_update_list])} where id=:id returning *;"
-   #query_param set
-   query_param_list=file_row_list
-   #sanitization query_param_list
-   response=await function_sanitization_query_param_list(postgres_object,"create",query_param_list)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   query_param_list=response["message"]
-   #query run
-   output=await postgres_object.execute_many(query=query,values=query_param_list)
-   #file close
-   await file.close()
-   #final
-   return {"status":1,"message":output}
+   
 
 
-#read
-@router.get("/csv/read")
-async def function_csv_read(request:Request,table:str,file:UploadFile):
-   #logic
-   ids_list=[str(item["id"]) for item in file_row_list]
-   ids_to_read=','.join(ids_list)
-   query=f"select * from {table} where id in ({ids_to_read}) order by id desc;"
-   query_param={}
-   output=await postgres_object.fetch_all(query=query,values=query_param)
-   #file close
-   await file.close()
-   #final
-   return {"status":1,"message":output}
 
-#delete
 
-@router.delete("/csv/delete")
-async def function_csv_delete(request:Request,table:str,file:UploadFile):
-   #query set
-   query=f"delete from {table} where id=:id;"
-   query_param_list=file_row_list
-   #sanitization query_param_list
-   response=await function_sanitization_query_param_list(postgres_object,"create",query_param_list)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   query_param_list=response["message"]
-   #query run
-   output=await postgres_object.execute_many(query=query,values=query_param_list)
-   #file close
-   await file.close()
-   #final
-   return {"status":1,"message":output}
+
+
+
