7ffa55b7d0ba958dea019f05525bbcb917f2fbf4
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-25 16:22:30 +0530

    Update api_admin.py

--- api_admin.py
+++ api_admin.py
@@ -101,6 +101,28 @@ async def function_admin_database_init(request:Request):
    #final
    return {"status":1,"message":"done"}
 
+#create
+from fastapi import Request
+from config import postgres_object
+from fastapi import UploadFile
+from fastapi import Depends
+from fastapi_limiter.depends import RateLimiter
+from function import function_token_check
+from function import function_csv
+from fastapi.responses import JSONResponse
+@router.post("/csv/create",dependencies=[Depends(RateLimiter(times=1,seconds=3))])
+async def function_csv_create(request:Request,table:str,file:UploadFile):
+   #auth check
+   response=await function_token_check(request)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   user=response["message"]
+   if user["type"]!="admin":return JSONResponse(status_code=400,content={"status":0,"message":"admin issue"})
+   #final
+   return {"status":1,"message":output}
+
+function_csv(postgres_object,file,mode):
+
+
 #update cell
 from fastapi import Request
 from config import postgres_object
