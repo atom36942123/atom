b68f98f99b87162774cfbb15a25f3a6151e4ccc4
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-25 11:51:57 +0530

    Update api_admin.py

--- api_admin.py
+++ api_admin.py
@@ -18,10 +18,10 @@ async def function_admin_database_init(request:Request):
    [await postgres_object.fetch_all(query=f"alter table {table} add column if not exists {k} {v[0]};",values={}) for k,v in config_database_column.items() for table in v[1]]
    [await postgres_object.fetch_all(query=f"create index if not exists index_{k}_{table} on {table} using {config_database_index[k]} ({k});",values={}) for k,v in config_database_column.items() for table in v[1] if k in config_database_index]
    #alter
+   [await postgres_object.fetch_all(query=f"alter table {table} alter column {k} set not null;",values={}) for k,v in config_database_column_not_null.items() for table in v]
    for item in config_database_table:await postgres_object.fetch_all(query=f"alter table {item} alter column id add generated always as identity;",values={})
    for item in config_database_table:await postgres_object.fetch_all(query=f"alter table {item} alter column created_at set default now();",values={})
    for item in config_database_column["is_protected"][1]:await postgres_object.fetch_all(query=f"create or replace rule rule_delete_disable_{item} as on delete to {item} where old.is_protected=1 do instead nothing;",values={})
-   [await postgres_object.fetch_all(query=f"alter table {table} alter column {k} set not null;",values={}) for k,v in config_database_column_not_null.items() for table in v]
    #query misc
    output=await postgres_object.fetch_all(query="select constraint_name from information_schema.constraint_column_usage;",values={})
    schema_constraint_name_list=[item["constraint_name"] for item in output]
