71ad5af7a94329d1d4e70cc3b457e84c8550856f
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-25 16:19:35 +0530

    Update function.py

--- function.py
+++ function.py
@@ -3,6 +3,7 @@ import csv,codecs
 from function import function_sanitization_query_param_list
 async def function_csv(postgres_object,file,mode):
   try:
+    if mode not in ["create","update"]:return {"status":0,"message":"wrong mode"}
     if file.content_type!="text/csv":return {"status":0,"message":"file must be csv"}
     file_csv=csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
     file_row_list=[]
@@ -11,25 +12,18 @@ async def function_csv(postgres_object,file,mode):
       column_to_insert_list=[*file_row_list[0]]
       query=f"insert into {table} ({','.join(column_to_insert_list)}) values ({','.join([':'+item for item in column_to_insert_list])}) returning *;"
       query_param=file_row_list
-      response=await function_sanitization_query_param_list(postgres_object,"create",query_param_list)
-      if response["status"]==0:return response
-      query_param=response["message"]
-      output=await postgres_object.execute_many(query=query,values=query_param)
-      await file.close()
-   if mode=="update":
-     column_to_update_list=[*file_row_list[0]]
-     column_to_update_list.remove("id")
-     query=f"update {table} set {','.join([f'{item}=coalesce(:{item},{item})' for item in column_to_update_list])} where id=:id returning *;"
-     query_param=file_row_list
-     response=await function_sanitization_query_param_list(postgres_object,"update",query_param_list)
-     if response["status"]==0:return response
-     query_param_list=response["message"]
-     output=await postgres_object.execute_many(query=query,values=query_param_list)
-     await file.close()
-
-     
-  except Exception as e:return {"status":0,"message":e.args}
-  return {"status":1,"message":"done"}
+    if mode=="update":
+      column_to_update_list=[*file_row_list[0]]
+      column_to_update_list.remove("id")
+      query=f"update {table} set {','.join([f'{item}=coalesce(:{item},{item})' for item in column_to_update_list])} where id=:id returning *;"
+      query_param=file_row_list
+   response=await function_sanitization_query_param_list(postgres_object,"create",query_param_list)
+   if response["status"]==0:return response
+   query_param=response["message"]
+   output=await postgres_object.execute_many(query=query,values=query_param)
+   await file.close()
+   except Exception as e:return {"status":0,"message":e.args}
+   return {"status":1,"message":"done"}
 
 #delete index all
 async def function_delete_index_all(postgres_object):
