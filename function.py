ceb6172c61af361aa524fc872851b818fa02ffcb
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-26 07:07:12 +0530

    Update function.py

--- function.py
+++ function.py
@@ -22,16 +22,17 @@ async def function_csv(postgres_object,mode,table,file,function_sanitization_que
   await file.close()
   return {"status":1,"message":"done"}
 
-#delete index all
-async def function_delete_index_all(postgres_object):
-  query="select 'drop index ' || string_agg(i.indexrelid::regclass::text,', ' order by n.nspname,i.indrelid::regclass::text, cl.relname) as output from pg_index i join pg_class cl ON cl.oid = i.indexrelid join pg_namespace n ON n.oid = cl.relnamespace left join pg_constraint co ON co.conindid = i.indexrelid where  n.nspname <> 'information_schema' and n.nspname not like 'pg\_%' and co.conindid is null and not i.indisprimary and not i.indisunique and not i.indisexclusion and not i.indisclustered and not i.indisreplident;"
-  query_param={}
-  output=await postgres_object.fetch_all(query=query,values=query_param)
-  if output[0]["output"]:
-    query=output[0]["output"]
-    query_param={}
-    output=await postgres_object.fetch_all(query=query,values=query_param)
-  return {"status":1,"message":"done"}
+#token check
+import jwt,json
+from config import config_key_jwt
+async def function_token_check(request):
+  header_authorization=request.headers.get("Authorization")
+  if not header_authorization:return {"status":0,"message":"authorization header is must"}
+  token=request.headers.get("Authorization").split(" ",1)[1]
+  payload=jwt.decode(token,config_key_jwt,algorithms="HS256")
+  data=payload["data"]
+  user=json.loads(data)
+  return {"status":1,"message":user}
 
 #token create
 import jwt,json,time
@@ -46,24 +47,40 @@ async def function_token_create(user):
   token=jwt.encode(payload,config_key_jwt)
   return {"status":1,"message":token}
 
-#token check
-import jwt,json
-from config import config_key_jwt
-async def function_token_check(request):
-  header_authorization=request.headers.get("Authorization")
-  if not header_authorization:return {"status":0,"message":"authorization header is must"}
-  token=request.headers.get("Authorization").split(" ",1)[1]
-  payload=jwt.decode(token,config_key_jwt,algorithms="HS256")
-  data=payload["data"]
-  user=json.loads(data)
-  return {"status":1,"message":user}
-  
 #redis key
 from fastapi import Request,Response
 def function_read_redis_key(func,namespace:str="",*,request:Request=None,response:Response=None,**kwargs):
   param=[repr(sorted(request.query_params.items())),namespace,request.method.lower(),request.url.path]
   param=":".join(param)
   return param
+
+#prepare where
+async def function_prepare_where(where_param_raw):
+  where_param={k:v.split(',',1)[1] for k,v in where_param_raw.items()}
+  where_param_operator={k:v.split(',',1)[0] for k,v in where_param_raw.items()}
+  key_list=[f"({k} {where_param_operator[k]} :{k} or :{k} is null)" for k,v in where_param.items()]
+  key_joined=' and '.join(key_list)
+  where_string=f"where {key_joined}" if key_joined else ""
+  return {"status":1,"message":[where_string,where_param]}
+
+#sanitization
+import hashlib,json
+from datetime import datetime
+from config import config_database_column
+async def function_sanitization_query_param_list(postgres_object,query_type,query_param_list):
+  if query_type not in ["create","update","read"]:return {"status":0,"message":"wrong query_type"}
+  for index,object in enumerate(query_param_list):
+    for k,v in object.items():
+      datatype=config_database_column[k][0]
+      if query_type in ["create","read","update"]:
+        if k in ["password","google_id"]:query_param_list[index][k]=hashlib.sha256(v.encode()).hexdigest() if v else None
+        if datatype in ["bigint","int"]:query_param_list[index][k]=int(v) if v else None
+        if datatype in ["numeric"]:query_param_list[index][k]=round(float(v),3) if v else None
+        if datatype in ["timestamptz","date"]:query_param_list[index][k]=datetime.strptime(v,'%Y-%m-%dT%H:%M:%S') if v else None
+        if "[]" in datatype:query_param_list[index][k]=v.split(",") if v else None
+      if query_type in ["create","update"]:
+        if datatype in ["jsonb"]:query_param_list[index][k]=json.dumps(v) if v else None
+  return {"status":1,"message":query_param_list}
 
 #creator key
 async def function_add_creator_key(postgres_object,object_list):
@@ -97,31 +114,14 @@ async def function_add_action_count(postgres_object,object_list,object_table,act
           x[key_name]=y["count"]
           break
   return {"status":1,"message":object_list}
-
-#prepare where
-async def function_prepare_where(where_param_raw):
-  where_param={k:v.split(',',1)[1] for k,v in where_param_raw.items()}
-  where_param_operator={k:v.split(',',1)[0] for k,v in where_param_raw.items()}
-  key_list=[f"({k} {where_param_operator[k]} :{k} or :{k} is null)" for k,v in where_param.items()]
-  key_joined=' and '.join(key_list)
-  where_string=f"where {key_joined}" if key_joined else ""
-  return {"status":1,"message":[where_string,where_param]}
-
-#sanitization
-import hashlib,json
-from datetime import datetime
-from config import config_database_column
-async def function_sanitization_query_param_list(postgres_object,query_type,query_param_list):
-  if query_type not in ["create","update","read"]:return {"status":0,"message":"wrong query_type"}
-  for index,object in enumerate(query_param_list):
-    for k,v in object.items():
-      datatype=config_database_column[k][0]
-      if query_type in ["create","read","update"]:
-        if k in ["password","google_id"]:query_param_list[index][k]=hashlib.sha256(v.encode()).hexdigest() if v else None
-        if datatype in ["bigint","int"]:query_param_list[index][k]=int(v) if v else None
-        if datatype in ["numeric"]:query_param_list[index][k]=round(float(v),3) if v else None
-        if datatype in ["timestamptz","date"]:query_param_list[index][k]=datetime.strptime(v,'%Y-%m-%dT%H:%M:%S') if v else None
-        if "[]" in datatype:query_param_list[index][k]=v.split(",") if v else None
-      if query_type in ["create","update"]:
-        if datatype in ["jsonb"]:query_param_list[index][k]=json.dumps(v) if v else None
-  return {"status":1,"message":query_param_list}
+  
+#delete index all
+async def function_delete_index_all(postgres_object):
+  query="select 'drop index ' || string_agg(i.indexrelid::regclass::text,', ' order by n.nspname,i.indrelid::regclass::text, cl.relname) as output from pg_index i join pg_class cl ON cl.oid = i.indexrelid join pg_namespace n ON n.oid = cl.relnamespace left join pg_constraint co ON co.conindid = i.indexrelid where  n.nspname <> 'information_schema' and n.nspname not like 'pg\_%' and co.conindid is null and not i.indisprimary and not i.indisunique and not i.indisexclusion and not i.indisclustered and not i.indisreplident;"
+  query_param={}
+  output=await postgres_object.fetch_all(query=query,values=query_param)
+  if output[0]["output"]:
+    query=output[0]["output"]
+    query_param={}
+    output=await postgres_object.fetch_all(query=query,values=query_param)
+  return {"status":1,"message":"done"}
