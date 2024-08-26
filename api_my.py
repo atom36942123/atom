a6e2a087d4cc43bb12d2f5512576743ce2703aa0
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-26 15:35:14 +0530

    Update api_my.py

--- api_my.py
+++ api_my.py
@@ -19,7 +19,6 @@ async def function_my_token(request:Request):
    query_param={"id":user["id"]}
    output=await postgres_object.fetch_all(query=query,values=query_param)
    user=output[0] if output else None
-   #raise error
    if not user:return JSONResponse(status_code=400,content={"status":0,"message":"no user exist for token passed"})
    #token create
    response=await function_token_create_jwt(user)
