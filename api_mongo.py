854d35b633f74a3490437b8f41c6d9d5b17b695e
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-27 11:31:28 +0530

    Update api_mongo.py

--- api_mongo.py
+++ api_mongo.py
@@ -1,46 +1,4 @@
-#router
-from fastapi import APIRouter
-router=APIRouter(tags=["mongo"])
 
-#create object
-from fastapi import Request
-from config import mongo_object
-@router.post("/mongo/create-object")
-async def function_mongo_create_object(request:Request,database:str,table:str):
-   #request body
-   request_body=await request.json()
-   #logic
-   if database=="test" and table=="users":
-      output=await mongo_object.test.users.insert_one(request_body)
-   #final
-   return {"status":1,"message":repr(output.inserted_id)}
-
-#read object
-from fastapi import Request
-from config import mongo_object
-from bson import ObjectId
-@router.get("/mongo/read-object")
-async def function_mongo_read_object(request:Request,database:str,table:str,id:str):
-   #logic
-   if database=="test" and table=="users":
-      output=response=await mongo_object.test.users.find_one({"_id":ObjectId(id)})
-      if output:output['_id']=str(output['_id'])
-   #final
-   return {"status":1,"message":output}
-
-#update object
-from fastapi import Request
-from config import mongo_object
-from bson import ObjectId
-@router.put("/mongo/update-object")
-async def function_mongo_update_object(request:Request,database:str,table:str,id:str):
-   #request body
-   request_body=await request.json()
-   #logic
-   if database=="test" and table=="users":
-      output=await mongo_object.test.users.update_one({"_id":ObjectId(id)},{"$set":request_body})
-   #final
-   return {"status":1,"message":output.modified_count}
 
 #delete object
 from fastapi import Request
@@ -50,6 +8,6 @@ from bson import ObjectId
 async def function_mongo_delete_object(request:Request,database:str,table:str,id:str):
    #logic
    if database=="test" and table=="users":
-      output=await mongo_object.test.users.delete_one({"_id":ObjectId(id)})
+      
    #final
    return {"status":1,"message":output.deleted_count}
