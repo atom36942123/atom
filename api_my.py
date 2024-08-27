efb32102fe89b63aa0efb94303d9b45914e1ca4e
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-27 20:46:41 +0530

    Update api_my.py

--- api_my.py
+++ api_my.py
@@ -251,14 +251,19 @@ from config import postgres_object
 from fastapi.responses import JSONResponse
 from function import function_auth_check
 @router.delete("/my/message-delete")
-async def function_my_message_delete(request:Request):
+async def function_my_message_delete(request:Request,mode:str):
    #auth check
    response=await function_auth_check(request,"jwt",[])
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    user=response["message"]
    #logic
-   query="delete from message where parent_table='users' and (created_by_id=:created_by_id or parent_id=:parent_id);"
-   query_param={"created_by_id":user['id'],"parent_id":user['id']}
+   if mode=="created":
+      query="delete from message where parent_table='users' and (created_by_id=:created_by_id or parent_id=:parent_id);"
+      
+   if mode=="all":
+      query="delete from message where parent_table='users' and (created_by_id=:created_by_id or parent_id=:parent_id);"
+      query_param={"created_by_id":user['id'],"parent_id":user['id']}
+   
    output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
    return {"status":1,"message":output}
