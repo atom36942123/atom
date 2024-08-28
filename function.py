68dc274c1961b21782169d0d3aae42869c9f9c91
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-28 19:04:30 +0530

    Update function.py

--- function.py
+++ function.py
@@ -262,10 +262,6 @@ async def function_background_log(postgres_object,created_by_id,request):
   path=request.url.path
   param=json.dumps(dict(request.query_params))
   body=json.dumps(dict(await request.body()))
- 
-  
-  
- 
   query="insert into log (created_by_id,request_path,request_query_param,request_body) values (:created_by_id,:request_path,:request_query_param,:request_body);"
   query_param={"created_by_id":created_by_id,"request_path":path,"request_query_param":param,"request_body":body}
   background.add_task(await postgres_object.fetch_all(query=query,values=query_param))
