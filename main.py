c58c793153909c3be9c09964f453faf2f5a73fcf
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-30 19:57:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,9 +63,6 @@ for item in file_name_api_list:
   app.include_router(x.router)
 
 #server start
-import uvicorn,asyncio
+from function import function_server_start
 if __name__=="__main__":
-  uvicorn_object=uvicorn.Server(config=uvicorn.Config(app,"0.0.0.0",8000,workers=16,log_level="info",reload=False,lifespan="on",loop="asyncio"))
-  loop=asyncio.new_event_loop()
-  asyncio.set_event_loop(loop)
-  loop.run_until_complete(uvicorn_object.serve())
+  function_background_create_log(app)
