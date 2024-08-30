d1a679f05b3b5ec71697c17b8687d36884e8f49d
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-30 19:50:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,8 +53,10 @@ async def function_root(request:Request):
   return {"status":1,"message":"welcome to atom"}
 
 #router
-
-
+import os,glob
+current_directory_path=os.path.dirname(os.path.realpath(__file__))
+file_path_all_list=[item for item in glob.glob(f"{current_directory_path}/*.py")]
+file_name_all_list=[item.rsplit("/",1)[1].split(".")[0] for item in file_path_all_list]
 file_name_api_list=[item for item in file_name_all_list if "api" in item]
 for item in file_name_api_list:
   x=__import__(item)
