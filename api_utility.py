c1f5425de340704cd1ed3ee69e87f92b753d7852
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-31 09:26:14 +0530

    Update api_utility.py

--- api_utility.py
+++ api_utility.py
@@ -41,6 +41,7 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 from config import postgres_object
 from function import function_object_read
+from function import function_add_creator_key
 from fastapi_cache.decorator import cache
 from function import function_redis_key_builder
 @router.get("/utility/object-read")
@@ -51,8 +52,12 @@ async def function_utility_object_read(request:Request,table:str,order:str="id d
    where_param_raw={k:v for k,v in request_query_param.items() if k not in ["table","order","limit","page"]}
    response=await function_object_read(postgres_object,table,where_param_raw,order,limit,(page-1)*limit)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #add creator key
+   response=await function_add_creator_key(postgres_object,output)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   output=response["message"]
    #final
-   return response
+   return {"status":1,"message":output}
 
 #bulk read
 from fastapi import Request
