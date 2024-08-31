2c581a6589ce08ebe2f97181a11559d70891e4c2
atom <102727104+atom36942123@users.noreply.github.com>
2024-08-31 11:49:55 +0530

    Update function.py

--- function.py
+++ function.py
@@ -5,17 +5,14 @@ async def function_message(postgres_object,parent_table,mode,payload):
     query=f"select * from message where parent_table=:parent_table and parent_id=:parent_id order by {order} limit {limit} offset {offset};"
     query_param={"parent_table":parent_table,"parent_id":parent_id}
   if mode=="delete_created_all":
-    created_by_id=payload["created_by_id"]
     query="delete from message where parent_table=:parent_table and created_by_id=:created_by_id;"
-    query_param={"parent_table":parent_table,"created_by_id":created_by_id}
+    query_param={"parent_table":parent_table,"created_by_id":payload["user_id"]}
   if mode=="delete_received_all":
-    parent_id=payload["parent_id"]
     query="delete from message where parent_table=:parent_table and parent_id=:parent_id;"
-    query_param={"parent_table":parent_table,"parent_id":parent_id}
+    query_param={"parent_table":parent_table,"parent_id":payload["user_id"]}
   if mode=="delete_all":
-    created_by_id,parent_id=payload["created_by_id"],payload["parent_id"]
-    query="delete from message where parent_table='users' and (created_by_id=:created_by_id or parent_id=:parent_id);"
-    query_param={"parent_table":parent_table,"created_by_id":created_by_id,"parent_id":parent_id}
+    query="delete from message where parent_table=:parent_table and (created_by_id=:created_by_id or parent_id=:parent_id);"
+    query_param={"parent_table":parent_table,"created_by_id":payload["user_id"],"parent_id":payload["user_id"]}
   output=await postgres_object.fetch_all(query=query,values=query_param)
   return {"status":1,"message":output}
 
