afb14ed1b719917e0a8c7de553b53c9b77a9bc0d
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-01 16:49:39 +0530

    Update api_external.py

--- api_external.py
+++ api_external.py
@@ -1,1 +1,37 @@
+#router
+from fastapi import APIRouter
+router=APIRouter(tags=["external"])
+
+#s3 delete single
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from config import config_key_root
+from function import function_aws
+@router.delete("/external/s3-delete-single")
+async def function_external_s3_delete_single(request:Request,url:str):
+  #auth check
+  if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content={"status":0,"message":"token root issue"})
+  #logic
+  response=await function_aws("s3_delete_single",{"url":url})
+  if response["status"]==0:return JSONResponse(status_code=400,content=response)
+  #final
+  return response
+
+#s3 delete all
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from config import config_key_root
+from function import function_aws
+@router.delete("/external/s3-delete-all")
+async def function_external_s3_delete_all(request:Request,url:str):
+  #auth check
+  if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content={"status":0,"message":"token root issue"})
+  #logic
+  response=await function_aws("s3_delete_all",{})
+  if response["status"]==0:return JSONResponse(status_code=400,content=response)
+  #final
+  return response
+
+
+
 
