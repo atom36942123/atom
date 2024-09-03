18ccbd40c545f299a2cd927013a5bcf92c51eb5a
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-03 22:14:26 +0530

    Update api_utility.py

--- api_utility.py
+++ api_utility.py
@@ -1,38 +1,3 @@
-from fastapi_cache.decorator import cache
-from function import function_redis_key_builder
-from function import function_object_read
-from function import function_add_creator_key
-@router.get("/utility/object-read")
-@cache(expire=60,key_builder=function_redis_key_builder)
-async def function_utility_object_read(request:Request,table:str,order:str="id desc",limit:int=100,page:int=1):
-   #logic
-   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
-   request_query_param=dict(request.query_params)
-   where_param_raw={k:v for k,v in request_query_param.items() if k not in ["table","order","limit","page"]}
-   response=await function_object_read(postgres_object,table,where_param_raw,order,limit,(page-1)*limit)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   output=response["message"]
-   #add creator key
-   response=await function_add_creator_key(postgres_object,output)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   output=response["message"]
-   #final
-   return {"status":1,"message":output}
-
-#bulk read
-from fastapi import Request
-from fastapi.responses import JSONResponse
-from config import postgres_object
-@router.get("/utility/bulk-read")
-async def function_utility_bulk_read(request:Request,table:str,ids:str):
-   #logic
-   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
-   query=f"select * from {table} where id in ({ids}) order by id desc;"
-   query_param={}
-   output=await postgres_object.fetch_all(query=query,values=query_param)
-   #final
-   return {"status":1,"message":output}
-
 #search location
 from fastapi import Request
 from fastapi.responses import JSONResponse
