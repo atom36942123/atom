5bf124e6a17c97a8ccc5c7ec00ef1598ccaa7040
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-03 22:24:24 +0530

    Update api.py

--- api.py
+++ api.py
@@ -587,3 +587,20 @@ async def function_utility_bulk_read(request:Request,table:str,ids:str):
    output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
    return {"status":1,"message":output}
+
+from function import function_where_raw
+from function import function_search_location
+@router.get("/utility/search-location")
+async def function_utility_search_location(request:Request,table:str,location:str,within:str,order:str="id desc",limit:int=100,page:int=1):
+   #where raw
+   request_query_param=dict(request.query_params)
+   where_param_raw={k:v for k,v in request_query_param.items() if k not in ["table","location","within","order","limit","page"]}
+   response=await function_where_raw(where_param_raw)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   where_string,where_param=response["message"][0],response["message"][1]
+   #logic
+   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
+   response=await function_search_location(postgres_object,table,where_string,where_param,location,within,order,limit,(page-1)*limit)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #final
+   return response
