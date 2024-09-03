2593f70224c7a0714f3a2f421dbf7b1906be5ebf
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-03 21:44:05 +0530

    Update api.py

--- api.py
+++ api.py
@@ -8,12 +8,10 @@ from fastapi.responses import JSONResponse
 from config import postgres_object
 from function import function_auth_check
 
-#rate limiter
-from fastapi_limiter.depends import RateLimiter
-from fastapi import Depends
-
 #auth
 import hashlib
+from fastapi_limiter.depends import RateLimiter
+from fastapi import Depends
 from function import function_token_create
 @router.post("/auth/signup",dependencies=[Depends(RateLimiter(times=1,seconds=5))])
 async def function_auth_signup(request:Request):
