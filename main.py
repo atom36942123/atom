833f723a8444dbd7df9b750dde877ca9da7f087e
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-03 15:36:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,16 @@ from config import config_sentry_dsn
 if False:sentry_sdk.init(dsn=config_sentry_dsn,traces_sample_rate=1.0,profiles_sample_rate=1.0)
 
 #lifespan
-from config import config_redis_server_url
-from function import function_redis_start
-from config import config_postgres_object
+from function import function_redis_service_init
+from config import postgres_object
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 @asynccontextmanager
 async def function_lifespan(app:FastAPI):
-  await function_redis_start(config_redis_server_url)
-  await config_postgres_object.connect()
+  await function_redis_service_init()
+  await postgres_object.connect()
   yield
-  await config_postgres_object.disconnect()
+  await postgres_object.disconnect()
   
 #app
 from fastapi import FastAPI
@@ -32,18 +31,18 @@ app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,all
 from fastapi import Request
 from fastapi.responses import JSONResponse
 import traceback
-from config import config_postgres_object
-from config import config_key_jwt,config_key_root
-from function import function_create_log
-from function import function_middleware_error
+from config import postgres_object
+from function import function_background_create_log
+from function import function_error_prepare
 @app.middleware("http")
 async def function_middleware(request:Request,api_function):
   try:
     response=await api_function(request)
-    await function_create_log(config_postgres_object,request,config_key_jwt,config_key_root)
+    await function_background_create_log(postgres_object,request)
   except Exception as e:
     print(traceback.format_exc())
-    response=await function_middleware_error(e.args)
+    error="".join(e.args)
+    response=await function_error_prepare(error)
     return JSONResponse(status_code=400,content=response)
   return response
 
@@ -54,11 +53,13 @@ async def function_root(request:Request):
   return {"status":1,"message":"welcome to atom"}
 
 #router
-from function import function_router_list
-response=function_router_list()
-router_list=response["message"]
-for item in router_list:app.include_router(item)
- 
+from function import function_read_filename_api
+response=function_read_filename_api()
+filename_api_list=response["message"]
+for item in filename_api_list:
+  x=__import__(item)
+  app.include_router(x.router)
+
 #server start
 from function import function_server_start
 if __name__=="__main__":
