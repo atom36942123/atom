1c109edbb1107de33aa582c89a8dd99d75a43f4e
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-04 08:26:06 +0530

    Update api.py

--- api.py
+++ api.py
@@ -610,3 +610,48 @@ async def function_utility_search_location(request:Request,table:str,location:st
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #final
    return response
+
+#external
+from function import function_aws
+@router.get("/external/s3-create-presigned-url")
+async def function_external_s3_create_presigned_url(request:Request,filename:str):
+   #logic
+   response=await function_aws("s3_create_presigned_url",{"filename":filename})
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #final
+   return response
+
+from function import function_aws
+@router.post("/external/ses-send-email")
+async def function_external_ses_send_email(request:Request):
+   #logic
+   body=await request.json()
+   response=await function_aws("ses_send_email",body)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #final
+   return response
+
+from function import function_aws
+@router.delete("/external/s3-delete-single")
+async def function_external_s3_delete_single(request:Request,url:str):
+   #auth check
+   response=await function_auth_check(postgres_object,request,["admin"])
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   user=response["message"]
+   #logic
+   response=await function_aws("s3_delete_single",{"url":url})
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #final
+   return response
+
+from config import config_key_root
+from function import function_aws
+@router.delete("/external/s3-delete-all")
+async def function_external_s3_delete_all(request:Request):
+   #auth check
+   if request.headers.get("Authorization").split(" ",1)[1]!=config_key_root:return JSONResponse(status_code=400,content={"status":0,"message":"token root issue"})
+   #logic
+   response=await function_aws("s3_delete_all",{})
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #final
+   return response
