62eca0df2f084abb3207c1b5c5b7891c7a447065
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-04 10:02:01 +0530

    Update api.py

--- api.py
+++ api.py
@@ -407,6 +407,81 @@ async def function_my_message_thread(request:Request,background:BackgroundTasks,
    #final
    return {"status":1,"message":output}
 
+#public
+from fastapi_cache.decorator import cache
+@router.get("/public/project-cache")
+@cache(expire=60)
+async def function_public_project_cache(request:Request):
+   #logic
+   query_dict={
+   "user_count":"select count(*) from users;"
+   }
+   temp={}
+   for k,v in query_dict.items():
+      query=v
+      query_param={}
+      output=await postgres_object.fetch_all(query=query,values=query_param)
+      temp[k]=output
+   #final
+   return {"status":1,"message":temp}
+
+from fastapi_cache.decorator import cache
+from function import function_redis_key_builder
+from function import function_where_raw
+from function import function_add_creator_key
+from function import function_add_action_count
+@router.get("/public/object-read")
+@cache(expire=60,key_builder=function_redis_key_builder)
+async def function_public_object_read(request:Request,table:str,order:str="id desc",limit:int=100,page:int=1):
+   #where raw
+   request_query_param=dict(request.query_params)
+   where_param_raw={k:v for k,v in request_query_param.items() if k not in ["table","order","limit","page"]}
+   response=await function_where_raw(where_param_raw)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   where_string,where_param=response["message"][0],response["message"][1]
+   #logic
+   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
+   query=f"select * from {table} {where_string} order by {order} limit {limit} offset {(page-1)*limit};"
+   query_param=where_param
+   output=await postgres_object.fetch_all(query=query,values=query_param)
+   #add creator key
+   response=await function_add_creator_key(postgres_object,output)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   output=response["message"]
+   #add action count
+   response=await function_add_action_count(postgres_object,output,table,"likes")
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   output=response["message"]
+   #final
+   return {"status":1,"message":output}
+
+@router.get("/public/bulk-ids-read")
+async def function_public_bulk_ids_read(request:Request,table:str,ids:str):
+   #logic
+   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
+   query=f"select * from {table} where id in ({ids}) order by id desc;"
+   query_param={}
+   output=await postgres_object.fetch_all(query=query,values=query_param)
+   #final
+   return {"status":1,"message":output}
+
+from function import function_where_raw
+from function import function_search_location
+@router.get("/public/search-location")
+async def function_public_search_location(request:Request,table:str,location:str,within:str,order:str="id desc",limit:int=100,page:int=1):
+   #where raw
+   request_query_param=dict(request.query_params)
+   where_param_raw={k:v for k,v in request_query_param.items() if k not in ["table","location","within","order","limit","page"]}
+   response=await function_where_raw(where_param_raw)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   where_string,where_param=response["message"][0],response["message"][1]
+   #logic
+   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
+   response=await function_search_location(postgres_object,table,where_string,where_param,location,within,order,limit,(page-1)*limit)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #final
+   return response
+
 #admin
 from function import function_database_init
 @router.get("/admin/database-init")
@@ -535,81 +610,6 @@ async def function_admin_bulk_ids_delete(request:Request,table:str,ids:str):
    output=await postgres_object.fetch_all(query=query,values=query_param)
    #final
    return {"status":1,"message":output}
-
-#public
-from fastapi_cache.decorator import cache
-@router.get("/public/project-cache")
-@cache(expire=60)
-async def function_public_project_cache(request:Request):
-   #logic
-   query_dict={
-   "user_count":"select count(*) from users;"
-   }
-   temp={}
-   for k,v in query_dict.items():
-      query=v
-      query_param={}
-      output=await postgres_object.fetch_all(query=query,values=query_param)
-      temp[k]=output
-   #final
-   return {"status":1,"message":temp}
-
-from fastapi_cache.decorator import cache
-from function import function_redis_key_builder
-from function import function_where_raw
-from function import function_add_creator_key
-from function import function_add_action_count
-@router.get("/public/object-read")
-@cache(expire=60,key_builder=function_redis_key_builder)
-async def function_public_object_read(request:Request,table:str,order:str="id desc",limit:int=100,page:int=1):
-   #where raw
-   request_query_param=dict(request.query_params)
-   where_param_raw={k:v for k,v in request_query_param.items() if k not in ["table","order","limit","page"]}
-   response=await function_where_raw(where_param_raw)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   where_string,where_param=response["message"][0],response["message"][1]
-   #logic
-   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
-   query=f"select * from {table} {where_string} order by {order} limit {limit} offset {(page-1)*limit};"
-   query_param=where_param
-   output=await postgres_object.fetch_all(query=query,values=query_param)
-   #add creator key
-   response=await function_add_creator_key(postgres_object,output)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   output=response["message"]
-   #add action count
-   response=await function_add_action_count(postgres_object,output,table,"likes")
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   output=response["message"]
-   #final
-   return {"status":1,"message":output}
-
-@router.get("/public/bulk-ids-read")
-async def function_public_bulk_ids_read(request:Request,table:str,ids:str):
-   #logic
-   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
-   query=f"select * from {table} where id in ({ids}) order by id desc;"
-   query_param={}
-   output=await postgres_object.fetch_all(query=query,values=query_param)
-   #final
-   return {"status":1,"message":output}
-
-from function import function_where_raw
-from function import function_search_location
-@router.get("/public/search-location")
-async def function_public_search_location(request:Request,table:str,location:str,within:str,order:str="id desc",limit:int=100,page:int=1):
-   #where raw
-   request_query_param=dict(request.query_params)
-   where_param_raw={k:v for k,v in request_query_param.items() if k not in ["table","location","within","order","limit","page"]}
-   response=await function_where_raw(where_param_raw)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   where_string,where_param=response["message"][0],response["message"][1]
-   #logic
-   if table not in ["users","post","atom","box"]:return JSONResponse(status_code=400,content=({"status":0,"message":"table not allowed"}))
-   response=await function_search_location(postgres_object,table,where_string,where_param,location,within,order,limit,(page-1)*limit)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   #final
-   return response
 
 #external
 from function import function_aws
