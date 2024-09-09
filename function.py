5176cba042e9a4aab309137e331b51fae30ccce4
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-09 15:41:43 +0530

    Update function.py

--- function.py
+++ function.py
@@ -108,6 +108,7 @@ async def function_object_create(postgres_object,mode,table,object_list):
 #verify otp
 async def function_otp_verify(postgres_object,otp,email,mobile):
   if email and mobile:return {"status":0,"message":"only one contact allowed"}
+  if not email and not mobile:return {"status":0,"message":"only one contact is mandatory"}
   if email:
     query="select otp from otp where email=:email order by id desc limit 1;"
     query_param={"email":email}
