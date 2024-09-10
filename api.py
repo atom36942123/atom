174c214232c88a61346fa92461cf221c4a9ca805
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-10 13:23:21 +0530

    Update api.py

--- api.py
+++ api.py
@@ -383,11 +383,14 @@ import random
 from function import function_sns
 @router.get("/utility/otp-send")
 async def function_utility_otp_send(request:Request,email:str=None,mobile:str=None):
-   #logic
+   #prework
    if email and mobile:return JSONResponse(status_code=400,content={"status":0,"message":"send either email or mobile"})
    otp=random.randint(100000,999999)
-   if mobile:
-   response=await function_sns("send_sms",filename,None)
+   #otp send
+   if email:response=await function_sns("send_sms",mobile,f"otp from atom {otp}")
+   if mobile:response=await function_sns("send_sms",mobile,f"otp from atom {otp}")
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #save otp
    query="insert into otp (otp,email,mobile) values (:otp,:email,:mobile) returning *;"
    query_param={"otp":otp,"email":email,"mobile":mobile}
    output=await postgres_object.fetch_all(query=query,values=query_param)
