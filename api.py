a5771d353e273a82d3f13e34def573a559572f60
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-10 13:48:09 +0530

    Update api.py

--- api.py
+++ api.py
@@ -387,8 +387,10 @@ async def function_utility_otp_send(request:Request,email:str=None,mobile:str=No
    if email and mobile:return JSONResponse(status_code=400,content={"status":0,"message":"send either email or mobile"})
    otp=random.randint(100000,999999)
    #otp send
-   if mobile:response=await function_sns("send_sms",{"mobile":mobile,"message":f"otp from atom {otp}"})
-   if email:response=await function_ses("send_sms",mobile,f"otp from atom {otp}")
+   if mobile:
+      response=await function_sns("send_sms",{"mobile":mobile,"message":f"otp from atom {otp}"})
+   if email:
+      response=await function_ses("send_email",{"to":email,"title":"otp","description":f"otp from atom {otp}"})
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #save otp
    query="insert into otp (otp,email,mobile) values (:otp,:email,:mobile) returning *;"
@@ -410,7 +412,8 @@ from function import function_s3
 @router.get("/utility/file-upload")
 async def function_utility_file_upload(request:Request,mode:str,filename:str):
    #logic
-   if mode=="s3":response=await function_s3("create_url",{"filename":filename})
+   if mode=="s3":
+      response=await function_s3("create_url",{"filename":filename})
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #final
    return response
@@ -423,7 +426,8 @@ async def function_utility_file_delete(request:Request,mode:str,url:str):
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    user=response["message"]
    #logic
-   if mode=="s3":response=await function_s3("delete_url",{"url":url})
+   if mode=="s3":
+      response=await function_s3("delete_url",{"url":url})
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #final
    return response
