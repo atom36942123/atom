b356ab97d3bbd0e27211f0610bddadc71e4f79ac
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-10 13:44:10 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,9 +1,10 @@
 #sns
 from config import config_aws_access_key_id,config_aws_secret_access_key
 from config import  config_sns_region_name
-async def function_sns(mode,mobile,message):
+async def function_sns(mode,payload):
   sns_client=boto3.client("sns",region_name=config_sns_region_name,aws_access_key_id=config_aws_access_key_id,aws_secret_access_key=config_aws_secret_access_key)
   if mode=="send_message":
+    mobile,message=payload["mobile"],payload["message"]
     output=sns_client.publish(PhoneNumber=mobile,Message=message)
   return {"status":1,"message":output}
 
