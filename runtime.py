951ebf6ce35cb7a0a1ce181d383b929c37c9f7a8
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-11 14:41:21 +0530

    Update runtime.py

--- runtime.py
+++ runtime.py
@@ -4,7 +4,7 @@ postgres_object=Database(config_postgres_database_url,min_size=1,max_size=100)
 
 #column datatype
 from function import function_postgres_column_datatype
-response=await function_postgres_column_datatype(postgres_object)
+response=function_postgres_column_datatype(postgres_object)
 config_column_datatype=response["message"]
 
 
