b8bbb08451a66e010b4b3d2693f6dab6956b2828
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-11 14:48:33 +0530

    Update runtime.py

--- runtime.py
+++ runtime.py
@@ -1,7 +1,4 @@
-#postgres object
-from config import config_postgres_database_url
-from databases import Database
-postgres_object=Database(config_postgres_database_url,min_size=1,max_size=100)
+
 
 #column datatype
 from function import function_postgres_column_datatype
