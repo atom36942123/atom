83d6c97a1da380feb8d76f47ac2c63693df775e1
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-11 13:29:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,4 +21,4 @@ config_is_delete_object_self=env("is_delete_object_self")
 
 #postgres object
 from databases import Database
-config_postgres_object=Database(config_postgres_database_url,min_size=1,max_size=100)
+postgres_object=Database(config_postgres_database_url,min_size=1,max_size=100)
