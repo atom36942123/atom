5c14c67b3532b9f87c005a1b6d8a649d9f0f8308
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-11 14:49:24 +0530

    Update runtime.py

--- runtime.py
+++ runtime.py
@@ -1,8 +1,5 @@
 
 
-#column datatype
-from function import function_postgres_column_datatype
-response=function_postgres_column_datatype(postgres_object)
-config_column_datatype=response["message"]
+
 
 
