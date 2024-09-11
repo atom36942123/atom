1f4671fa1916efe1da280100b6d4c5e760d86594
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-11 14:50:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from databases import Database
 postgres_object=Database(config_postgres_database_url,min_size=1,max_size=100)
 
 #column datatype
-from function import function_postgres_column_datatype
-response=function_postgres_column_datatype(postgres_object)
-config_column_datatype=response["message"]
+# from function import function_postgres_column_datatype
+# response=function_postgres_column_datatype(postgres_object)
+# config_column_datatype=response["message"]
 
 #logging
 import logging
