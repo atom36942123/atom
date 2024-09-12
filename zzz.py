2648f3474fe61d9872d3cbac44a54099e74f3799
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-12 22:15:46 +0530

    Update zzz.py

--- zzz.py
+++ zzz.py
@@ -1,44 +1,9 @@
-import random,boto3
-from config import aws_default_region,aws_access_key_id,aws_secret_access_key
-from config import ses_sender_email
-from function import postgres_otp_verify
-@router.get("/utility")
-async def utility(request:Request,mode:str,email:str=None,mobile:str=None):
-   #logic
-   if mode=="send_otp_mobile_sns":
-      if not mobile:return JSONResponse(status_code=400,content={"status":0,"message":"mobile is must"})
-      otp=random.randint(100000,999999)
-      sns_client=boto3.client("sns",region_name=aws_default_region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-      output=sns_client.publish(PhoneNumber=mobile,Message=f"otp={otp}")
-      query="insert into otp (otp,mobile) values (:otp,:mobile) returning *;"
-      query_param={"otp":otp,"mobile":mobile}
-      output=await postgres_object.fetch_all(query=query,values=query_param)
-      response={"status":1,"message":"otp sent"}
-   if mode=="send_otp_email_ses":
-      if not email:return JSONResponse(status_code=400,content={"status":0,"message":"email is must"})
-      otp=random.randint(100000,999999)
-      ses_client=boto3.client("ses",region_name=aws_default_region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-      output=ses_client.send_email(Source=ses_sender_email,Destination={"ToAddresses":[email]},Message={"Subject":{"Charset":"UTF-8","Data":"otp"},"Body":{"Text":{"Charset":"UTF-8","Data":str(otp)}}})
-      query="insert into otp (otp,email) values (:otp,:email) returning *;"
-      query_param={"otp":otp,"email":email}
-      output=await postgres_object.fetch_all(query=query,values=query_param)
-      response={"status":1,"message":"otp sent"}
-   if mode=="otp_verify":
-      
-   #final
-   return response
 
 
 
 
 
-@router.get("/utility/otp-verify")
-async def function_utility_otp_verify(request:Request,otp:int,email:str=None,mobile:str=None):
-   #logic
-   response=await function_otp_verify(postgres_object,otp,email,mobile)
-   if response["status"]==0:return JSONResponse(status_code=400,content=response)
-   #final
-   return response
+
 
 from function import function_s3
 @router.get("/utility/file")
