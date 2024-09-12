7988f01ff4a5bf09a50c47b3d0118ad0ae95462a
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-12 07:38:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ if False:sentry_sdk.init(dsn=sentry_dsn,traces_sample_rate=1.0,profiles_sample_r
 #lifespan
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from function import function_redis_start
+from function import redis_start
 from config import redis_server_url
-from function import function_postgres_column_datatype
+from function import postgres_column_datatype
 @asynccontextmanager
-async def function_lifespan(app:FastAPI):
-  await function_redis_start(redis_server_url)
+async def lifespan(app:FastAPI):
+  await redis_start(redis_server_url)
   await postgres_object.connect()
-  response=await function_postgres_column_datatype(postgres_object)
+  response=await postgres_column_datatype(postgres_object)
   global column_datatype
   column_datatype=response["message"]
   yield
@@ -31,7 +31,7 @@ async def function_lifespan(app:FastAPI):
   
 #app
 from fastapi import FastAPI
-app=FastAPI(lifespan=function_lifespan,title="atom")
+app=FastAPI(lifespan=lifespan,title="atom")
 
 #cors
 from fastapi.middleware.cors import CORSMiddleware
@@ -41,35 +41,35 @@ app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,all
 from fastapi import Request
 from fastapi.responses import JSONResponse
 import traceback
-from function import function_middleware_error
-from function import function_postgres_create_log
+from function import middleware_error
+from function import postgres_create_log
 from config import jwt_secret_key
 @app.middleware("http")
-async def function_middleware(request:Request,api_function):
+async def middleware(request:Request,api_function):
   try:
     request.state.postgres_object=postgres_object
     request.state.column_datatype=column_datatype
     response=await api_function(request)
-    await function_postgres_create_log(postgres_object,request,jwt_secret_key)
+    await postgres_create_log(postgres_object,request,jwt_secret_key)
   except Exception as e:
     print(traceback.format_exc())
-    response=await function_middleware_error(e.args)
+    response=await middleware_error(e.args)
     return JSONResponse(status_code=400,content=response)
   return response
 
 #root api
 from fastapi import Request
 @app.get("/")
-async def function_root(request:Request):
+async def root(request:Request):
   return {"status":1,"message":"welcome to atom"}
 
 #router
-from function import function_router_list
-response=function_router_list()
+from function import router_list
+response=router_list()
 router_list=response["message"]
 for item in router_list:app.include_router(item)  
   
 #server start
-from function import function_server_start
+from function import server_start
 if __name__=="__main__":
-  function_server_start(app)
+  server_start(app)
