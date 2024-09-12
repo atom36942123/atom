597025fd8ce3b2abae27996f58c3b257fc60f036
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-12 07:38:41 +0530

    Update api.py

--- api.py
+++ api.py
@@ -5,16 +5,16 @@ router=APIRouter(tags=["api"])
 #database
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from function import function_postgres_init
-from function import function_postgres_clean
+from function import postgres_init
+from function import postgres_clean
 @router.get("/database")
-async def function_database(request:Request,mode:str):
+async def database(request:Request,mode:str):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
    #logic
-   if mode=="init":response=await function_postgres_init(postgres_object)
-   if mode=="clean":response=await function_postgres_clean(postgres_object)
+   if mode=="init":response=await postgres_init(postgres_object)
+   if mode=="clean":response=await postgres_clean(postgres_object)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #final
    return response
@@ -23,12 +23,12 @@ async def function_database(request:Request,mode:str):
 from fastapi import Request
 from fastapi.responses import JSONResponse
 import hashlib
-from function import function_token_create
+from function import token_create
 from config import jwt_secret_key
 from fastapi import Depends
 from fastapi_limiter.depends import RateLimiter
 @router.post("/signup",dependencies=[Depends(RateLimiter(times=1,seconds=3))])
-async def function_signup(request:Request,username:str,password:str):
+async def signup(request:Request,username:str,password:str):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
@@ -38,7 +38,7 @@ async def function_signup(request:Request,username:str,password:str):
    output=await postgres_object.fetch_all(query=query,values=query_param)
    user=user=output[0]
    #token create
-   response=await function_token_create(user,jwt_secret_key)
+   response=await token_create(user,jwt_secret_key)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    token=response["message"]
    #final
@@ -48,12 +48,12 @@ async def function_signup(request:Request,username:str,password:str):
 from fastapi import Request
 from fastapi.responses import JSONResponse
 import hashlib
-from function import function_token_create
+from function import token_create
 from config import jwt_secret_key
-from function import function_postgres_read_user_force
-from function import function_postgtes_otp_verify
+from function import postgres_read_user_force
+from function import postgtes_otp_verify
 @router.post("/login")
-async def function_login(request:Request,mode:str,username:str=None,password:str=None,google_id:str=None,otp:int=None,email:str=None,mobile:str=None,type:str=None):
+async def login(request:Request,mode:str,username:str=None,password:str=None,google_id:str=None,otp:int=None,email:str=None,mobile:str=None,type:str=None):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
@@ -65,25 +65,25 @@ async def function_login(request:Request,mode:str,username:str=None,password:str
       user=output[0] if output else None
       if not user:return JSONResponse(status_code=400,content={"status":0,"message":"no user"})
    if mode=="oauth_google":
-      response=await function_postgres_read_user_force(postgres_object,"google_id",google_id)
+      response=await postgres_read_user_force(postgres_object,"google_id",google_id)
       if response["status"]==0:return JSONResponse(status_code=400,content=response)
       user=response["message"]
    if mode=="otp_email":
-      response=await function_postgres_otp_verify(postgres_object,otp,email,None)
+      response=await postgres_otp_verify(postgres_object,otp,email,None)
       if response["status"]==0:return JSONResponse(status_code=400,content=response)
-      response=await function_postgres_read_user_force(postgres_object,"email",email)
+      response=await postgres_read_user_force(postgres_object,"email",email)
       if response["status"]==0:return JSONResponse(status_code=400,content=response)
       user=response["message"]
    if mode=="otp_mobile":
-      response=await function_postgres_otp_verify(postgres_object,otp,None,mobile)
+      response=await postgres_otp_verify(postgres_object,otp,None,mobile)
       if response["status"]==0:return JSONResponse(status_code=400,content=response)
-      response=await function_postgres_read_user_force(postgres_object,"mobile",mobile)
+      response=await postgres_read_user_force(postgres_object,"mobile",mobile)
       if response["status"]==0:return JSONResponse(status_code=400,content=response)
       user=response["message"]
    #user type check
    if type and user["type"]!=type:return JSONResponse(status_code=400,content={"status":0,"message":f"only {type} can login"})
    #token create
-   response=await function_token_create(user,jwt_secret_key)
+   response=await token_create(user,jwt_secret_key)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    token=response["message"]
    #final
@@ -92,18 +92,18 @@ async def function_login(request:Request,mode:str,username:str=None,password:str
 #my
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from function import function_auth_check
+from function import auth_check
 from config import jwt_secret_key
 from datetime import datetime
-from function import function_token_create
-from function import function_postgres_object_update
+from function import token_create
+from function import postgres_object_update
 @router.get("/my")
-async def function_my(request:Request,mode:str):
+async def my(request:Request,mode:str):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
    #auth check
-   response=await function_auth_check(request,jwt_secret_key,None,None,None)
+   response=await auth_check(request,jwt_secret_key,None,None,None)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    user=response["message"]
    #logic
@@ -120,7 +120,7 @@ async def function_my(request:Request,mode:str):
       output=await postgres_object.fetch_all(query=query,values=query_param)
       user=output[0] if output else None
       if not user:return JSONResponse(status_code=400,content={"status":0,"message":"no user"})
-      response=await function_token_create(user,jwt_secret_key)
+      response=await token_create(user,jwt_secret_key)
       if response["status"]==0:return JSONResponse(status_code=400,content=response)
    if mode=="exit":
       if True:return {"status":1,"message":"exit not allowed"}
@@ -130,23 +130,23 @@ async def function_my(request:Request,mode:str):
       response={"status":1,"message":"account deleted"}
    #update last active at
    object={"id":user["id"],"last_active_at":datetime.now().strftime('%Y-%m-%dT%H:%M:%S')}
-   await function_postgres_object_update(postgres_object,column_datatype,"background","users",[object])
+   await postgres_object_update(postgres_object,column_datatype,"background","users",[object])
    #final
    return response
 
 #object create
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from function import function_auth_check
+from function import auth_check
 from config import jwt_secret_key
-from function import function_postgres_object_create
+from function import postgres_object_create
 @router.post("/object")
-async def function_object(request:Request,table:str):
+async def object(request:Request,table:str):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
    #auth
-   response=await function_auth_check(request,jwt_secret_key,None,None,None)
+   response=await auth_check(request,jwt_secret_key,None,None,None)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    user=response["message"]
    #object
@@ -157,7 +157,7 @@ async def function_object(request:Request,table:str):
    for item in ["id","created_at","updated_at","updated_by_id","is_active","is_verified","is_protected","password","google_id","otp"]:
       if item in object:return JSONResponse(status_code=400,content={"status":0,"message":f"{item} not allowed"})
    #logic
-   response=await function_postgres_object_create(postgres_object,column_datatype,"normal",table,[object])
+   response=await postgres_object_create(postgres_object,column_datatype,"normal",table,[object])
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #final
    return response
@@ -165,24 +165,24 @@ async def function_object(request:Request,table:str):
 #object update
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from function import function_auth_check
+from function import auth_check
 from config import jwt_secret_key
-from function import function_object_ownership_check
-from function import function_postgres_object_ownership_check
+from function import object_ownership_check
+from function import postgres_object_ownership_check
 @router.put("/my/object-update")
-async def function_my_object_update(request:Request,table:str):
+async def my_object_update(request:Request,table:str):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
    #auth
-   response=await function_auth_check(request,jwt_secret_key,None,None,None)
+   response=await auth_check(request,jwt_secret_key,None,None,None)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    user=response["message"]
    #object
    object=await request.json()
    object["updated_by_id"]=user["id"]
    #object ownership check
-   response=await function_postgres_object_ownership_check(postgres_object,table,object["id"],user["id"])
+   response=await postgres_object_ownership_check(postgres_object,table,object["id"],user["id"])
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #object check
    if table in ["spatial_ref_sys","otp","log","atom","box"]:return JSONResponse(status_code=400,content={"status":0,"message":"table not allowed"})
@@ -192,7 +192,7 @@ async def function_my_object_update(request:Request,table:str):
       for item in ["email","mobile"]:
          if item in object:return JSONResponse(status_code=400,content={"status":0,"message":f"{item} not allowed"})
    #logic
-   response=await function_postgres_object_update(postgres_object,column_datatype,"normal",table,[object])
+   response=await postgres_object_update(postgres_object,column_datatype,"normal",table,[object])
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #final
    return response
@@ -200,28 +200,28 @@ async def function_my_object_update(request:Request,table:str):
 #csv
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from function import function_auth_check
+from function import auth_check
 from config import jwt_secret_key
 from fastapi import UploadFile
-from function import function_csv_to_object_list
-from function import function_postgres_object_create
-from function import function_postgres_object_update
+from function import csv_to_object_list
+from function import postgres_object_create
+from function import postgres_object_update
 @router.post("/csv")
-async def function_csv(request:Request,mode:str,table:str,file:UploadFile):
+async def csv(request:Request,mode:str,table:str,file:UploadFile):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
    #auth
-   response=await function_auth_check(request,jwt_secret_key,postgres_object,1,["admin"])
+   response=await auth_check(request,jwt_secret_key,postgres_object,1,["admin"])
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    user=response["message"]
    #file
-   response=await function_csv_to_object_list(file)
+   response=await csv_to_object_list(file)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    object_list=response["message"]
    #logic
-   if mode=="create":response=await function_postgres_object_create(postgres_object,column_datatype,"normal",table,object_list)
-   if mode=="update":response=await function_postgres_object_update(postgres_object,column_datatype,"normal",table,object_list)
+   if mode=="create":response=await postgres_object_create(postgres_object,column_datatype,"normal",table,object_list)
+   if mode=="update":response=await postgres_object_update(postgres_object,column_datatype,"normal",table,object_list)
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #final
    return response
@@ -229,15 +229,15 @@ async def function_csv(request:Request,mode:str,table:str,file:UploadFile):
 #qrunner
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from function import function_auth_check
+from function import auth_check
 from config import jwt_secret_key
 @router.get("/qrunner")
-async def function_qrunner(request:Request,mode:str,query:str):
+async def qrunner(request:Request,mode:str,query:str):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
    #auth
-   response=await function_auth_check(request,jwt_secret_key,postgres_object,1,["admin"])
+   response=await auth_check(request,jwt_secret_key,postgres_object,1,["admin"])
    if response["status"]==0:return JSONResponse(status_code=400,content=response)
    user=response["message"]
    #logic
@@ -257,10 +257,10 @@ async def function_qrunner(request:Request,mode:str,query:str):
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from fastapi_cache.decorator import cache
-from function import function_redis_key_builder
+from function import redis_key_builder
 @router.get("/pcache")
-@cache(expire=60,key_builder=function_redis_key_builder)
-async def function_pcache(request:Request):
+@cache(expire=60,key_builder=redis_key_builder)
+async def pcache(request:Request):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
