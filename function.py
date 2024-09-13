617971346c47bd3c87dd43b29e672f2fdba1a508
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-13 08:55:56 +0530

    Update function.py

--- function.py
+++ function.py
@@ -238,7 +238,7 @@ async def postgres_add_creator_key(postgres_object,object_list):
 import jwt,json
 from fastapi import BackgroundTasks
 async def postgres_create_log(postgres_object,request,jwt_secret_key):
-  #if request.method not in ["DELETE"]:return {"status":1,"message":"done"}
+  if request.method not in ["DELETE"]:return {"status":1,"message":"done"}
   created_by_id=None
   authorization_header=request.headers.get("Authorization")
   if authorization_header:created_by_id=json.loads(jwt.decode(authorization_header.split(" ",1)[1],jwt_secret_key,algorithms="HS256")["data"])["id"]
