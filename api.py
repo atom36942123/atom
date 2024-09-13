943ff34ab30a983f8cb743caf8a76d4839310ec6
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-13 23:02:52 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 #router
 from fastapi import APIRouter
-router=APIRouter(tags=["core"])
+router=APIRouter(tags=["api"])
 
 #postgres-init
 from fastapi import Request
@@ -868,43 +868,76 @@ async def object_update_admin(request:Request,table:str):
    #final
    return response
 
-#otp
+#otp send mobile sns
 from fastapi import Request
 from fastapi.responses import JSONResponse
-import random,boto3
-from config import aws_default_region,aws_access_key_id,aws_secret_access_key,ses_sender_email
-from function import postgtes_otp_verify
-@router.get("/otp")
-async def otp(request:Request,mode:str,email:str=None,mobile:str=None,otp:int=None):
+from config import aws_default_region,aws_access_key_id,aws_secret_access_key
+import boto3,random
+@router.get("/otp-send-mobile-sns")
+async def otp_send_mobile_sns(request:Request,mobile:str):
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
    #logic
-   if mode=="send_otp_mobile_sns":
-      if not mobile:return JSONResponse(status_code=400,content={"status":0,"message":"mobile is must"})
-      otp=random.randint(100000,999999)
-      sns_client=boto3.client("sns",region_name=aws_default_region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-      output=sns_client.publish(PhoneNumber=mobile,Message=f"otp={otp}")
-      query="insert into otp (otp,mobile) values (:otp,:mobile) returning *;"
-      query_param={"otp":otp,"mobile":mobile}
-      output=await postgres_object.fetch_all(query=query,values=query_param)
-      response={"status":1,"message":"otp sent"}
-   if mode=="send_otp_email_ses":
-      if not email:return JSONResponse(status_code=400,content={"status":0,"message":"email is must"})
-      otp=random.randint(100000,999999)
-      ses_client=boto3.client("ses",region_name=aws_default_region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
-      output=ses_client.send_email(Source=ses_sender_email,Destination={"ToAddresses":[email]},Message={"Subject":{"Charset":"UTF-8","Data":"otp"},"Body":{"Text":{"Charset":"UTF-8","Data":str(otp)}}})
-      query="insert into otp (otp,email) values (:otp,:email) returning *;"
-      query_param={"otp":otp,"email":email}
-      output=await postgres_object.fetch_all(query=query,values=query_param)
-      response={"status":1,"message":"otp sent"}
-   if mode=="otp_verify":
-      if not otp:return JSONResponse(status_code=400,content={"status":0,"message":"otp is must"})
-      if not email and not mobile:return JSONResponse(status_code=400,content={"status":0,"message":"email/mobile any ome is must"})
-      response=await postgtes_otp_verify(postgres_object,otp,email,mobile)
-      if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   otp=random.randint(100000,999999)
+   sns_client=boto3.client("sns",region_name=aws_default_region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+   output=sns_client.publish(PhoneNumber=mobile,Message=f"otp={otp}")
+   query="insert into otp (otp,mobile) values (:otp,:mobile) returning *;"
+   query_param={"otp":otp,"mobile":mobile}
+   output=await postgres_object.fetch_all(query=query,values=query_param)
+   #final
+   return {"status":1,"message":output}
+
+#otp send email ses
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from config import aws_default_region,aws_access_key_id,aws_secret_access_key,ses_sender_email
+import boto3,random
+@router.get("/otp-send-email-ses")
+async def otp_send_email_ses(request:Request,email:str):
+   #middleware
+   postgres_object=request.state.postgres_object
+   column_datatype=request.state.column_datatype
+   #logic
+   otp=random.randint(100000,999999)
+   ses_client=boto3.client("ses",region_name=aws_default_region,aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+   output=ses_client.send_email(Source=ses_sender_email,Destination={"ToAddresses":[email]},Message={"Subject":{"Charset":"UTF-8","Data":"otp"},"Body":{"Text":{"Charset":"UTF-8","Data":str(otp)}}})
+   query="insert into otp (otp,email) values (:otp,:email) returning *;"
+   query_param={"otp":otp,"email":email}
+   output=await postgres_object.fetch_all(query=query,values=query_param)
+   #final
+   return {"status":1,"message":output}
+
+#otp verify email
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from function import postgtes_otp_verify
+@router.get("/otp-verify-email")
+async def ot_verify_email(request:Request,otp:int,email:str):
+   #middleware
+   postgres_object=request.state.postgres_object
+   column_datatype=request.state.column_datatype
+   #logic
+   response=await postgtes_otp_verify(postgres_object,otp,email,None)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
    #final
    return response
+
+#otp verify mobile
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from function import postgtes_otp_verify
+@router.get("/otp-verify-mobile")
+async def ot_verify_mobile(request:Request,otp:int,mobile:str):
+   #middleware
+   postgres_object=request.state.postgres_object
+   column_datatype=request.state.column_datatype
+   #logic
+   response=await postgtes_otp_verify(postgres_object,otp,None,mobile)
+   if response["status"]==0:return JSONResponse(status_code=400,content=response)
+   #final
+   return response
+
 
 #blob
 from fastapi import Request
