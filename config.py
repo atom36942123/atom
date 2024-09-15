ae38bdd8918cbfcbfab8d0f95f12784b28493082
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-15 07:53:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,14 @@ redis_server_url=env("redis_server_url")
 mongo_server_url=env("mongo_server_url")
 sentry_dsn=env("sentry_dsn")
 jwt_secret_key=env("jwt_secret_key")
-elasticsearch=env("elasticsearch")
-sns=
+elasticsearch_username=env("elasticsearch_username")
+elasticsearch_password=env("elasticsearch_password")
+elasticsearch_cloud_id=env("elasticsearch_cloud_id")
+sns_region=env("sns_region")
+sns_access_key_id=env("access_key_id")
+sns_secret_access_key=env("secret_access_key")
 
 
-aws_default_region=env("aws_default_region")
-aws_access_key_id=env("aws_access_key_id")
-aws_secret_access_key=env("aws_secret_access_key")
-s3_bucket_name=env("s3_bucket_name")
-ses_sender_email=env("ses_sender_email")
 
 
 
