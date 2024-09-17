523c244005b2ed7bd97645619d3856ea44901633
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-17 19:13:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,8 +72,11 @@ from function import router_list
 response=router_list()
 router_list=response["message"]
 for item in router_list:app.include_router(item)  
-  
+
 #server start
-from function import server_start
+import uvicorn,asyncio
 if __name__=="__main__":
-  server_start(app)
+  uvicorn_object=uvicorn.Server(config=uvicorn.Config(app,"0.0.0.0",8000,workers=16,log_level="info",reload=False,lifespan="on",loop="asyncio"))
+  loop=asyncio.new_event_loop()
+  asyncio.set_event_loop(loop)
+  loop.run_until_complete(uvicorn_object.serve())
