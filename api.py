011347a17c9436aca52042687b8aa7b2cb0161c4
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-18 09:56:52 +0530

    Update api.py

--- api.py
+++ api.py
@@ -667,7 +667,7 @@ async def admin_postgres_init(request:Request):
    #auth check
    token=request.headers.get("Authorization").split(" ",1)[1]
    token_hashed=hashlib.sha256(token.encode()).hexdigest()
-   if token_hashed!="a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3":return JSONResponse(status_code=400,content={"status":0,"message":"root token issue"})
+   if token_hashed!="a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3":return JSONResponse(status_code=400,content={"status":0,"message":"token root issue"})
    #middleware
    postgres_object=request.state.postgres_object
    column_datatype=request.state.column_datatype
