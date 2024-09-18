819340d344108fddbc2a4d99dc044b538bb32692
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-18 08:20:33 +0530

    Update function.py

--- function.py
+++ function.py
@@ -438,7 +438,7 @@ async def postgres_init(postgres_object):
   "create or replace rule rule_delete_disable_root_user as on delete to users where old.id=1 do instead nothing;",
   "CREATE OR REPLACE FUNCTION function_set_updated_at_now() RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = now(); RETURN NEW; END; $$ language 'plpgsql';",
   "CREATE OR REPLACE FUNCTION function_delete_disable_bulk() RETURNS trigger LANGUAGE plpgsql AS $$DECLARE n bigint := TG_ARGV[0]; BEGIN IF (SELECT count(*) FROM deleted_rows) <= n IS NOT TRUE THEN RAISE EXCEPTION 'cant delete more than % rows', n; END IF; RETURN OLD; END;$$;",
-  "CREATE TRIGGER trigger_delete_disable_bulk_users AFTER DELETE ON users REFERENCING OLD TABLE AS deleted_rows FOR EACH STATEMENT EXECUTE PROCEDURE function_delete_disable_bulk(1);",
+  "CREATE OR REPLACE TRIGGER trigger_delete_disable_bulk_users AFTER DELETE ON users REFERENCING OLD TABLE AS deleted_rows FOR EACH STATEMENT EXECUTE PROCEDURE function_delete_disable_bulk(1);",
   ]
   #start
   temp={}
