35fec3e9ba50425f70c922a0e8804c88a8438619
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-19 08:13:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -98,3 +98,6 @@ postgres_postquery=[
 "CREATE OR REPLACE FUNCTION function_delete_disable_bulk() RETURNS trigger LANGUAGE plpgsql AS $$DECLARE n bigint := TG_ARGV[0]; BEGIN IF (SELECT count(*) FROM deleted_rows) <= n IS NOT TRUE THEN RAISE EXCEPTION 'cant delete more than % rows', n; END IF; RETURN OLD; END;$$;",
 "CREATE OR REPLACE TRIGGER trigger_delete_disable_bulk_users AFTER DELETE ON users REFERENCING OLD TABLE AS deleted_rows FOR EACH STATEMENT EXECUTE PROCEDURE function_delete_disable_bulk(1);",
 ]
+
+#policy
+
