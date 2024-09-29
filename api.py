9de512269bd4463c4071d6d8b8ebb619051ea6ad
atom <102727104+atom36942123@users.noreply.github.com>
2024-09-29 08:47:26 +0530

    Update api.py

--- api.py
+++ api.py
@@ -41,7 +41,7 @@ async def pinit(request:Request):
   #auth
   if request.headers.get("Authorization").split(" ",1)[1]!=root_secret_key:return JSONResponse(status_code=400,content={"status":0,"message":"auth issue"})
   #logic
-  response=await postgres_init(postgres_object,postgres_prequery,postgres_table,postgres_column,postgres_notnull,postgres_identity,postgres_default,postgres_unique,postgres_index,postgres_postquery)
+  response=await postgres_init(postgres_object,postgres_prequery,postgres_table,postgres_column,postgres_notnull,postgres_identity,postgres_unique,postgres_index,postgres_postquery)
   if response["status"]==0:return JSONResponse(status_code=400,content=response)
   #final
   return response
