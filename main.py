6ca68cb53266c4a05e73b3b455823ad5ac23cc64
atom <102727104+atom36942123@users.noreply.github.com>
2024-10-03 15:56:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,7 @@ from config import sentry_dsn
 import sentry_sdk
 if False:sentry_sdk.init(dsn=sentry_dsn,traces_sample_rate=1.0,profiles_sample_rate=1.0)
 
-#lifespan
-from fastapi import FastAPI
-from contextlib import asynccontextmanager
-from function import redis_service_start
-from config import redis_server_url
-from function import postgres_column_datatype
-@asynccontextmanager
-async def lifespan(app:FastAPI):
-  await redis_service_start(redis_server_url)
-  await postgres_object.connect()
-  global column_datatype
-  response=await postgres_column_datatype(postgres_object)
-  column_datatype=response["message"]
-  yield
-  await postgres_object.disconnect()
+
 
 
   
