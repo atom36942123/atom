b751ac86f5d2c31468b73e9d9ae4e7b7a9d047eb
atom <102727104+atom36942123@users.noreply.github.com>
2024-10-03 14:15:39 +0530

    Update lifespan.py

--- lifespan.py
+++ lifespan.py
@@ -1,1 +1,15 @@
-
+#lifespan
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from function import redis_service_start
+from config import redis_server_url
+from function import postgres_column_datatype
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+  await redis_service_start(redis_server_url)
+  await postgres_object.connect()
+  global column_datatype
+  response=await postgres_column_datatype(postgres_object)
+  column_datatype=response["message"]
+  yield
+  await postgres_object.disconnect()
