52653b1a90343888eddbcad3b81ddf21bb2a5cfc
atom <102727104+atom36942123@users.noreply.github.com>
2024-10-03 15:58:06 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-#app
-from fastapi import FastAPI
-app=FastAPI(lifespan=lifespan,title="atom")
-
-#cors
-from fastapi.middleware.cors import CORSMiddleware
-app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
