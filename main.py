a675bde8a2b5ec556e1faa48adca59e34170c259
atom <102727104+atom36942123@users.noreply.github.com>
2024-10-03 14:36:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,13 +31,7 @@ async def lifespan(app:FastAPI):
 
 
   
-#app
-from fastapi import FastAPI
-app=FastAPI(lifespan=lifespan,title="atom")
 
-#cors
-from fastapi.middleware.cors import CORSMiddleware
-app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
   
 #middleware
 from fastapi import Request
